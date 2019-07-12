c4ba295b4f103e05e45c739ca56b1f94a143625a
Denis Batvinkin <batvos7@gmail.com>
2019-07-12 19:32:47 +0300

    Initial Commit

---
+++ AnyType/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AnyType
+//
+//  Created by Denis Batvinkin on 12.07.2019.
+//  Copyright © 2019 AnyType. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
