8803824a008dafe49e3478ddd284c26c6922cbc2
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-12-13 17:29:01 +0300

    presentation: common views text view toolbars have been added.

---
+++ AnyType/Sources/PresentationLayer/Common Views/TextView/TextView+BlocksAccessoryView.swift
@@ -0,0 +1,315 @@
+//
+//  TextView+BlocksAccessoryView.swift
+//  AnyType
+//
+//  Created by Dmitry Lobanov on 10.12.2019.
+//  Copyright © 2019 AnyType. All rights reserved.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+// We have several views here.
+extension ABC {
+    enum BlockToolbar {}
+}
+
+// MARK: BlockToolbar
+extension ABC.BlockToolbar {
+    class AccessoryView: UIView {
+        typealias Style = ABC.Style
+        // MARK: Variables
+        var style: Style = .default
+        var model: ViewModel = .init()
+        var userResponse: AnyCancellable?
+
+        // MARK: Updates
+        func update(state: State) {
+            func button(for state: State) -> UIButton? {
+                switch state {
+                case .unknown: return nil
+                case .addBlock: return self.addBlockButton
+                case .turnIntoBlock: return self.turnIntoBlockButton
+                case .changeColor: return self.changeColorButton
+                case .editBlock: return self.editActionsButton
+                }
+            }
+
+            let selectedButton = button(for: state)
+            let otherButtons = [addBlockButton, turnIntoBlockButton, changeColorButton, editActionsButton].filter { $0 != selectedButton }
+
+            UIView.animate(withDuration: 0.3) {
+                for button in otherButtons {
+                    button?.backgroundColor = self.style.normalColor()
+                }
+                selectedButton?.backgroundColor = self.style.highlightedColor()
+            }
+        }
+
+        // MARK: Initialization
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            self.setup()
+        }
+
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            self.setup()
+        }
+
+        // MARK: Actions
+        @objc func processAddBlock() {
+            process(.addBlock)
+        }
+
+        @objc func processTurnIntoBlock() {
+            process(.turnIntoBlock)
+        }
+
+        @objc func processChangeColor() {
+            process(.changeColor)
+        }
+
+        @objc func processEditActions() {
+            process(.editBlock)
+        }
+        
+        @objc func processDismissKeyboard() {
+            process(.keyboardDismiss)
+        }
+
+        func process(_ action: Action) {
+            self.model.process(action)
+        }
+
+        // MARK: Public API Configurations
+        // something that we should put in public api.
+
+        func setupCustomization() {
+            self.backgroundColor = self.style.backgroundColor()
+            for button in [addBlockButton, turnIntoBlockButton, changeColorButton, editActionsButton, dismissKeyboardButton] {
+                button?.tintColor = self.style.normalColor()
+            }
+
+            self.addBlockButton.addTarget(self, action: #selector(Self.processAddBlock), for: .touchUpInside)
+            self.turnIntoBlockButton.addTarget(self, action: #selector(Self.processTurnIntoBlock), for: .touchUpInside)
+            self.changeColorButton.addTarget(self, action: #selector(Self.processChangeColor), for: .touchUpInside)
+            self.editActionsButton.addTarget(self, action: #selector(Self.processEditActions), for: .touchUpInside)
+            self.dismissKeyboardButton.addTarget(self, action: #selector(Self.processDismissKeyboard), for: .touchUpInside)
+        }
+
+        func setupInteraction() {
+            self.userResponse = self.model.$userResponse.dropFirst().sink { (state) in
+                print("Value! \(state)")
+                self.update(state: state)
+            }
+        }
+
+        // MARK: Customization
+        var insets: UIEdgeInsets = .init(top: 10, left: 10, bottom: 10, right: 10)
+
+        // MARK: Setup
+        func setup() {
+            self.setupUIElements()
+            self.addLayout()
+            self.setupCustomization()
+            self.setupInteraction()
+        }
+
+        // MARK: UI Elements
+        var addBlockButton: UIButton!
+        var turnIntoBlockButton: UIButton!
+        var leftStackView: UIStackView!
+
+        var changeColorButton: UIButton!
+        var editActionsButton: UIButton!
+        var dismissKeyboardButton: UIButton!
+        var rightStackView: UIStackView!
+
+        var contentView: UIView!
+
+        // MARK: Setup UI Elements
+        func setupUIElements() {
+
+            // UIView
+
+            self.translatesAutoresizingMaskIntoConstraints = false
+
+            self.addBlockButton = { () -> UIButton in
+                let view = UIButton(type: .system)
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.setImage(UIImage(named: "TextEditor/Toolbar/Blocks/AddBlock"), for: .normal)
+                return view
+            }()
+
+            self.turnIntoBlockButton = { () -> UIButton in
+                let view = UIButton(type: .system)
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.setImage(UIImage(named: "TextEditor/Toolbar/Blocks/TurnIntoBlock"), for: .normal)
+                return view
+            }()
+
+            self.leftStackView = { () -> UIStackView in
+                let view = UIStackView()
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.axis = .horizontal
+                view.distribution = .fillEqually
+                return view
+            }()
+            
+            self.changeColorButton = { () -> UIButton in
+                let view = UIButton(type: .system)
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.setImage(UIImage(named: "TextEditor/Toolbar/Blocks/ChangeColor"), for: .normal)
+                return view
+            }()
+
+            self.editActionsButton = { () -> UIButton in
+                let view = UIButton(type: .system)
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.setImage(UIImage(named: "TextEditor/Toolbar/Blocks/EditActions"), for: .normal)
+                return view
+            }()
+            
+            self.dismissKeyboardButton = { () -> UIButton in
+                let view = UIButton(type: .system)
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.setImage(UIImage(named: "TextEditor/Toolbar/General/Keyboard"), for: .normal)
+                return view
+            }()
+            
+            self.rightStackView = {  () -> UIStackView in
+                let view = UIStackView()
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.axis = .horizontal
+                view.distribution = .fillEqually
+                return view
+            }()
+
+            self.leftStackView.addArrangedSubview(self.addBlockButton)
+            self.leftStackView.addArrangedSubview(self.turnIntoBlockButton)
+            
+            self.rightStackView.addArrangedSubview(self.changeColorButton)
+            self.rightStackView.addArrangedSubview(self.editActionsButton)
+            self.rightStackView.addArrangedSubview(self.dismissKeyboardButton)
+
+            self.contentView = { () -> UIView in
+                let view = UIView()
+                view.translatesAutoresizingMaskIntoConstraints = false
+                return view
+            }()
+
+            self.contentView.addSubview(self.leftStackView)
+            self.contentView.addSubview(self.rightStackView)
+            self.addSubview(contentView)
+        }
+
+        // MARK: Layout
+        func addLayout() {
+            if let view = self.contentView, let superview = view.superview {
+                view.leftAnchor.constraint(equalTo: superview.leftAnchor).isActive = true
+                view.rightAnchor.constraint(equalTo: superview.rightAnchor).isActive = true
+                view.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+                view.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+            }
+            if let view = self.leftStackView, let superview = view.superview {
+                view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: 16.0).isActive = true
+                view.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+                view.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+//                view.rightAnchor.constraint(greaterThanOrEqualTo: rightView.leftAnchor, constant: -10.0).isActive = true
+                view.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: 0.20, constant: 0.0).isActive = true
+            }
+            if let view = self.rightStackView, let superview = view.superview {
+                view.trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -16.0).isActive = true
+                view.topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+                view.bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+                view.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: 0.30, constant: 0.0).isActive = true
+            }
+        }
+
+        override var intrinsicContentSize: CGSize {
+            var size = self.addBlockButton.intrinsicContentSize
+            size.width += self.insets.left + self.insets.right
+            size.height += self.insets.top + self.insets.bottom
+            return size
+        }
+    }
+}
+
+// MARK: ViewModel
+extension ABC.BlockToolbar {
+    // MARK: Action
+    enum Action {
+        case unknown
+        case addBlock
+        case turnIntoBlock
+        case changeColor
+        case editBlock
+        case keyboardDismiss
+    }
+    // MARK: State
+    enum State {
+        case unknown
+        case addBlock
+        case turnIntoBlock
+        case changeColor
+        case editBlock
+    }
+    struct UserAction {
+        var action: Action = .unknown
+        var view: UIView?
+        static var zero = Self.init()
+    }
+    
+    // MARK: ViewModel
+    class ViewModel: NSObject, ObservableObject {
+        // MARK: Initialization
+        override init() {
+            self.turnIntoBlockViewModel = .init()
+            self.addBlockViewModel = .init()
+            self.changeColorViewModel = .init()
+            self.editActionsViewModel = .init()
+            super.init()
+            self.setup()
+        }
+
+        // MARK: Setup
+        func setup() {
+            self.addBlockViewModelDidChanged = (self.addBlockViewModel.value).sink { (value) in
+                print("AddBlock Model!: \(String(describing: value.0)) \(String(describing: value.1)) \(value.2.types)")
+            }
+            self.changeColorViewModelDidChangeColor = (self.changeColorViewModel.$value).sink { (value) in
+                let textColor = value.textColor
+                let backgroundColor = value.backgroundColor
+                print("TextColor! \(String(describing: textColor)) \n BackgroundColor! \(String(describing: backgroundColor))")
+            }
+        }
+
+        // MARK: Publishers
+        @Published fileprivate var userResponse: State = .unknown
+        @Published var userAction: UserAction = .zero
+
+        // MARK: Streams
+        var changeColorViewModelDidChangeColor: AnyCancellable?
+        var addBlockViewModelDidChanged: AnyCancellable?
+
+        // MARK: ViewModels
+        @ObservedObject var turnIntoBlockViewModel: TurnIntoBlock.ViewModel
+        @ObservedObject var addBlockViewModel: AddBlock.ViewModel
+        @ObservedObject var changeColorViewModel: ChangeColor.ViewModel
+        @ObservedObject var editActionsViewModel: EditActions.ViewModel
+
+        // MARK: Private Setters
+        fileprivate func process(_ action: Action) {
+            switch action {
+            case .unknown: return
+            case .addBlock: self.userAction = .init(action: action, view: AddBlock.InputViewBuilder.createView(self._addBlockViewModel))
+            case .turnIntoBlock: self.userAction = .init(action: action, view: TurnIntoBlock.InputViewBuilder.createView(self._turnIntoBlockViewModel))
+            case .changeColor: self.userAction = .init(action: action, view: ChangeColor.InputViewBuilder.createView(self._changeColorViewModel))
+            case .editBlock: self.userAction = .init(action: action, view: EditActions.InputViewBuilder.createView(self._editActionsViewModel))
+            case .keyboardDismiss: ABC.KeyboardHandler.shared.dismiss()
+            }
+        }
+    }
+}
---
+++ AnyType/Sources/PresentationLayer/Common Views/TextView/TextView+MarkStyle.swift
@@ -0,0 +1,314 @@
+//
+//  TextView+MarkStyle.swift
+//  AnyType
+//
+//  Created by Dmitry Lobanov on 25.11.2019.
+//  Copyright © 2019 AnyType. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+typealias ABC = TextView
+
+// MARK: Conversion
+extension ABC {
+    enum Convertor {
+        static func color(from color: Color?) -> UIColor? {
+            return color.flatMap { (color) -> UIColor? in
+                nil
+            }
+        }
+        static func font(from font: Font?) -> UIFont? {
+            return font.flatMap { (font) -> UIFont? in
+                nil
+            }
+        }
+        static func range(from range: Range<Int>) -> NSRange {
+            return NSRange(location: range.lowerBound, length: range.count)
+        }
+    }
+}
+
+// MARK: MarkStyle
+extension ABC {
+    enum MarkStyle: Equatable, CaseIterable {
+        static var allCases: [MarkStyle] {
+            return [
+                .bold(false),
+                .italic(false),
+                .keyboard(false),
+                .strikethrough(false),
+                .underscored(false),
+                .textColor(nil),
+                .backgroundColor(nil),
+                .link(nil)
+            ]
+        }
+        
+        // change font to bold
+        // turn on, turn off
+        // NSAttributedString.Key.font
+        case bold(Bool = true)
+        
+        // change font to italic
+        // turn on, turn off
+        // NSAttributedString.Key.font
+        case italic(Bool = true)
+        
+        // change font to ???
+        // change paragraphStyle to .keyboard
+        // turn on, turn off
+        // NSAttributedString.Key.font
+        // NSAttributedString.Key.paragraphStyle
+        case keyboard(Bool = true)
+        
+        // style also?
+        // turn on, turn off
+        // NSAttributedString.Key.strikethroughStyle
+        case strikethrough(Bool = true)
+        
+        // style also?
+        // turn on, turn off
+        // NSAttributedString.Key.underlineStyle
+        case underscored(Bool = true)
+        
+        
+        // choose colors
+        // NSAttributedString.Key.foregroundColor
+        case textColor(UIColor?)
+        
+        // choose colors
+        // NSAttributedString.Key.backgroundColor
+        case backgroundColor(UIColor?)
+        
+        // NOTE: It has DIFFERENT attributes.
+        // Actually, .linkAttributes in UITextView.
+        // set link
+        // NSAttributedString.Key.link
+        case link(URL?)
+        
+        // MARK: Opposite
+        func opposite() -> Self {
+            switch self {
+            case let .bold(value): return
+                .bold(!value)
+            case let .italic(value): return
+                .italic(!value)
+            case let .keyboard(value): return
+                .keyboard(!value)
+            case let .strikethrough(value): return
+                .strikethrough(!value)
+            case let .underscored(value): return
+                .underscored(!value)
+                
+            case let .textColor(value): return
+                .textColor(value) // or nil maybe?
+            case let .backgroundColor(value): return
+                .backgroundColor(value) // or nil maybe?
+            case let .link(value): return
+                .link(value) // or nil maybe?
+            }
+        }
+        
+        // MARK: Conversion
+        func from(attributes: [NSAttributedString.Key : Any]) -> Self? {
+            switch self {
+            case .bold: return .bold( (attributes[.font] as? UIFont)?.fontDescriptor.symbolicTraits.contains(.traitBold) ?? false )
+            case .italic: return .italic( (attributes[.font] as? UIFont)?.fontDescriptor.symbolicTraits.contains(.traitItalic) ?? false )
+            case .keyboard: return .keyboard(
+                //                (attributes[.font] as? UIFont)
+//                (attributes[.paragraphStyle] as? NSParagraphStyle) == NSParagraphStyle.keyboardStyle &&
+                (attributes[.font] as? UIFont)?.fontDescriptor.symbolicTraits.contains(.traitMonoSpace) ?? false
+                )
+            case .strikethrough: return .strikethrough( (attributes[.strikethroughStyle] as? Int) == 1 )
+            case .underscored: return .underscored( (attributes[.underlineStyle] as? Int) == 1 )
+            case .textColor: return .textColor( attributes[.foregroundColor] as? UIColor )
+            case .backgroundColor: return .backgroundColor( attributes[.backgroundColor] as? UIColor )
+            case .link: return .link( attributes[.link] as? URL )
+            }
+        }
+        
+        // TODO: rethink.
+        // Should we make option set here?
+        static func from(attributes: [NSAttributedString.Key : Any]) -> [Self] {
+            return allCases.compactMap{$0.from(attributes: attributes)}
+        }
+        
+        // CAUTION:
+        // This method return ONLY SLIDES of correspoding attributes and can return empty dictionary.
+        func to(old: [NSAttributedString.Key : Any]) -> [NSAttributedString.Key : Any] {
+            switch self {
+            case let .bold(value):
+                if let font = old[.font] as? UIFont {
+                    let oldTraits = font.fontDescriptor.symbolicTraits
+                    let traits = value ? oldTraits.union(.traitBold) : oldTraits.symmetricDifference(.traitBold)
+                    if let newDescriptor = font.fontDescriptor.withSymbolicTraits(traits) {
+                        let newFont: UIFont = .init(descriptor: newDescriptor, size: font.pointSize)
+                        return [.font : newFont]
+                    }
+                    return [:]
+                }
+                return [:]
+            case let .italic(value):
+                if let font = old[.font] as? UIFont {
+                    let oldTraits = font.fontDescriptor.symbolicTraits
+                    let traits = value ? oldTraits.union(.traitItalic) : oldTraits.symmetricDifference(.traitItalic)
+                    if let newDescriptor = font.fontDescriptor.withSymbolicTraits(traits) {
+                        let newFont: UIFont = .init(descriptor: newDescriptor, size: font.pointSize)
+                        return [.font : newFont]
+                    }
+                    return [:]
+                }
+                return [:]
+            case let .keyboard(value):
+                print("value: \(value)")
+                var result: [NSAttributedString.Key : Any] = [:]
+                
+                if let font = old[.font] as? UIFont {
+                    let oldTraits = font.fontDescriptor.symbolicTraits
+                    let traits = value ? oldTraits.union(.traitMonoSpace) : oldTraits.symmetricDifference(.traitMonoSpace)
+                    // TODO: Simplify
+                    // AAAAA
+                    // It could be done as italic or bold, but it can't.
+                    // .traitMonoSpace change font family, so, we should extract traits and apply them to
+                    // value  - current font
+                    // !value - preferred body font
+                    if let newDescriptor = (value ? font : .preferredFont(forTextStyle: .body)).fontDescriptor.withSymbolicTraits(traits) {
+                        let newFont: UIFont = .init(descriptor: newDescriptor, size: font.pointSize)
+                        print("font: \(font) \n vs \n \(newFont)")
+                        result.merge([.font : newFont], uniquingKeysWith: {(lhs,rhs) in rhs})
+                    }
+                }
+                
+                // we don't care about paragraphStyle, cause we set custom paragraph style IF value is true.
+                // otherwise, we set it to default or nil (?)
+//                if value {
+//                    result.merge([.paragraphStyle: NSParagraphStyle.keyboardStyle], uniquingKeysWith: {(lhs, rhs) in rhs})
+//                }
+//                else {
+//                    // or set it to default
+//                    _ = result.removeValue(forKey: .paragraphStyle)
+//                }
+                
+                return result
+            case let .strikethrough(value): return [ .strikethroughStyle : value ? 1 : 0 ]
+            case let .underscored(value): return [ .underlineStyle : value ? 1 : 0 ]
+            case let .textColor(value): return [ .foregroundColor : value as Any ]
+            case let .backgroundColor(value): return [ .backgroundColor : value as Any ]
+            case let .link(value): return [ .link : value as Any ]
+            }
+        }
+    }
+}
+
+// MARK: KeyboardStyle
+// TODO: Add style.
+/*
+ kbd { display: inline; font-family: 'Mono'; line-height: 1.71; background: rgba(247,245,240,0.5); padding: 2px 4px; border-radius: 2px; }
+ */
+extension NSParagraphStyle {
+    private class KeyboardStyle: NSParagraphStyle {
+        override var headIndent: CGFloat {
+            return 10.0
+        }
+        override var tailIndent: CGFloat {
+            return 10.0
+        }
+    }
+    fileprivate static var keyboardStyle: NSParagraphStyle = {
+        return KeyboardStyle()
+    }()
+}
+
+// MARK: Range Either
+extension ABC {
+    enum Either<Left, Right> {
+        case left(Left)
+        case right(Right)
+    }
+    enum RangedEither<Left, Right> {
+        case range(Left)
+        case whole(Right)
+    }
+    typealias RangeEither = RangedEither<NSRange, Bool>
+}
+
+// MARK: MarkStyleModifier
+extension ABC {
+    class MarkStyleModifier {
+        func charactersCount() -> Int {
+            return self.attributedString.length
+        }
+        var attributedString: NSMutableAttributedString = .init()
+        init() {}
+        init(attributedText: NSMutableAttributedString) {
+            attributedString = attributedText
+        }
+        init(text: String) {
+            attributedString = NSMutableAttributedString(string: text)
+            attributedString.setAttributes([.font : UIFont.preferredFont(forTextStyle: .body)], range: NSRange(location: 0, length: attributedString.length))
+        }
+    }
+}
+
+// MARK: MarkStyleModifier / Apply Styles
+extension ABC.MarkStyleModifier {
+    typealias Convertor = ABC.Convertor
+    typealias MarkStyle = ABC.MarkStyle
+    typealias RangeEither = ABC.RangeEither
+    // MARK: Attributes
+    private func getAttributes(at range: NSRange) -> [NSAttributedString.Key : Any] {
+        return attributedString.attributes(at: range.lowerBound, longestEffectiveRange: nil, in: range)
+    }
+    
+    private func mergeAttributes(origin: [NSAttributedString.Key : Any], changes: [NSAttributedString.Key : Any]) -> [NSAttributedString.Key : Any] {
+        var result = origin
+        result.merge(changes) { (source, target) in target }
+        return result
+    }
+    
+    // MARK: Applying style
+    private func applyStyle(style: MarkStyle, range: NSRange) -> Self {
+        let oldAttributes = getAttributes(at: range)
+        let changedAttributes = style.to(old: oldAttributes)
+        let newAttributes = self.mergeAttributes(origin: oldAttributes, changes: changedAttributes)
+        attributedString.addAttributes(newAttributes, range: range)
+        // and send event about it?
+        // that attributes at range are changed.
+        // also, on selection we should fire event about attributes.
+        return self
+    }
+    
+    public func applyStyle(style: MarkStyle, rangeOrWholeString either: RangeEither) -> Self {
+        switch either {
+        case let .range(value): return applyStyle(style: style, range: value)
+        case let .whole(value): return value ? applyStyle(style: style, range: NSRange(location: 0, length: charactersCount())) : self
+        }
+    }
+    
+    // MARK: Get Mark Styles
+    private func getMarkStyles(at range: NSRange) -> [MarkStyle] {
+        TextView.MarkStyle.from(attributes: getAttributes(at: range))
+    }
+    
+    public func getMarkStyles(at either: RangeEither) -> [MarkStyle] {
+        switch either {
+        case let .range(value): return getMarkStyles(at: value)
+        case let .whole(value): return value ? getMarkStyles(at: NSRange(location: 0, length: charactersCount())) : []
+        }
+    }
+
+    // MARK: Get specific Mark Style
+    private func getMarkStyle(style: MarkStyle, at range: NSRange) -> MarkStyle? {
+        style.from(attributes: getAttributes(at: range))
+    }
+    
+    public func getMarkStyle(style: MarkStyle, at either: RangeEither) -> MarkStyle? {
+        switch either {
+        case let .range(value): return getMarkStyle(style: style, at: value)
+        case let .whole(value): return value ? getMarkStyle(style: style, at: NSRange(location: 0, length: charactersCount())) : nil
+        }
+    }
+}
