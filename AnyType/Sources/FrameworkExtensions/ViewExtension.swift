566c16cb624a6825e0b9464db7ff98b77f796e92
Dmitry Lobanov <gaussblurinc@gmail.com>
2019-12-18 18:19:20 +0300

    extensions: swiftUI view bounds accessors have been added.

--- AnyType/Sources/FrameworkExtensions/ViewExtension.swift
+++ AnyType/Sources/FrameworkExtensions/ViewExtension.swift
@@ -10,16 +10,62 @@ import SwiftUI
 
 
 extension View {
-    
     func errorToast(isShowing: Binding<Bool>, errorText: String) -> some View {
         ErrorAlertView(isShowing: isShowing, errorText: errorText, presenting: self)
     }
-    
     func renderedImage(size: CGSize = CGSize(width: 320, height: 160)) -> UIImage? {
         let sceneDeleage = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
         let rect = CGRect(origin: .zero, size: size)
         let image = sceneDeleage?.window?.rootViewController?.view.renderedImage(rect: rect)
         
         return image
+    }
+}
+
+// MARK: Bounds accessors
+extension View {
+    public func saveBounds(viewId: Int, coordinateSpace: CoordinateSpace = .global) -> some View {
+        background(GeometryReader { proxy in
+            Color.clear.preference(key: SaveBoundsPrefKey.self, value: [SaveBoundsPrefData(viewId: viewId, bounds: proxy.frame(in: coordinateSpace))])
+        })
+    }
+    
+    public func retrieveBounds(viewId: Int, _ rect: Binding<CGRect>) -> some View {
+        onPreferenceChange(SaveBoundsPrefKey.self) { preferences in
+            DispatchQueue.main.async {
+                // The async is used to prevent a possible blocking loop,
+                // due to the child and the ancestor modifying each other.
+                let p = preferences.first(where: { $0.viewId == viewId })
+                rect.wrappedValue = p?.bounds ?? .zero
+            }
+        }
+    }
+    
+    public func retrieveBounds(viewId: Int, onRect: @escaping (CGRect) -> () ) -> some View {
+        onPreferenceChange(SaveBoundsPrefKey.self) { preferences in
+            DispatchQueue.main.async {
+                // The async is used to prevent a possible blocking loop,
+                // due to the child and the ancestor modifying each other.
+                let p = preferences.first(where: { $0.viewId == viewId })
+                onRect(p?.bounds ?? .zero)
+            }
+        }
+    }
+    
+    public func outputBounds(viewId: Int) -> some View {
+        saveBounds(viewId: 0).retrieveBounds(viewId: viewId, onRect: {print($0)})
+    }
+}
+
+private struct SaveBoundsPrefData: Equatable {
+    let viewId: Int
+    let bounds: CGRect
+}
+
+private struct SaveBoundsPrefKey: PreferenceKey {
+    static var defaultValue: [SaveBoundsPrefData] = []
+    
+    static func reduce(value: inout [SaveBoundsPrefData], nextValue: () -> [SaveBoundsPrefData]) {
+        value.append(contentsOf: nextValue())
     }
 }
