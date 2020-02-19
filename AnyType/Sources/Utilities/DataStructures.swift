d2459d1427dceabc70b7d6209c10d7ca76ff4c9a
Dmitry Lobanov <gaussblurinc@gmail.com>
2020-02-19 14:42:28 +0300

    sutilities: data structures custom structures have been added.

---
+++ AnyType/Sources/Utilities/DataStructures.swift
@@ -0,0 +1,11 @@
+//
+//  DataStructures.swift
+//  AnyType
+//
+//  Created by Dmitry Lobanov on 22.02.2020.
+//  Copyright © 2020 AnyType. All rights reserved.
+//
+
+import Foundation
+
+enum DataStructures {}
