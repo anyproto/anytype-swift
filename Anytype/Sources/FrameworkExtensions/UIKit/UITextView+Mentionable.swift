0e94ea2fca84d7e9eb8c054ccd55c04a73513080
Alex Kovalev <alex.bender.kov8@gmail.com>
2021-06-24 17:30:27 +0300

    Fixed typing attributes erasing after setting empty attributed string to UITextView (#777)

--- Anytype/Sources/FrameworkExtensions/UIKit/UITextView+Mentionable.swift
+++ Anytype/Sources/FrameworkExtensions/UIKit/UITextView+Mentionable.swift
@@ -2,6 +2,10 @@ import UIKit
 
 extension UITextView: Mentionable {
     
+    private enum Constants {
+        static let attachmentLenght = 1
+    }
+    
     func removeMentionIfNeeded(replacementRange: NSRange, replacementText: String) {
         guard replacementText == "" else { return }
         let mentionSearchRange = NSRange(location: 0, length: selectedRange.location)
@@ -12,7 +16,20 @@ extension UITextView: Mentionable {
             let mutableAttributedString = NSMutableAttributedString(attributedString: attributedText)
             removeMentionInteractionButton(from: subrange)
             mutableAttributedString.deleteCharacters(in: subrange)
-            attributedText = mutableAttributedString
+            if subrange.location > 0 {
+                let mentionAttachmentRange = NSRange(location: subrange.location - Constants.attachmentLenght,
+                                                     length: Constants.attachmentLenght)
+                removeMentionInteractionButton(from: mentionAttachmentRange)
+                mutableAttributedString.deleteCharacters(in: mentionAttachmentRange)
+            }
+            // If we will set empty attributed string it will erase all typing attributes
+            if mutableAttributedString.length == 0 {
+                let oldTypingAttributes = typingAttributes
+                attributedText = mutableAttributedString
+                typingAttributes = oldTypingAttributes
+            } else {
+                attributedText = mutableAttributedString
+            }
             shouldStop[0] = true
         }
     }
