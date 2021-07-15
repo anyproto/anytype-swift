1c5bb7af8ff84dd30da64f2b9e4939815e14b529
Konstantin Mordan <mordan2be@gmail.com>
2021-07-15 14:05:47 +0300

    Add details service

---
+++ Anytype/Sources/ServiceLayer/Details/DetailsService.swift
@@ -0,0 +1,42 @@
+//
+//  DetailsService.swift
+//  Anytype
+//
+//  Created by Konstantin Mordan on 15.07.2021.
+//  Copyright © 2021 Anytype. All rights reserved.
+//
+
+import Foundation
+import BlocksModels
+import Combine
+
+final class DetailsService {
+    
+    private let service = ObjectActionsService()
+    private let eventHandler: EventHandler
+
+    private let objectId: String
+    
+    private var subscriptions: [AnyCancellable] = []
+    
+    init(eventHandler: EventHandler, objectId: String) {
+        self.eventHandler = eventHandler
+        self.objectId = objectId
+    }
+    
+    func update(details: [DetailsKind: DetailsEntry<AnyHashable>]) {
+        service.setDetails(
+            contextID: objectId,
+            details: details
+        )
+        .sinkWithDefaultCompletion("setDetails combletion") { [weak self] success in
+            self?.eventHandler.handle(
+                events: PackOfEvents(
+                    contextId: success.contextID,
+                    events: success.messages
+                )
+            )
+        }
+        .store(in: &subscriptions)
+    }
+}
