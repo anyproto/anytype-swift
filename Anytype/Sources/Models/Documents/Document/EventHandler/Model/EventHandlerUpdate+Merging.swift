bfc962df081f4923d13858f2072e8b07ab151646
Vladimir Ignatov <abbadoh@mail.ru>
2021-07-16 13:24:04 +0300

    New merging algorithm

---
+++ Anytype/Sources/Models/Documents/Document/EventHandler/Model/EventHandlerUpdate+Merging.swift
@@ -0,0 +1,33 @@
+import BlocksModels
+
+extension Array where Element == EventHandlerUpdate {
+    var merged: [EventHandlerUpdate] {
+        guard !hasGeneralUpdate else {
+            return [.general]
+        }
+        
+        var updateIds = Set<BlockId>()
+        var details: DetailsData? = nil
+        
+        for update in self {
+            switch update {
+            case let .update(blockIds: blockIds):
+                blockIds.forEach { updateIds.insert($0) }
+            case let .details(detailsData):
+                details = detailsData
+            case .general:
+                assertionFailure("No general events soppose to be in mergedUpdates")
+            }
+        }
+        
+        if let details = details {
+            return [.update(blockIds: updateIds), .details(details)]
+        }
+        
+        return [.update(blockIds: updateIds)]
+    }
+    
+    var hasGeneralUpdate: Bool {
+        contains(where: { .general == $0 })
+    }
+}
---
+++ Anytype/Sources/Models/Documents/Document/EventHandler/Model/EventHandlerUpdate.swift
@@ -0,0 +1,19 @@
+import BlocksModels
+
+enum EventHandlerUpdate: Hashable {
+    case general
+    case update(blockIds: Set<BlockId>)
+    
+    case details(DetailsData)
+
+    var hasUpdate: Bool {
+        switch self {
+        case .general:
+            return true
+        case .details:
+            return true
+        case let .update(blockIds):
+            return !blockIds.isEmpty
+        }
+    }
+}
