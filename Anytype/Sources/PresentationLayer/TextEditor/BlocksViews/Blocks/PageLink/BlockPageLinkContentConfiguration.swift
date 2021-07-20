a33272df8cc65f89ab1db91a30439e78463448b5
Vladimir Ignatov <abbadoh@mail.ru>
2021-07-20 12:50:17 +0300

    block link first iteration

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/PageLink/BlockPageLinkContentConfiguration.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/PageLink/BlockPageLinkContentConfiguration.swift
@@ -2,33 +2,15 @@ import UIKit
 import BlocksModels
 
 
-struct BlockPageLinkContentConfiguration: UIContentConfiguration, Hashable {
-    var information: BlockInformation
-    weak var viewModel: BlockPageLinkViewModel?
+struct BlockPageLinkContentConfiguration: UIContentConfiguration, Hashable, Equatable {
+    let content: BlockLink
+    let state: BlockPageLinkState
     
-    init?(_ information: BlockInformation) {
-        switch information.content {
-        case let .link(value) where value.style == .page:
-            self.information = information
-        default:
-            return nil
-        }
-    }
-
     func makeContentView() -> UIView & UIContentView {
-        let view = BlockPageLinkContentView(configuration: self)
-        return view
+        return BlockPageLinkContentView(configuration: self)
     }
 
     func updated(for state: UIConfigurationState) -> BlockPageLinkContentConfiguration {
         return self
-    }
-
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.information == rhs.information
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(self.information)
     }
 }
