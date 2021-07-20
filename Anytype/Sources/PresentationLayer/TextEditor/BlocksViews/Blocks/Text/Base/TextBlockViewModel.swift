0f82aea9a46ee50e7a2919fd77059ababc79b57a
Konstantin Mordan <mordan2be@gmail.com>
2021-07-20 14:13:11 +0300

    Fixed title update after updating layout

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Text/Base/TextBlockViewModel.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Text/Base/TextBlockViewModel.swift
@@ -24,6 +24,7 @@ struct TextBlockViewModel: BlockViewModelProtocol {
         [blockId,
          indentationLevel,
          toggled,
+         isCheckable,
          block.blockModel.information.content
         ] as [AnyHashable]
     }
