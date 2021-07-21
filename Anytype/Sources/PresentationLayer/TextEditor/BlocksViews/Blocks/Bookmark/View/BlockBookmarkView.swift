362d9d74700eaeb1c5c325be6ef9c14c7f353cd9
Vladimir Ignatov <abbadoh@mail.ru>
2021-07-21 22:20:06 +0300

    Move bookmark information to the separate view

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Bookmark/View/BlockBookmarkView.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Bookmark/View/BlockBookmarkView.swift
@@ -3,16 +3,7 @@ import UIKit
 import BlocksModels
     
 final class BlockBookmarkView: UIView {
-    /// Views
-    private var contentView = UIView()
-    private var titleView: UILabel!
-    private var descriptionView: UILabel!
-    private var iconView: UIImageView!
-    private var urlView: UILabel!
-    
-    private var leftStackView: UIStackView!
-    private var urlStackView: UIStackView!
-    
+    private var informationView = BlockBookmarkInfoView()
     private var imageView: UIImageView!
     
     private var imageViewWidthConstraint: NSLayoutConstraint?
@@ -33,56 +24,6 @@ final class BlockBookmarkView: UIView {
     func setupUIElements() {
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        self.leftStackView = {
-            let view = UIStackView()
-            view.axis = .vertical
-            view.translatesAutoresizingMaskIntoConstraints = false
-            return view
-        }()
-        
-        self.urlStackView = {
-            let view = UIStackView()
-            view.axis = .horizontal
-            view.translatesAutoresizingMaskIntoConstraints = false
-            return view
-        }()
-
-        self.titleView = {
-            let view = UILabel()
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.font = UIFont.captionMedium
-            view.textColor = .grayscale90
-            return view
-        }()
-        
-        self.descriptionView = {
-            let view = UILabel()
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.numberOfLines = 3
-            view.lineBreakMode = .byWordWrapping
-            view.font = .caption
-            view.textColor = .grayscale70
-            return view
-        }()
-        
-        self.iconView = {
-            let view = UIImageView()
-            view.contentMode = .scaleAspectFit
-            view.clipsToBounds = true
-            view.heightAnchor.constraint(equalToConstant: Layout.iconHeight).isActive = true
-            view.widthAnchor.constraint(equalToConstant: Layout.iconHeight).isActive = true
-            view.translatesAutoresizingMaskIntoConstraints = false
-            return view
-        }()
-
-        self.urlView = {
-            let view = UILabel()
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.font = .caption
-            view.textColor = .grayscale90
-            return view
-        }()
-        
         self.imageView = {
             let view = UIImageView()
             view.contentMode = .scaleAspectFit
@@ -91,45 +32,32 @@ final class BlockBookmarkView: UIView {
             return view
         }()
         
-        urlStackView.addArrangedSubview(iconView)
-        urlStackView.addArrangedSubview(urlView)
         
-        leftStackView.addArrangedSubview(titleView)
-        leftStackView.addArrangedSubview(descriptionView)
-        leftStackView.addArrangedSubview(urlStackView)
+        addSubview(informationView)
+        addSubview(imageView)
         
-        contentView.addSubview(leftStackView)
-        contentView.addSubview(imageView)
-        
-        addSubview(contentView)
-        
-        leftStackView.backgroundColor = .white
         imageView.backgroundColor = .white
     }
     
     /// Layout
     func addLayout() {
-        contentView.edgesToSuperview()
-
-        if let view = leftStackView, let superview = view.superview {
-            NSLayoutConstraint.activate([
-                view.leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: Layout.commonInsets.left),
-                view.trailingAnchor.constraint(lessThanOrEqualTo: superview.trailingAnchor, constant: -Layout.commonInsets.right),
-                view.topAnchor.constraint(equalTo: superview.topAnchor, constant: Layout.commonInsets.top),
-                view.bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -Layout.commonInsets.bottom)
-            ])
-        }
+        NSLayoutConstraint.activate([
+            informationView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Layout.commonInsets.left),
+            informationView.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -Layout.commonInsets.right),
+            informationView.topAnchor.constraint(equalTo: self.topAnchor, constant: Layout.commonInsets.top),
+            informationView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -Layout.commonInsets.bottom)
+        ])
     }
     
     func addLayoutForImageView() {
-        if let view = self.imageView, let superview = view.superview, let leftView = self.leftStackView {
+        if let view = self.imageView, let superview = view.superview {
             self.imageViewWidthConstraint = view.widthAnchor.constraint(equalTo: superview.widthAnchor, multiplier: Layout.imageSizeFactor)
             self.imageViewWidthConstraint?.isActive = true
             
             self.imageViewHeightConstraint = view.heightAnchor.constraint(equalToConstant: Layout.imageHeightConstant)
             self.imageViewHeightConstraint?.isActive = true
             NSLayoutConstraint.activate([
-                view.leadingAnchor.constraint(equalTo: leftView.trailingAnchor, constant: Layout.commonInsets.left),
+                view.leadingAnchor.constraint(equalTo: informationView.trailingAnchor, constant: Layout.commonInsets.left),
                 view.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
                 view.topAnchor.constraint(greaterThanOrEqualTo: superview.topAnchor),
                 view.bottomAnchor.constraint(lessThanOrEqualTo: superview.bottomAnchor)
@@ -138,7 +66,7 @@ final class BlockBookmarkView: UIView {
     }
     
     func updateIcon(icon: UIImage) {
-        iconView.image = icon
+        informationView.updateIcon(icon: icon)
     }
     
     func updateImage(image: UIImage) {
@@ -147,32 +75,15 @@ final class BlockBookmarkView: UIView {
     
     /// Configurations
     func handle(state: BlockBookmarkState) {
+        informationView.update(state: state)
+        
         switch state {
-        case let .onlyURL(payload):
-            urlView.text = payload.url
-            titleView.isHidden = true
-            descriptionView.isHidden = true
-            urlView.isHidden = false
-            iconView.isHidden = true
-            urlStackView.isHidden = false
         case let .fetched(payload):
-            titleView.text = payload.title
-            descriptionView.text = payload.subtitle
-            urlView.text = payload.url
             if payload.hasImage {
                 addLayoutForImageView()   
             }
-            titleView.isHidden = false
-            descriptionView.isHidden = false
-            urlView.isHidden = false
-            iconView.isHidden = self.iconView.image == nil
-            urlStackView.isHidden = false
         default:
-            titleView.isHidden = true
-            descriptionView.isHidden = true
-            urlView.isHidden = true
-            iconView.isHidden = true
-            urlStackView.isHidden = true
+            break
         }
     }
 }
@@ -183,7 +94,6 @@ private extension BlockBookmarkView {
         static let spacing: CGFloat = 5
         static let imageSizeFactor: CGFloat = 1 / 3
         static let imageHeightConstant: CGFloat = 150
-        static let iconHeight: CGFloat = 24
     }
 }
 
