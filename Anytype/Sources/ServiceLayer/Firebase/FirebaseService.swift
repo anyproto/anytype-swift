cb55b4d53949fdc8d5492eb14f3a4c568338c7d6
Alex <alex.bender.kov8@gmail.com>
2021-07-22 10:48:38 +0300

    Added asset to FierbaseService

--- Anytype/Sources/ServiceLayer/Firebase/FirebaseService.swift
+++ Anytype/Sources/ServiceLayer/Firebase/FirebaseService.swift
@@ -3,11 +3,16 @@ import Firebase
 final class FirebaseService {
     private let settingsDevFile = "GoogleService-Info-Dev"
     private let settingsReleaseFile = "GoogleService-Info-Release"
+    private let devBundleIdentifier = "com.anytypeio.anytype.dev"
     
     func setup() {
         DispatchQueue.main.async {
-            let bundleId = Bundle.main.bundleIdentifier ?? ""
-            let file = bundleId.hasSuffix("dev") ? self.settingsDevFile : self.settingsReleaseFile
+            guard let bundleId = Bundle.main.bundleIdentifier else {
+                assertionFailure("Can not get bundle id")
+                return
+            }
+            let isDev = bundleId == self.devBundleIdentifier
+            let file = isDev ? self.settingsDevFile : self.settingsReleaseFile
             guard let path = Bundle.main.path(forResource: file, ofType: "plist"),
                   let options = FirebaseOptions(contentsOfFile: path),
                   FirebaseApp.app().isNil else {
