135ed68bbcd7d61b661bafe606144725661adc9e
Vladimir Ignatov <abbadoh@mail.ru>
2021-07-24 02:23:16 +0300

    Remove empty state from media block views

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/File/File/View/BlockFileView.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/File/File/View/BlockFileView.swift
@@ -33,53 +33,23 @@ class BlockFileView: UIView & UIContentView {
         translatesAutoresizingMaskIntoConstraints = true
         
         addSubview(fileView)
-        addSubview(emptyView)
         
         heightAnchor.constraint(equalToConstant: 48).isActive = true
-        emptyView.edgesToSuperview(insets: UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20))
         fileView.edgesToSuperview(insets: UIEdgeInsets(top: 9, left: 20, bottom: 9, right: 20))
     }
     
     func handleNewFile(_ file: BlockFile) {
-        switch file.state  {
-        case .empty:
-            emptyView.change(state: .empty)
-
-            fileView.isHidden = true
-            emptyView.isHidden = false
-        case .uploading:
-            emptyView.change(state: .uploading)
-
-            fileView.isHidden = true
-            emptyView.isHidden = false
-        case .error:
-            emptyView.change(state: .error)
-
-            fileView.isHidden = true
-            emptyView.isHidden = false
-        case .done:
-            let data = BlockFileMediaData(
-                size: FileSizeConverter.convert(size: Int(file.metadata.size)),
-                name: file.metadata.name,
-                typeIcon: BlockFileIconBuilder.convert(mime: file.metadata.mime)
-            )
-            fileView.handleNewData(data: data)
-            
-            fileView.isHidden = false
-            emptyView.isHidden = true
-        }
+        assert(file.state == .done, "Wrong state \(file.state) for block file")
+        
+        let data = BlockFileMediaData(
+            size: FileSizeConverter.convert(size: Int(file.metadata.size)),
+            name: file.metadata.name,
+            typeIcon: BlockFileIconBuilder.convert(mime: file.metadata.mime)
+        )
+        fileView.handleNewData(data: data)
     }
     
     // MARK:- Views
     
     private let fileView: BlockFileMediaView = BlockFileMediaView()
-    
-    private let emptyView: BlocksFileEmptyView = {
-        let view = BlocksFileEmptyView(
-            image: UIImage.blockFile.empty.file,
-            text: "Upload a file"
-        )
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
 }
