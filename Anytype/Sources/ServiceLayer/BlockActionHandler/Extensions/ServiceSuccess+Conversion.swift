c5b7ffc0a2bcaf3fc3d73ef3a4d50fbfbf615e4f
Denis Batvinkin <batvos7@gmail.com>
2021-07-30 17:37:41 +0300

    fix enter on title

--- Anytype/Sources/ServiceLayer/BlockActionHandler/Extensions/ServiceSuccess+Conversion.swift
+++ Anytype/Sources/ServiceLayer/BlockActionHandler/Extensions/ServiceSuccess+Conversion.swift
@@ -23,48 +23,4 @@ extension ResponseEvent {
         
         return .init(middlewareEvents: messages, localEvents: localEvents)
     }
-    
-    var splitEvent: PackOfEvents {
-        /// Find added block.
-        let addEntryMessage = messages.first { $0.value == .blockAdd($0.blockAdd) }
-        guard let addedBlock = addEntryMessage?.blockAdd.blocks.first else {
-            anytypeAssertionFailure("blocks.split.afterUpdate can't find added block")
-            return self.defaultEvent
-        }
-
-        /// Find set children ids.
-        let setChildrenMessage = messages.first { $0.value == .blockSetChildrenIds($0.blockSetChildrenIds)}
-        guard let setChildrenEvent = setChildrenMessage?.blockSetChildrenIds else {
-            anytypeAssertionFailure("blocks.split.afterUpdate can't find set children event")
-            return self.defaultEvent
-        }
-
-        let addedBlockId = addedBlock.id
-
-        /// Find a block after added block, because we insert previous block.
-        guard let addedBlockIndex = setChildrenEvent.childrenIds.firstIndex(where: { $0 == addedBlockId }) else {
-            anytypeAssertionFailure("blocks.split.afterUpdate can't find index of added block in children ids.")
-            return self.defaultEvent
-        }
-
-        /// If we are adding as bottom, we don't need to find block after added block.
-        /// Our addedBlockIndex is focused index.
-        let focusedIndex = addedBlockIndex
-        //setChildrenEvent.childrenIds.index(after: addedBlockIndex)
-
-        /// Check that our childrenIds collection indices contains index.
-        guard setChildrenEvent.childrenIds.indices.contains(focusedIndex) else {
-            anytypeAssertionFailure("blocks.split.afterUpdate children ids doesn't contain index of focused block.")
-            return self.defaultEvent
-        }
-
-        let focusedBlockId = setChildrenEvent.childrenIds[focusedIndex]
-
-        return PackOfEvents(
-            middlewareEvents: messages,
-            localEvents: [
-                .setFocus(blockId: focusedBlockId, position: .beginning)
-            ]
-        )
-    }
 }
