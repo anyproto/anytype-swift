f2456d2591f634b40ba86190f3899827293e6b70
Vladimir Ignatov <abbadoh@mail.ru>
2021-07-30 10:16:23 +0300

    refactor DetailsEntryConverter

--- Modules/BlocksModels/Sources/Block/BlockModel/DetailsEntryConverter.swift
+++ Modules/BlocksModels/Sources/Block/BlockModel/DetailsEntryConverter.swift
@@ -9,22 +9,22 @@ private extension LoggerCategory {
 
 public class DetailsEntryConverter {
     public static func convert(details: [String: Google_Protobuf_Value]) -> [DetailsKind: DetailsEntry<AnyHashable>] {
-        var result: [DetailsKind: DetailsEntry<AnyHashable>] = [:]
-        
-        details.forEach { key, value in
-            guard let kind = DetailsKind(rawValue: key) else {
-                // TODO: Add anytypeAssertionFailure for debug when all converters will be added
-                // TASK: https://app.clickup.com/t/h137nr
-                os.Logger.create(.detailsEntryConverter).error("Add converters for this type: \(key)")
-    //                anytypeAssertionFailure("Add converters for this type: \(detail.key)")
-                return
+        Dictionary(
+            uniqueKeysWithValues: details.compactMap { key, value -> (DetailsKind, DetailsEntry<AnyHashable>)? in
+                guard let kind = DetailsKind(rawValue: key) else {
+                    // TODO: Add anytypeAssertionFailure for debug when all converters will be added
+                    // TASK: https://app.clickup.com/t/h137nr
+                    os.Logger.create(.detailsEntryConverter).error("Add converters for this type: \(key)")
+                    return nil
+                }
+                
+                guard let entry = DetailsEntryConverter.convert(value: value, kind: kind) else {
+                    return nil
+                }
+                
+                return (kind, entry)
             }
-            
-            let value = DetailsEntryConverter.convert(value: value, kind: kind)
-            value.flatMap { result[kind] = $0}
-        }
-        
-        return result
+        )
     }
     
     public static func convert(value: Google_Protobuf_Value, kind: DetailsKind) -> DetailsEntry<AnyHashable>? {
