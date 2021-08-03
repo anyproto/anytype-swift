76d9951013a96b8594d8feaefbfec6851487c949
Alex <alex.bender.kov8@gmail.com>
2021-08-03 18:39:05 +0300

    Removed unused method

--- Anytype/Sources/PresentationLayer/Common/UIKit/BottomSheets/TextAttributesView/Markup/MarkupState.swift
+++ Anytype/Sources/PresentationLayer/Common/UIKit/BottomSheets/TextAttributesView/Markup/MarkupState.swift
@@ -3,13 +3,4 @@ enum MarkupState: CaseIterable {
     case disabled
     case applied
     case notApplied
-    
-    mutating func toggleAppliedState() {
-        guard self != .disabled else { return }
-        if self == .applied {
-            self = .notApplied
-        } else {
-            self = .applied
-        }
-    }
 }
