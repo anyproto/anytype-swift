e258887ce6e1a30ffd247089bf331a04bc523a5e
Denis Batvinkin <batvos7@gmail.com>
2021-08-11 11:48:16 +0300

    update middle to 0.15.32

--- Modules/ProtobufMessages/Sources/commands.pb.swift
+++ Modules/ProtobufMessages/Sources/commands.pb.swift
@@ -14088,6 +14088,74 @@ public struct Anytype_Rpc {
     public init() {}
   }
 
+  public struct GenericErrorResponse {
+    // SwiftProtobuf.Message conformance is added in an extension below. See the
+    // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+    // methods supported on all messages.
+
+    public var error: Anytype_Rpc.GenericErrorResponse.Error {
+      get {return _error ?? Anytype_Rpc.GenericErrorResponse.Error()}
+      set {_error = newValue}
+    }
+    /// Returns true if `error` has been explicitly set.
+    public var hasError: Bool {return self._error != nil}
+    /// Clears the value of `error`. Subsequent reads from it will return its default value.
+    public mutating func clearError() {self._error = nil}
+
+    public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+    public struct Error {
+      // SwiftProtobuf.Message conformance is added in an extension below. See the
+      // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+      // methods supported on all messages.
+
+      public var code: Anytype_Rpc.GenericErrorResponse.Error.Code = .null
+
+      public var description_p: String = String()
+
+      public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+      public enum Code: SwiftProtobuf.Enum {
+        public typealias RawValue = Int
+        case null // = 0
+        case unknownError // = 1
+
+        /// ...
+        case badInput // = 2
+        case UNRECOGNIZED(Int)
+
+        public init() {
+          self = .null
+        }
+
+        public init?(rawValue: Int) {
+          switch rawValue {
+          case 0: self = .null
+          case 1: self = .unknownError
+          case 2: self = .badInput
+          default: self = .UNRECOGNIZED(rawValue)
+          }
+        }
+
+        public var rawValue: Int {
+          switch self {
+          case .null: return 0
+          case .unknownError: return 1
+          case .badInput: return 2
+          case .UNRECOGNIZED(let i): return i
+          }
+        }
+
+      }
+
+      public init() {}
+    }
+
+    public init() {}
+
+    fileprivate var _error: Anytype_Rpc.GenericErrorResponse.Error? = nil
+  }
+
   public init() {}
 }
 
@@ -15301,6 +15369,15 @@ extension Anytype_Rpc.Debug.Thread.Response.Error.Code: CaseIterable {
 extension Anytype_Rpc.Debug.Tree.Response.Error.Code: CaseIterable {
   // The compiler won't synthesize support with the UNRECOGNIZED case.
   public static var allCases: [Anytype_Rpc.Debug.Tree.Response.Error.Code] = [
+    .null,
+    .unknownError,
+    .badInput,
+  ]
+}
+
+extension Anytype_Rpc.GenericErrorResponse.Error.Code: CaseIterable {
+  // The compiler won't synthesize support with the UNRECOGNIZED case.
+  public static var allCases: [Anytype_Rpc.GenericErrorResponse.Error.Code] = [
     .null,
     .unknownError,
     .badInput,
@@ -35085,6 +35162,84 @@ extension Anytype_Rpc.Debug.Tree.Response.Error: SwiftProtobuf.Message, SwiftPro
 }
 
 extension Anytype_Rpc.Debug.Tree.Response.Error.Code: SwiftProtobuf._ProtoNameProviding {
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    0: .same(proto: "NULL"),
+    1: .same(proto: "UNKNOWN_ERROR"),
+    2: .same(proto: "BAD_INPUT"),
+  ]
+}
+
+extension Anytype_Rpc.GenericErrorResponse: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = Anytype_Rpc.protoMessageName + ".GenericErrorResponse"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "error"),
+  ]
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularMessageField(value: &self._error) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    if let v = self._error {
+      try visitor.visitSingularMessageField(value: v, fieldNumber: 1)
+    }
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Anytype_Rpc.GenericErrorResponse, rhs: Anytype_Rpc.GenericErrorResponse) -> Bool {
+    if lhs._error != rhs._error {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Anytype_Rpc.GenericErrorResponse.Error: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = Anytype_Rpc.GenericErrorResponse.protoMessageName + ".Error"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .same(proto: "code"),
+    2: .same(proto: "description"),
+  ]
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularEnumField(value: &self.code) }()
+      case 2: try { try decoder.decodeSingularStringField(value: &self.description_p) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    if self.code != .null {
+      try visitor.visitSingularEnumField(value: self.code, fieldNumber: 1)
+    }
+    if !self.description_p.isEmpty {
+      try visitor.visitSingularStringField(value: self.description_p, fieldNumber: 2)
+    }
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Anytype_Rpc.GenericErrorResponse.Error, rhs: Anytype_Rpc.GenericErrorResponse.Error) -> Bool {
+    if lhs.code != rhs.code {return false}
+    if lhs.description_p != rhs.description_p {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Anytype_Rpc.GenericErrorResponse.Error.Code: SwiftProtobuf._ProtoNameProviding {
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     0: .same(proto: "NULL"),
     1: .same(proto: "UNKNOWN_ERROR"),
