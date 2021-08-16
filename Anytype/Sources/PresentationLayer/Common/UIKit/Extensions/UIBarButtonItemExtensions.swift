5744ac224af276c58a4cc1d973d9d598186c0934
Konstantin Mordan <mordan2be@gmail.com>
2021-08-16 13:49:20 +0300

    Add back nav bar item

--- Anytype/Sources/PresentationLayer/Common/UIKit/Extensions/UIBarButtonItemExtensions.swift
+++ Anytype/Sources/PresentationLayer/Common/UIKit/Extensions/UIBarButtonItemExtensions.swift
@@ -10,7 +10,15 @@ import UIKit
 
 extension UIBarButtonItem {
     
+    static func back(withBackground: Bool, action: @escaping () -> Void) -> UIBarButtonItem? {
+        editorBarButtonItem(with: .backArrow, withBackground: withBackground, action: action)
+    }
+    
     static func settings(withBackground: Bool, action: @escaping () -> Void) -> UIBarButtonItem? {
+        editorBarButtonItem(with: .more, withBackground: withBackground, action: action)
+    }
+    
+    private static func editorBarButtonItem(with image: UIImage?, withBackground: Bool, action: @escaping () -> Void) -> UIBarButtonItem? {
         let backgroundView = UIView()
         backgroundView.backgroundColor = withBackground ? .black.withAlphaComponent(0.35) : .clear
         backgroundView.clipsToBounds = true
@@ -20,7 +28,7 @@ extension UIBarButtonItem {
         }
                 
         let button = UIButton(type: .system)
-        button.setImage(.more, for: .normal)
+        button.setImage(image, for: .normal)
         button.tintColor = UIColor.grayscaleWhite
         button.addAction(
             UIAction(
