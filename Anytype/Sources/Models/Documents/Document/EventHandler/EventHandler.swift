dea65e509560a56f9d649d1ae3f9cd33f4169473
Alex <alex.bender.kov8@gmail.com>
2021-09-02 16:35:27 +0300

    mentionMarkupUpdater renamed to mentionMarkupEventProvider

--- Anytype/Sources/Models/Documents/Document/EventHandler/EventHandler.swift
+++ Anytype/Sources/Models/Documents/Document/EventHandler/EventHandler.swift
@@ -9,7 +9,7 @@ protocol EventHandlerProtocol: AnyObject {
     func handle(events: PackOfEvents)
 }
 
-class EventHandler: EventHandlerProtocol {
+final class EventHandler: EventHandlerProtocol {
     lazy var didProcessEventsPublisher = didProcessEventsSubject.eraseToAnyPublisher()
     
     private lazy var eventPublisher = NotificationEventListener(handler: self)
@@ -19,7 +19,7 @@ class EventHandler: EventHandlerProtocol {
     
     private var middlewareConverter: MiddlewareEventConverter?
     private var localConverter: LocalEventConverter?
-    private var mentionMarkupUpdater: MentionMarkupEventProvider?
+    private var mentionMarkupEventProvider: MentionMarkupEventProvider?
     
     private let pageEventConverter = PageEventConverter()
 
@@ -33,7 +33,7 @@ class EventHandler: EventHandlerProtocol {
         setupMiddlewareConverter(with: container)
         localConverter = LocalEventConverter(container: container)
         eventPublisher.startListening(contextId: rootId)
-        mentionMarkupUpdater = MentionMarkupEventProvider(container: container)
+        mentionMarkupEventProvider = MentionMarkupEventProvider(container: container)
     }
     
     func handleBlockShow(events: PackOfEvents) -> [PageEvent] {
@@ -52,7 +52,7 @@ class EventHandler: EventHandlerProtocol {
         let middlewareUpdates = events.middlewareEvents.compactMap(\.value).compactMap { middlewareConverter?.convert($0) ?? nil
         }
         let localUpdates = events.localEvents.compactMap { localConverter?.convert($0) ?? nil }
-        let markupUpdates = [mentionMarkupUpdater?.updateMentionsEvent()].compactMap { $0 }
+        let markupUpdates = [mentionMarkupEventProvider?.updateMentionsEvent()].compactMap { $0 }
         let updates = middlewareUpdates + localUpdates + markupUpdates
         
         guard let container = self.container, let rootId = container.rootId else {
