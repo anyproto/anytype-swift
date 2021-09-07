90bb11aa05c249143037ac9136bb1fec0632e57c
Alex <alex.bender.kov8@gmail.com>
2021-09-07 20:21:16 +0300

    Added image guidelines for mention

--- Anytype/Sources/PresentationLayer/Common/ObjectIcon/ObjectIconImageGuidelineFactories/EmojiIconImageGuideline.swift
+++ Anytype/Sources/PresentationLayer/Common/ObjectIcon/ObjectIconImageGuidelineFactories/EmojiIconImageGuideline.swift
@@ -21,6 +21,21 @@ enum EmojiIconImageGuideline {
         cornerRadius: 10
     )
     
+    static let x28 = ImageGuideline(
+        size: CGSize(width: 28, height: 28),
+        cornerRadius: 0
+    )
+    
+    static let x24 = ImageGuideline(
+        size: CGSize(width: 24, height: 24),
+        cornerRadius: 0
+    )
+    
+    static let x20 = ImageGuideline(
+        size: CGSize(width: 20, height: 20),
+        cornerRadius: 0
+    )
+    
     static let x18 = ImageGuideline(
         size: CGSize(width: 18, height: 18),
         cornerRadius: 0
--- Anytype/Sources/PresentationLayer/Common/ObjectIcon/ObjectIconImageGuidelineFactories/TodoIconImageGuideline.swift
+++ Anytype/Sources/PresentationLayer/Common/ObjectIcon/ObjectIconImageGuidelineFactories/TodoIconImageGuideline.swift
@@ -16,6 +16,16 @@ enum TodoIconImageGuideline {
         cornerRadius: 7
     )
     
+    static let x24 = ImageGuideline(
+        size: CGSize(width: 24, height: 24),
+        cornerRadius: 6
+    )
+    
+    static let x20 = ImageGuideline(
+        size: CGSize(width: 20, height: 20),
+        cornerRadius: 5
+    )
+    
     static let x18 = ImageGuideline(
         size: CGSize(width: 18, height: 18),
         cornerRadius: 4
