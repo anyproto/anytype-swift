53fed20366d0f40fd9e11f5b96cafd466b29cb73
Vladimir Ignatov <abbadoh@mail.ru>
2021-09-09 12:20:31 +0700

    Add guidelines for editor search

--- Anytype/Sources/PresentationLayer/Common/ObjectIcon/ObjectIconImageGuidelineFactories/EmojiIconImageGuideline.swift
+++ Anytype/Sources/PresentationLayer/Common/ObjectIcon/ObjectIconImageGuidelineFactories/EmojiIconImageGuideline.swift
@@ -1,11 +1,3 @@
-//
-//  EmojiIconImageGuideline.swift
-//  EmojiIconImageGuideline
-//
-//  Created by Konstantin Mordan on 25.08.2021.
-//  Copyright © 2021 Anytype. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
@@ -19,6 +11,11 @@ enum EmojiIconImageGuideline {
     static let x48 = ImageGuideline(
         size: CGSize(width: 48, height: 48),
         cornerRadius: 10
+    )
+    
+    static let x40 = ImageGuideline(
+        size: CGSize(width: 40, height: 40),
+        cornerRadius: 8
     )
     
     static let x18 = ImageGuideline(
