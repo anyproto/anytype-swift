76e0af9535466a5ebb6ee2ae2dac38814a743dc7
Konstantin Mordan <mordan2be@gmail.com>
2021-09-17 18:51:20 +0300

    Fixed crash caused by whitespace in emoji string

--- Modules/AnytypeCore/AnytypeCore/Extensions/StringExtension.swift
+++ Modules/AnytypeCore/AnytypeCore/Extensions/StringExtension.swift
@@ -2,4 +2,9 @@ public extension String {
     var isNotEmpty: Bool {
         !isEmpty
     }
+    
+    /// Returns trimmed string (without whitespaces and newlines at the edges).
+    var trimmed: String {
+        trimmingCharacters(in: .whitespacesAndNewlines)
+    }
 }
