af3f5089729ea96885de7e587b044282e310f057
Vladimir Ignatov <abbadoh@mail.ru>
2021-09-17 13:25:15 +0700

    add shared DetailsContainer

--- Modules/BlocksModels/Sources/Details/Container/DetailsContainer.swift
+++ Modules/BlocksModels/Sources/Details/Container/DetailsContainer.swift
@@ -1,4 +1,6 @@
 public final class DetailsContainer: DetailsContainerProtocol {
+    // TODO: Remove after migration to single document for whole app
+    public static let shared: DetailsContainerProtocol = DetailsContainer()
     
     private var details: [String: LegacyDetailsModel] = [:]
     
@@ -9,12 +11,14 @@ public final class DetailsContainer: DetailsContainerProtocol {
     }
     
     public func add(model: LegacyDetailsModel, by id: ParentId) {
+        
         guard details[id] == nil else { return }
         // It is ok if details already exists
         // For example, if we add new page block, we will receive .objectDetailsSet event
         // then store details first time
         // and then we will receive .blockAdd and store details second time
         details[id] = model
+        Self.shared.add(model: model, by: id)
     }
     
 }
