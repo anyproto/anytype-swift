5c67bbeeeb4c610dabe981d2e359e9a65c270566
Denis Batvinkin <batvos7@gmail.com>
2021-09-20 14:39:43 +0300

    fix on pr

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/File/Audio/AudioBlockContentView.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/File/Audio/AudioBlockContentView.swift
@@ -69,7 +69,7 @@ final class AudioBlockContentView: UIView, UIContentView {
     }
 }
 
-extension AudioBlockContentView {
+private extension AudioBlockContentView {
     enum Layout {
         static let blockContentPadding: UIEdgeInsets = .init(top: 10, left: 20, bottom: -10, right: -10)
         static let blockBackgroundPadding: UIEdgeInsets = .init(top: 10, left: 0, bottom: -10, right: 0)
