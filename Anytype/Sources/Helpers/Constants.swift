43ae3d8620db8c1f2ba6935d101235c9599cf5ca
Denis Batvinkin <batvos7@gmail.com>
2021-09-21 17:37:40 +0300

    fix icloud settings

--- Anytype/Sources/Helpers/Constants.swift
+++ Anytype/Sources/Helpers/Constants.swift
@@ -1,11 +1,5 @@
 import UIKit
 
-func getDocumentsDirectory() -> URL {
-    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    let documentsDirectory = paths[0]
-    
-    return documentsDirectory
-}
 
 var windowHolder: WindowHolder? {
     let sceneDeleage = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
