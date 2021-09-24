92df8fc6d9faefd0b88e241237f63c856204e525
Konstantin Mordan <mordan2be@gmail.com>
2021-09-24 15:56:01 +0300

    Update files tree

--- Anytype/Sources/Models/Documents/Document/Cover/DocumentCover.swift
+++ Anytype/Sources/Models/Documents/Document/Cover/DocumentCover.swift
@@ -4,5 +4,5 @@ import UIKit.UIColor
 enum DocumentCover: Hashable {
     case imageId(String)
     case color(UIColor)
-    case gradient(_ startColor: UIColor, _ endColor: UIColor)
+    case gradient(GradientColor)
 }
