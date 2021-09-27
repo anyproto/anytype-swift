868e4f02df71a9ff3b2fc2a6220f526e6d0c6320
Konstantin Mordan <mordan2be@gmail.com>
2021-09-27 19:58:18 +0300

    Implemented using image upload operations for cover

--- Anytype/Sources/Utilities/ImageUploadingDemon/ImageUploadingDemon.swift
+++ Anytype/Sources/Utilities/ImageUploadingDemon/ImageUploadingDemon.swift
@@ -10,12 +10,25 @@ import Foundation
 
 final class ImageUploadingDemon {
     
-    static let shared = ImageUploadingDemon()
+    // MARK: - Internal variables
     
     let queue: OperationQueue = {
         let queue = OperationQueue()
         queue.qualityOfService = .utility
         return queue
     }()
+    
+    // MARK: - Initializer
+    
+    static let shared = ImageUploadingDemon()
+
+    // MARK: - Internal func
+    
+    func uploadImageFrom(item: NSItemProvider, onUpload: ((String) -> Void)?) {
+        let operation = ImageUploadingOperation(item)
+        operation.onImageUpload = onUpload
+        
+        queue.addOperation(operation)
+    }
     
 }
