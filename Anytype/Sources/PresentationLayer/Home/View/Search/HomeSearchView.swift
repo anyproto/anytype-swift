c29a7148baa6c159db360f641bd85ed3a620d214
Vladimir Ignatov <abbadoh@mail.ru>
2021-09-29 13:01:41 +0700

    Reuse search view

--- Anytype/Sources/PresentationLayer/Home/View/Search/HomeSearchView.swift
+++ Anytype/Sources/PresentationLayer/Home/View/Search/HomeSearchView.swift
@@ -2,72 +2,10 @@ import SwiftUI
 
 struct HomeSearchView: View {
     @EnvironmentObject var viewModel: HomeViewModel
-    @Environment(\.presentationMode) var presentationMode
-    
-    @State private var searchText = ""
-    @State private var data = [HomeSearchCellData]()
-    
-    private let service = SearchService()
-    
+        
     var body: some View {
-        VStack() {
-            DragIndicator(bottomPadding: 0)
-            SearchBar(text: $searchText)
-            if data.isEmpty {
-                emptyState
-            } else {
-                searchResults
-            }
+        SearchView(title: nil) { data in
+            viewModel.showPage(pageId: data.id)
         }
-        .onChange(of: searchText) { search(text: $0) }
-        .onAppear { search(text: searchText) }
-    }
-    
-    private var searchResults: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(data) { data in
-                    Button(action: {
-                        presentationMode.wrappedValue.dismiss()
-                        viewModel.showPage(pageId: data.id)
-                    }) {
-                        HomeSearchCell(data: data)
-                    }
-                    .frame(maxWidth: .infinity)
-                    .modifier(DividerModifier(spacing: 0, leadingPadding: 72, trailingPadding: 12, alignment: .leading))
-                }
-            }
-        }
-    }
-    
-    private var emptyState: some View {
-        VStack(alignment: .center) {
-            Spacer()
-            AnytypeText(
-                "\("There is no object named".localized) \"\(searchText)\"",
-                style: .uxBodyRegular,
-                color: .textPrimary
-            )
-            .multilineTextAlignment(.center)
-            AnytypeText(
-                "Try to create a new one or search for something else",
-                style: .uxBodyRegular,
-                color: .textSecondary
-            )
-            .multilineTextAlignment(.center)
-            Spacer()
-        }.padding(.horizontal)
-    }
-    
-    private func search(text: String) {
-        service.search(text: text) { results in
-            data = results.map { HomeSearchCellData(searchResult: $0) }
-        }
-    }
-}
-
-struct HomeSearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeSearchView()
     }
 }
---
+++ Anytype/Sources/PresentationLayer/TextEditor/EditorView/Views/MoveToSearchView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct MoveToSearchView: View {
+    
+    var body: some View {
+        SearchView(title: "Move to") { data in
+            print(data)
+            // TODO
+        }
+    }
+}
