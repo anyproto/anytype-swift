848895931a5d81b8a65fcc958d8aa7e0c8ab3234
Konstantin Mordan <mordan2be@gmail.com>
2021-10-04 15:51:31 +0300

    Refactored MediaPicker

--- Anytype/Sources/PresentationLayer/Common/UIKit/Pickers/MediaPicker/MediaPickerViewModel.swift
+++ Anytype/Sources/PresentationLayer/Common/UIKit/Pickers/MediaPicker/MediaPickerViewModel.swift
@@ -1,17 +1,6 @@
 import Foundation
     
-final class MediaPickerViewModel {
+struct MediaPickerViewModel {
     let type: MediaPickerContentType
-    let completion: ((FilePickerResultInformation?) -> Void)
-    
-    init(type: MediaPickerContentType, completion: @escaping (FilePickerResultInformation?) -> Void) {
-        self.type = type
-        self.completion = completion
-    }
-    
-    func process(_ url: URL?) {
-        completion(
-            url.flatMap { FilePickerResultInformation(documentUrl: $0) }
-        )
-    }
+    let completion: (NSItemProvider?) -> Void
 }
