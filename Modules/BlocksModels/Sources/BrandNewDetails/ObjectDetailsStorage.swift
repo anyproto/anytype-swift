f0fc2b5617677e84d592b45d29ab96a417f6eb7c
Konstantin Mordan <mordan2be@gmail.com>
2021-10-10 13:51:58 +0300

    Add ObjectDetailsStorage

---
+++ Modules/BlocksModels/Sources/BrandNewDetails/ObjectDetailsStorage.swift
@@ -0,0 +1,28 @@
+//
+//  ObjectDetailsStorage.swift
+//  BlocksModels
+//
+//  Created by Konstantin Mordan on 10.10.2021.
+//  Copyright © 2021 Dmitry Lobanov. All rights reserved.
+//
+
+import Foundation
+import AnytypeCore
+
+public final class ObjectDetailsStorage {
+    
+    private var detailsStorage = SynchronizedDictionary<BlockId, ObjectDetails>()
+    
+}
+
+extension ObjectDetailsStorage: ObjectDetailsStorageProtocol {
+    
+    public func get(id: BlockId) -> ObjectDetails? {
+        detailsStorage[id]
+    }
+    
+    public func add(details: ObjectDetails, id: BlockId) {
+        detailsStorage[id] = details
+    }
+    
+}
