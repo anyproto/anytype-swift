de2a19538c88376e0de8eb1e38eed73c8921a044
Dmitry <bududomasidet@gmail.com>
2021-10-17 23:03:28 +0300

    Download image not using main thread

--- Anytype/Sources/PresentationLayer/Common/UIKit/ImageViewer/ImageViewer/ImageViewerViewModel.swift
+++ Anytype/Sources/PresentationLayer/Common/UIKit/ImageViewer/ImageViewer/ImageViewerViewModel.swift
@@ -3,23 +3,16 @@ import Combine
 
 final class ImageViewerViewModel {
     @Published var image: UIImage?
+    @Published var isLoading: Bool = false
+
+    private var cancellables = [AnyCancellable]()
 
     init(imageSource: ImageSource) {
-        self.image = imageSource.uiImage
-    }
-}
-
-private extension ImageSource {
-    var uiImage: UIImage? {
-        switch self {
-        case .image(let image):
-            return image
-        case .middleware(let fileId):
-            guard let url = fileId.resolvedUrl,
-                  let data = try? Data(contentsOf: url),
-                  let image = UIImage(data: data) else { return nil }
-
-            return image
-        }
+        isLoading = true
+        imageSource.image.sink { [weak self] _ in
+            self?.isLoading = false
+        } receiveValue: { [weak self] image in
+            self?.image = image
+        }.store(in: &cancellables)
     }
 }
