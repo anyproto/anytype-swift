cd301b2bf19ce3564aedee564543e573529839b8
Konstantin Mordan <mordan2be@gmail.com>
2021-10-22 22:33:09 +0800

    Add RelationKey

---
+++ Modules/BlocksModels/Sources/Relations/RelationKey.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+
+public enum RelationKey: String {
+    case id = "id"
+    case name = "name"
+    case iconEmoji = "iconEmoji"
+    case iconImage = "iconImage"
+    case coverId = "coverId"
+    case coverType = "coverType"
+    case isArchived = "isArchived"
+    case isFavorite = "isFavorite"
+    case description = "description"
+    case layout = "layout"
+    case layoutAlign = "layoutAlign"
+    case done = "done"
+    case type = "type"
+    case lastOpenedDate = "lastOpenedDate"
+    case isHidden = "isHidden"
+    case lastModifiedDate = "lastModifiedDate"
+    case isDraft = "isDraft"
+    case featuredRelations = "featuredRelations"
+    case relationFormat = "relationFormat"
+    case isReadonly = "isReadonly"
+}
