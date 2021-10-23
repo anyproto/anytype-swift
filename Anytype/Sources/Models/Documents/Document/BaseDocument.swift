eb56239d25e37a3e85c93da7b9748110744f8aae
Denis Batvinkin <batvos7@gmail.com>
2021-10-23 22:18:13 +0300

    fix no pr

--- Anytype/Sources/Models/Documents/Document/BaseDocument.swift
+++ Anytype/Sources/Models/Documents/Document/BaseDocument.swift
@@ -2,12 +2,14 @@ import Foundation
 import BlocksModels
 import Combine
 import AnytypeCore
+import ProtobufMessages
+
 
 private extension LoggerCategory {
     static let baseDocument: Self = "BaseDocument"
 }
 
-final class BaseDocument: BaseDocumentProtocol, PartialBaseDocument {
+final class BaseDocument: BaseDocumentProtocol {
     var onUpdateReceive: ((EventsListenerUpdate) -> Void)?
     
     private let blockActionsService = ServiceLocator.shared.blockActionsServiceSingle()
@@ -17,7 +19,7 @@ final class BaseDocument: BaseDocumentProtocol, PartialBaseDocument {
 
     let blocksContainer: BlockContainerModelProtocol = BlockContainer()
     let detailsStorage: ObjectDetailsStorageProtocol = ObjectDetailsStorage()
-    var objectRestrictions: ObjectRestrictions = ObjectRestrictions()
+    private(set) var objectRestrictions: ObjectRestrictions = ObjectRestrictions()
         
     init(objectId: BlockId) {
         self.objectId = objectId
@@ -84,5 +86,43 @@ final class BaseDocument: BaseDocumentProtocol, PartialBaseDocument {
         }
         eventsListener.startListening()
     }
-    
+
+    private func parseShowEvents(event: MiddlewareResponse) {
+        let objectShowEvent = showEventsFromMessages(event.messages).first
+        guard let objectShowEvent = objectShowEvent else { return }
+
+        let rootId = objectShowEvent.rootID
+        guard rootId.isNotEmpty else { return }
+
+        let parsedBlocks = objectShowEvent.blocks.compactMap {
+            BlockInformationConverter.convert(block: $0)
+        }
+        let parsedDetails = objectShowEvent.details.map {
+            ObjectDetails(
+                id: $0.id,
+                rawDetails: MiddlewareDetailsConverter.convertSetEvent($0)
+            )
+        }
+
+        TreeBlockBuilder.buildBlocksTree(
+            from: parsedBlocks,
+            with: rootId,
+            in: blocksContainer
+        )
+
+        parsedDetails.forEach {
+            detailsStorage.add(details: $0, id: $0.id)
+        }
+
+        objectRestrictions = MiddlewareObjectRestrictionsConverter.convertObjectRestrictions(middlewareResctrictions: objectShowEvent.restrictions)
+    }
+
+    private func showEventsFromMessages(_ messages: [Anytype_Event.Message]) -> [Anytype_Event.Object.Show] {
+        messages
+            .compactMap { $0.value }
+            .compactMap { value -> Anytype_Event.Object.Show? in
+                guard case .objectShow(let event) = value else { return nil }
+                return event
+            }
+    }
 }
--- Anytype/Sources/Models/Documents/Document/BaseDocumentProtocol.swift
+++ Anytype/Sources/Models/Documents/Document/BaseDocumentProtocol.swift
@@ -2,18 +2,14 @@ import BlocksModels
 import Combine
 
 
-protocol PartialBaseDocument: AnyObject {
+protocol BaseDocumentProtocol: AnyObject {
     var blocksContainer: BlockContainerModelProtocol { get }
     var detailsStorage: ObjectDetailsStorageProtocol { get }
-    var objectRestrictions: ObjectRestrictions { get set }
-}
-
-protocol BaseDocumentProtocol: PartialBaseDocument {
+    var objectRestrictions: ObjectRestrictions { get }
     var objectId: BlockId { get }
     var onUpdateReceive: ((EventsListenerUpdate) -> Void)? { get set }
     var objectDetails: ObjectDetails? { get }
     var flattenBlocks: [BlockModelProtocol] { get }
 
     func open()
-
 }
