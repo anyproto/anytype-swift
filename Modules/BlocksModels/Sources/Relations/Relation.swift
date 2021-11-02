8fed2ade986114ba5f46b4208d2869f270f23bc9
Konstantin Mordan <mordan2be@gmail.com>
2021-11-02 20:09:58 +0800

    Clean up relation struct

--- Modules/BlocksModels/Sources/Relations/Relation.swift
+++ Modules/BlocksModels/Sources/Relations/Relation.swift
@@ -6,13 +6,13 @@ public struct Relation: Hashable {
     public let key: String
     public let name: String
     public let format: Format
-    public let source: Source
     public let isHidden: Bool = false
     public let isReadOnly: Bool = false
     public let isMulti: Bool = false
+    // list of values for multiple relations. ex.: tags
     public let selections: [Option] = []
+    // list of types used by relation. ex.: type of file
     public let objectTypes: [String] = []
-    public let defaultValue: AnyHashable? = nil
     
 }
 
