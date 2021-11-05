10bd0cbf2b58374759cc19efc6ed8b0c4c135820
Konstantin Mordan <mordan2be@gmail.com>
2021-11-05 18:17:07 +0800

    Remove commented code

--- Modules/BlocksModels/Sources/Relations/RelationFormat.swift
+++ Modules/BlocksModels/Sources/Relations/RelationFormat.swift
@@ -2,24 +2,6 @@ import Foundation
 
 public extension Relation {
     
-    // TODO: - Remove later
-//    enum Format: String, Hashable {
-//        case shortText = "Short text"
-//        case longText = "Text"
-//        case number = "Number"
-//        case status = "Status"
-//        case tag = "Tag"
-//        case date = "Date"
-//        case file = "File"
-//        case checkbox = "Checkbox"
-//        case url = "URL"
-//        case email = "Email"
-//        case phone = "Phone number"
-//        case emoji = "Emoji"
-//        case object = "Object"
-//        case relations = "Relations"
-//    }
-    
     enum Format: Hashable {
         case longText
         case shortText
