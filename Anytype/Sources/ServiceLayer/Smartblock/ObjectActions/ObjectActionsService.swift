4fde59aa7541aa713f6a507a3b1732977b79e66e
Vladimir Ignatov <abbadoh@mail.ru>
2021-11-09 13:20:33 +0400

    Update middleware

--- Anytype/Sources/ServiceLayer/Smartblock/ObjectActions/ObjectActionsService.swift
+++ Anytype/Sources/ServiceLayer/Smartblock/ObjectActions/ObjectActionsService.swift
@@ -22,7 +22,11 @@ final class ObjectActionsService: ObjectActionsServiceProtocol {
     }
 
     func setFavorite(objectId: BlockId, _ isFavorite: Bool) {
-        _ = Anytype_Rpc.Object.SetIsFavorite.Service.invoke(contextID: objectId, isFavorite: isFavorite)
+        setFavorite(objectIds: [objectId], isFavorite)
+    }
+    
+    func setFavorite(objectIds: [BlockId], _ isFavorite: Bool) {
+        _ = Anytype_Rpc.ObjectList.Set.IsFavorite.Service.invoke(objectIds: objectIds, isFavorite: isFavorite)
     }
     
     /// NOTE: `CreatePage` action will return block of type `.link(.page)`.
--- Anytype/Sources/ServiceLayer/Smartblock/ObjectActions/ObjectActionsServiceProtocol.swift
+++ Anytype/Sources/ServiceLayer/Smartblock/ObjectActions/ObjectActionsServiceProtocol.swift
@@ -6,6 +6,7 @@ protocol ObjectActionsServiceProtocol {
     func delete(objectIds: [BlockId])
     func setArchive(objectId: BlockId, _ isArchived: Bool)
     func setArchive(objectIds: [BlockId], _ isArchived: Bool)
+    func setFavorite(objectIds: [BlockId], _ isFavorite: Bool)
     func setFavorite(objectId: BlockId, _ isFavorite: Bool)
     func convertChildrenToPages(contextID: BlockId, blocksIds: [BlockId], objectType: String) -> [BlockId]?
     func setDetails(contextID: BlockId, details: ObjectRawDetails)
