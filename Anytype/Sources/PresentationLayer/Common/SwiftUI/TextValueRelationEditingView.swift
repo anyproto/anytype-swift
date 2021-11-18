87f9daad0de5db4c604642b7eccb72303d838d92
Konstantin Mordan <mordan2be@gmail.com>
2021-11-18 19:22:04 +0800

    Fixed code review issues

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/TextValueRelationEditingView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/TextValueRelationEditingView.swift
@@ -1,14 +1,5 @@
 import SwiftUI
 
-struct ViewHeightKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = value + defaultValue
-    }
-    
-}
-
 struct TextValueRelationEditingView: View {
     @State var text: String = ""
     @State private var height: CGFloat = 0
@@ -41,29 +32,17 @@ struct TextValueRelationEditingView: View {
                 .font(AnytypeFontBuilder.font(anytypeFont: .uxBodyRegular))
                 .opacity(0)
                 .padding(6)
-                .background(
-                    GeometryReader {
-                        Color.clear
-                            .preference(
-                                key: ViewHeightKey.self,
-                                value: $0.frame(in: .local).size.height
-                            )
-                    }
-                )
-                .border(Color.yellow)
+                .background(FrameCatcher { height = $0.height })
             
             TextEditor(text: $text)
                 .font(AnytypeFontBuilder.font(anytypeFont: .uxBodyRegular))
                 .foregroundColor(Color.grayscale90)
                 .frame(maxHeight: max(40, height))
-                .border(Color.red)
             
             AnytypeText("Add text", style: .uxBodyRegular, color: .textTertiary)
                 .padding(6)
                 .opacity(text.isEmpty ? 1 : 0)
         }
-        .onPreferenceChange(ViewHeightKey.self) { height = $0 }
-        .frame(maxHeight: 200)
     }
 }
 
