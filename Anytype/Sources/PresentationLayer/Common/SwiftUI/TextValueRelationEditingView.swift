3c1c90616c9a4e772be92f22d4ddf82d5b45adb4
Konstantin Mordan <mordan2be@gmail.com>
2021-11-18 14:17:31 +0800

    Implement self sized TextValueRelationEditingView

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/TextValueRelationEditingView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/TextValueRelationEditingView.swift
@@ -1,38 +1,64 @@
 import SwiftUI
 
 struct ViewHeightKey: PreferenceKey {
-    static var defaultValue: CGFloat = 50
+    static var defaultValue: CGFloat = 0
 
     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
+        value = value + defaultValue
     }
+    
 }
 
 struct TextValueRelationEditingView: View {
     @State var text: String = ""
-    @State private var height: CGFloat = .zero
+    @State private var height: CGFloat = 0
     
     var body: some View {
         VStack(spacing: 0) {
             Spacer()
-            VStack(spacing: 0) {
-                DragIndicator(bottomPadding: 0)
-                AnytypeText("About".localized, style: .uxTitle1Semibold, color: .textPrimary)
-                    .padding([.top, .bottom], 12)
-                ZStack(alignment: .leading) {
-                    Text(text).foregroundColor(.clear).padding(6)
-                        .background(GeometryReader {
-                            Color.clear.preference(key: ViewHeightKey.self, value: $0.frame(in: .local).size.height)
-                        })
-                    TextEditor(text: $text)
-                        .frame(maxHeight: height)
-                    //.border(Color.red)        // << for testing
-                }
-                .onPreferenceChange(ViewHeightKey.self) { height = $0 }
-            }
-            .background(Color.background)
-            .cornerRadius(16, corners: [.topLeft, .topRight])
+            content
         }
+        .padding(.bottom, -UIApplication.shared.mainWindowInsets.bottom)
+    }
+    
+    private var content: some View {
+        VStack(spacing: 0) {
+            DragIndicator(bottomPadding: 0)
+            AnytypeText("About".localized, style: .uxTitle1Semibold, color: .textPrimary)
+                .padding([.top, .bottom], 12)
+            
+            textEditingView
+            
+            Spacer.fixedHeight(20 + UIApplication.shared.mainWindowInsets.bottom)
+        }
+        .background(Color.background)
+        .cornerRadius(16, corners: [.topLeft, .topRight])
+    }
+    
+    private var textEditingView: some View {
+        ZStack(alignment: .leading) {
+            Text(text)
+                .font(AnytypeFontBuilder.font(anytypeFont: .uxBodyRegular))
+                .opacity(0)
+                .padding(6)
+                .background(
+                    GeometryReader {
+                        Color.clear
+                            .preference(
+                                key: ViewHeightKey.self,
+                                value: $0.frame(in: .local).size.height
+                            )
+                    }
+                )
+                .border(Color.yellow)
+            
+            TextEditor(text: $text)
+                .font(AnytypeFontBuilder.font(anytypeFont: .uxBodyRegular))
+                .foregroundColor(Color.grayscale90)
+                .frame(maxHeight: max(48, height))
+                .border(Color.red)
+        }
+        .onPreferenceChange(ViewHeightKey.self) { height = $0 }
     }
 }
 
