659f6fdaf5fc4428bba7ce1418a4ae73ac8e3f00
Vladimir Ignatov <abbadoh@mail.ru>
2021-11-19 14:20:02 +0400

    lil design improvements

--- Anytype/Sources/PresentationLayer/TextEditor/Set/Views/SetDetailsHeader.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/Set/Views/SetDetailsHeader.swift
@@ -55,15 +55,28 @@ struct SetDetailsHeader: View {
     }
     
     private var header: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             CoverConstants.gradients[1].asLinearGradient()
                 .frame(height: 240)
+                .ifLet(model.document.objectDetails?.objectIconImage) { view, icon in
+                    view.overlay(
+                        SwiftUIObjectIconImageView(iconImage: icon, usecase: .openedObject)
+                            .frame(width: 96, height: 96)
+                            .padding(.leading, 20)
+                            .padding(.bottom, -15),
+                        alignment: .bottomLeading
+                    )
+                }
+            Spacer.fixedHeight(25)
             AnytypeText("\(model.document.objectDetails?.title ??  "Untitled")", style: .title, color: .textPrimary)
                 .fixedSize(horizontal: false, vertical: true)
-                .padding()
-            AnytypeText("Set description", style: .body, color: .textPrimary)
-                .fixedSize(horizontal: false, vertical: true)
-                .padding()
+                .padding(.horizontal, 20)
+            if let description = model.document.objectDetails?.description {
+                Spacer.fixedHeight(6)
+                AnytypeText(description, style: .body, color: .textPrimary)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding()
+            }
         }
     }
     
