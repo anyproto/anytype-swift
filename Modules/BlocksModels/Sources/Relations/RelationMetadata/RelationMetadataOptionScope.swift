545407bb2d6a046dd6a8912c71f588d5a6c0218d
Konstantin Mordan <mordan2be@gmail.com>
2021-11-22 15:48:38 +0800

    Fixed code style

--- Modules/BlocksModels/Sources/Relations/RelationMetadata/RelationMetadataOptionScope.swift
+++ Modules/BlocksModels/Sources/Relations/RelationMetadata/RelationMetadataOptionScope.swift
@@ -14,21 +14,21 @@ public extension RelationMetadata.Option {
 extension RelationMetadata.Option.Scope {
     
     init(rawValue: Int) {
-      switch rawValue {
-      case 0: self = .local
-      case 1: self = .relation
-      case 2: self = .format
-      default: self = .unrecognized(rawValue)
-      }
+        switch rawValue {
+        case 0: self = .local
+        case 1: self = .relation
+        case 2: self = .format
+        default: self = .unrecognized(rawValue)
+        }
     }
-
+    
     var rawValue: Int {
-      switch self {
-      case .local: return 0
-      case .relation: return 1
-      case .format: return 2
-      case .unrecognized(let i): return i
-      }
+        switch self {
+        case .local: return 0
+        case .relation: return 1
+        case .format: return 2
+        case .unrecognized(let i): return i
+        }
     }
     
 }
