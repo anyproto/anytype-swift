3a7617eb436e780d60ba2a6b3a0220ca91c183b9
Konstantin Mordan <mordan2be@gmail.com>
2021-11-22 22:11:54 +0800

    Added all to ParsedRelations

--- Anytype/Sources/Models/Relations/ParsedRelations.swift
+++ Anytype/Sources/Models/Relations/ParsedRelations.swift
@@ -2,7 +2,15 @@ import AnytypeCore
 
 struct ParsedRelations {
 
+    let all: [Relation]
+    
     let featuredRelations: [Relation]
     let otherRelations: [Relation]
+    
+    init(featuredRelations: [Relation], otherRelations: [Relation]) {
+        self.all = featuredRelations + otherRelations
+        self.featuredRelations = featuredRelations
+        self.otherRelations = otherRelations
+    }
     
 }
