700bb9926dd511b7d628ee74d5c328329aebf580
Dmitry <bududomasidet@gmail.com>
2021-11-29 20:11:31 +0800

    Cosmetics

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Text/Code/CodeBlockView.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Text/Code/CodeBlockView.swift
@@ -15,11 +15,17 @@ final class CodeBlockView: BaseBlockView<CodeBlockContentConfiguration> {
         applyNewConfiguration()
     }
 
+    override func update(with state: UICellConfigurationState) {
+        super.update(with: state)
+
+        textView.isUserInteractionEnabled = state.isEditing
+    }
+
     // MARK: - Setup view
 
     private func setupViews() {
         addSubview(contentView) {
-            $0.pinToSuperview(insets: UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0))
+            $0.pinToSuperview(insets: UIEdgeInsets(top: 6, left: 10, bottom: -6, right: -10))
         }
         
         contentView.layoutUsing.stack {
