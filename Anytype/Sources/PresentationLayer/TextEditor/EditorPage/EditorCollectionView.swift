372c07d6ac149a82bc76256bd452df8612379d61
Dmitry <bududomasidet@gmail.com>
2021-11-30 14:47:31 +0800

    Collection view improvements

--- Anytype/Sources/PresentationLayer/TextEditor/EditorPage/EditorCollectionView.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/EditorPage/EditorCollectionView.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class EditorCollectionView: UICollectionView {
-    private(set) var indexPathsForMovingItems = [IndexPath]()
+    private(set) var indexPathsForMovingItems = Set<IndexPath>()
 
     func deselectAllMovingItems() {
         indexPathsForMovingItems.removeAll()
@@ -11,27 +11,32 @@ class EditorCollectionView: UICollectionView {
 
     func setItemIsMoving(_ isMoving: Bool, at indexPath: IndexPath) {
         if isMoving && !indexPathsForMovingItems.contains(indexPath) {
-            indexPathsForMovingItems.append(indexPath)
+            indexPathsForMovingItems.insert(indexPath)
         } else if !isMoving && indexPathsForMovingItems.contains(indexPath) {
-            indexPathsForMovingItems.firstIndex(of: indexPath).map {
-                indexPathsForMovingItems.remove(at: $0)
-            }
-        }
-        if indexPathsForMovingItems.contains(indexPath) {
-            indexPathsForMovingItems.append(indexPath)
+            indexPathsForMovingItems.remove(indexPath)
         }
 
+        if indexPathsForMovingItems.contains(indexPath) {
+            indexPathsForMovingItems.insert(indexPath)
+        }
+
+        setCelIsMoving(isMoving: isMoving, at: indexPath)
+    }
+
+    override func cellForItem(at indexPath: IndexPath) -> UICollectionViewCell? {
+        let cell = super.cellForItem(at: indexPath)
+
+        let contentView = cell?.contentView as? CustomTypesAccessable
+        contentView?.isMoving = indexPathsForMovingItems.contains(indexPath)
+
+        return cell
+    }
+
+
+    private func setCelIsMoving(isMoving: Bool, at indexPath: IndexPath) {
         let cell = cellForItem(at: indexPath)
         let contentView = cell?.contentView as? CustomTypesAccessable
 
         contentView?.isMoving = isMoving
-    }
-
-    override func reloadData() {
-        super.reloadData()
-
-        indexPathsForMovingItems.forEach { indexPath in
-            setItemIsMoving(true, at: indexPath)
-        }
     }
 }
