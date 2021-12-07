b30345f7d8e26a138a64eae38963c6fb4dc66c76
Vladimir Ignatov <abbadoh@mail.ru>
2021-12-07 18:42:35 +0400

    Remove unused pipelines

--- Scripts/ruby/pipeline_starter.rb
+++ Scripts/ruby/pipeline_starter.rb
@@ -1,14 +1,11 @@
 require_relative 'pipelines/install_pipeline'
 require_relative 'pipelines/update_pipeline'
-require_relative 'pipelines/list_pipeline'
-require_relative 'pipelines/current_version_pipeline'
 
 class PipelineStarter
   def self.start(options)
     case options[:command]
     when Commands::InstallCommand then InstallPipeline.start(options)
     when Commands::UpdateCommand then UpdatePipeline.start(options)
-    when Commands::ListCommand then ListPipeline.start(options)
     else
       puts "I don't recognize this command: #{options[:command]}"
       return
