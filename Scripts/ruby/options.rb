d34352184d6564bffcbc064e8654e39f2d2b287d
Vladimir Ignatov <abbadoh@mail.ru>
2021-12-10 18:10:25 +0400

    Remove install and update options

--- Scripts/ruby/options.rb
+++ Scripts/ruby/options.rb
@@ -1,7 +1,6 @@
 require 'optparse'
 
 require_relative 'library/environment'
-require_relative 'commands'
 
 class Options
   def self.parsed(args)
@@ -12,9 +11,8 @@ class Options
     options = {}
 
     OptionParser.new do |opts|
-      opts.on('-h', '--help', 'Show help') { help_message(opts); exit(0)}
-
-      opts.on('-u', '--update', '--update [VERSION]', 'Update middleware to latest or provided version') {|v| options[:command] = UpdateCommand.new(v)}
+      opts.on('-h', '--help', 'Show help') { help_message(opts); exit(0) }
+      opts.on('-l', '--latest', 'Update to the latest version') {|v| options[:latest] = true }
 
       opts.on('--token', '--token [TOKEN]', 'Token to access repository. It is private option.') {|v| options[:token] = v}
       opts.on('--on-ci', 'Run on CI') { |v| options[:runsOnCI] = true }
@@ -26,7 +24,7 @@ class Options
 
   private_class_method def self.default_options
     {
-      command: UpdateCommand.new,
+      latest: false,
       runsOnCI: false,
       token: EnvironmentVariables.token || '',
     }
--- Scripts/ruby/pipeline_starter.rb
+++ Scripts/ruby/pipeline_starter.rb
@@ -1,8 +1,26 @@
-require_relative 'pipelines/update_pipeline'
+require_relative 'pipelines/base_pipeline'
+require_relative 'pipelines/version_provider'
 
 class PipelineStarter
   def self.start(options)
-    UpdatePipeline.start(options)
+    version = VersionProvider.version(options)
+    BasePipeline.work(version, options)
+    
+    if options[:latest]
+      update_version(version, options)
+    end 
+
     puts "Done 💫"
+  end
+
+  def self.update_version(version, options)
+    filePath = Constants::LIBRARY_FILE_PATH
+    result = { Constants::LOCKFILE_VERSION_KEY => version}.to_yaml
+    result = result.gsub(/^---\s+/, '')
+    result = result.gsub(/:/, ': ~>')
+    File.open(filePath, 'w') do |file_handler|
+      file_handler.write(result)
+      puts "Updated library file"
+    end
   end
 end
