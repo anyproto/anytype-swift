d9401a5bc6f997c5a917b43d246caed25950e197
Konstantin Mordan <mordan2be@gmail.com>
2021-12-13 19:21:45 +0800

    Remove commented code

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/Relations/RelationViewProvider/RelationValueView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/Relations/RelationViewProvider/RelationValueView.swift
@@ -30,34 +30,6 @@ struct RelationValueView: View {
             case .unknown(let unknown):
                 RelationsListRowPlaceholderView(hint: unknown.value, type: style.placeholderType)
             }
-            
-//            let value = relation.value
-//            let hint = relation.hint
-//
-//            switch value {
-//            case .text(let string):
-//                TextRelationView(value: string, hint: hint, style: style, allowMultiLine: style.allowMultiLine)
-//            case .number(let string):
-//                TextRelationView(value: string, hint: hint, style: style)
-//            case .status(let statusRelation):
-//                StatusRelationView(value: statusRelation, hint: hint, style: style)
-//            case .date(let value):
-//                TextRelationView(value: value?.text, hint: hint, style: style)
-//            case .object(let objectsRelation):
-//                ObjectRelationView(value: objectsRelation, hint: hint, style: style)
-//            case .checkbox(let bool):
-//                CheckboxRelationView(isChecked: bool)
-//            case .url(let string):
-//                TextRelationView(value: string, hint: hint, style: style)
-//            case .email(let string):
-//                TextRelationView(value: string, hint: hint, style: style)
-//            case .phone(let string):
-//                TextRelationView(value: string, hint: hint, style: style)
-//            case .tag(let tags):
-//                TagRelationView(value: tags, hint: hint, style: style)
-//            case .unknown(let string):
-//                RelationsListRowPlaceholderView(hint: string, type: style.placeholderType)
-//            }
         }
     }
 }
