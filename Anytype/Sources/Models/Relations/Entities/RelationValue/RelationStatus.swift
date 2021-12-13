401a0e6001cd9707c68d98411ded686b14984f9e
Konstantin Mordan <mordan2be@gmail.com>
2021-12-13 13:58:32 +0800

    Create separate files for relations

---
+++ Anytype/Sources/Models/Relations/Entities/RelationValue/RelationStatus.swift
@@ -0,0 +1,41 @@
+import Foundation
+import BlocksModels
+
+extension Relation {
+    
+    struct Status: RelationProtocol, Hashable, Identifiable {
+        let id: String
+        let name: String
+        let isFeatured: Bool
+        let isEditable: Bool
+        
+        let value: Option?
+        let allOptions: [Option]
+    }
+    
+}
+
+extension Relation.Status {
+    
+    struct Option: Hashable, Identifiable {
+        let id: String
+        let text: String
+        let color: AnytypeColor
+        let scope: RelationMetadata.Option.Scope
+    }
+    
+}
+
+extension Relation.Status.Option {
+    
+    init(option: RelationMetadata.Option) {
+        let middlewareColor = MiddlewareColor(rawValue: option.color)
+        let anytypeColor: AnytypeColor = middlewareColor?.asDarkColor ?? .grayscale90
+        
+        self.id = option.id
+        self.text = option.text
+        self.color = anytypeColor
+        self.scope = option.scope
+    }
+    
+}
