780c7c3f92abca5f68bd622a84e18e44a4de2520
Vladimir Ignatov <abbadoh@mail.ru>
2021-12-14 17:54:07 +0400

    Unwrapping of codegn runner part 1

---
+++ Scripts/ruby/codegen/codegen_workers_2.rb
@@ -0,0 +1,26 @@
+class CodegenWorker
+  attr_accessor :tool,:transform, :filePath
+  def initialize(tool, transform, filePath)
+    self.tool = tool
+    self.transform = transform
+    self.filePath = filePath
+  end
+
+  def work
+    action = "ruby #{tool} --transform #{transform} --filePath #{filePath}"
+    ShellExecutor.run_command_line action
+  end
+end
+
+class FormatWorker
+  attr_accessor :tool, :filePath
+  def initialize(tool, filePath)
+    self.tool = tool
+    self.filePath = filePath
+  end
+
+  def action
+    action = "ruby #{tool} --inputFilePath #{filePath}"
+    ShellExecutor.run_command_line action
+  end
+end
