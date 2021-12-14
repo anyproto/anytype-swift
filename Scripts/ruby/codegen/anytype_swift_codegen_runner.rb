899f1383a82761236cb7b6c1e12dc663abd8b883
Vladimir Ignatov <abbadoh@mail.ru>
2021-12-14 19:01:43 +0400

    Remove shiza

--- Scripts/ruby/codegen/anytype_swift_codegen_runner.rb
+++ Scripts/ruby/codegen/anytype_swift_codegen_runner.rb
@@ -7,15 +7,21 @@ require_relative '../library/shell_executor'
 require_relative '../workers_hub'
 require_relative '../pipeline_starter'
 require_relative 'codegen_config'
-require_relative 'codegen_commands'
 require_relative 'codegen_pipelines'
-require_relative 'codegen_runner_options'
-require_relative 'codegen_runner_options_parser'
 
 class MainWork
   def self.work(arguments)
-    options = CodegenRunnerOptionsParser.parse_options(arguments)
-    CompoundPipeline.start(options)
+    options = {
+        list: CodegenConfig.make_all.map(&:options),
+        toolPath: File.expand_path("#{__dir__}/anytype_swift_codegen.rb"),
+        outputDirectory: File.expand_path(CodegenConfig::ProtobufDirectory),
+        formatToolPath: File.expand_path("#{__dir__}/swift_format.rb")
+    }
+
+    options[:list].each{ |value|
+      CodegenPipeline.start(options[:toolPath], value[:transform], value[:filePath])
+    }
+    FormatDirectoryPipeline.start(options)
   end
 end
 
--- Scripts/ruby/codegen/codegen_pipelines.rb
+++ Scripts/ruby/codegen/codegen_pipelines.rb
@@ -1,6 +1,5 @@
 require_relative '../pipeline_starter'
 require_relative 'codegen_workers_2'
-require_relative 'codegen_commands'
 
 class ListTransformsPipeline < BasePipeline
   def self.start(options)
@@ -56,11 +55,11 @@ module AnytypeSwiftCodegenPipeline
 end
 
 class CodegenPipeline < BasePipeline
-  def self.start(options)
-    if Dir.exists? options[:toolPath]
-      TravelerWorker.new(options[:toolPath]).work
+  def self.start(toolPath, transform, filePath)
+    if Dir.exists? toolPath
+      TravelerWorker.new(toolPath).work
     end
-    CodegenWorker.new(options[:toolPath], options[:transform], options[:filePath]).work
+    CodegenWorker.new(toolPath, transform, filePath).work
   end
 end
 
@@ -73,21 +72,5 @@ class FormatDirectoryPipeline < BasePipeline
     Dir.entries(directory).map{|f| File.join(directory, f)}.select{|f| File.file?(f) && File.extname(f) == '.swift'}.each{|f|
       FormatWorker.new(options[:formatToolPath], f).work
     }
-  end
-end
-
-class CompoundPipeline < BasePipeline
-  def self.start(options)
-    case options[:command]
-    when CodegenCommand then CodegenPipeline.start(options)
-    when CodegenListCommand then
-      options[:command].list.each{ |value|
-        CodegenPipeline.start(options.merge value)
-      }
-      FormatDirectoryPipeline.start(options)
-    else
-      puts "I don't recognize this command: #{options[:command]}"
-      return
-    end
   end
 end
