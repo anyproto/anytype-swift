9f2a5f5c8e1f0835b086812e7081eab4178870fd
Vladimir Ignatov <abbadoh@mail.ru>
2021-12-14 16:38:13 +0400

    Cleanup flow

--- Scripts/ruby/pipelines/version_provider.rb
+++ Scripts/ruby/pipelines/version_provider.rb
@@ -3,18 +3,10 @@ require_relative '../constants'
 require_relative '../library/lib_version'
 
 class VersionProvider
-  def self.version(options)
-    if options[:latest] 
-      latest_version(options)
-    else
-      version_from_library_file(options)
-    end
-  end
+  def self.version_from_library_file(token)
+    version = LibraryFile.get()
 
-  private_class_method def self.version_from_library_file(options)
-    version = LibraryVersion.get()
-
-    information = GetRemoteInformationWorker.new(options[:token]).work
+    information = GetRemoteInformationWorker.new(token).work
     versions = GetRemoteAvailableVersionsWorker.new(information).work
 
     if versions.include?(version)
@@ -26,8 +18,8 @@ class VersionProvider
     end
   end
 
-  private_class_method def self.latest_version(options)
-    information = GetRemoteInformationWorker.new(options[:token]).work
+  def self.latest_version(token)
+    information = GetRemoteInformationWorker.new(token).work
     version = GetRemoteVersionWorker.new(information).work
     puts "Latest version in remote <#{version}>"
 
