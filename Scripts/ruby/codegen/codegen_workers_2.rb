89f18864f269afaa78eb033d992006d8121764a6
Vladimir Ignatov <abbadoh@mail.ru>
2021-12-14 22:46:21 +0400

    Remove codegen pipeline

--- Scripts/ruby/codegen/codegen_workers_2.rb
+++ Scripts/ruby/codegen/codegen_workers_2.rb
@@ -1,17 +1,3 @@
-class CodegenWorker
-  attr_accessor :tool,:transform, :filePath
-  def initialize(tool, transform, filePath)
-    self.tool = tool
-    self.transform = transform
-    self.filePath = filePath
-  end
-
-  def work
-    action = "ruby #{tool} --transform #{transform} --filePath #{filePath}"
-    ShellExecutor.run_command_line action
-  end
-end
-
 class FormatWorker
   attr_accessor :tool, :filePath
   def initialize(tool, filePath)
