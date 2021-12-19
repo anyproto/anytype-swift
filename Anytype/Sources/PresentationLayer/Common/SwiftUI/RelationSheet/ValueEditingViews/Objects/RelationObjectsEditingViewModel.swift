2ce99d836c204c5344eabdabd055da16f1406db5
Konstantin Mordan <mordan2be@gmail.com>
2021-12-19 18:31:32 +0800

    Add dummy RelationObjectsEditingView

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationSheet/ValueEditingViews/Objects/RelationObjectsEditingViewModel.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationSheet/ValueEditingViews/Objects/RelationObjectsEditingViewModel.swift
@@ -1,5 +1,27 @@
 import Foundation
 
-final class RelationObjectsEditingViewModel {
+final class RelationObjectsEditingViewModel: ObservableObject {
+    
+    var dismissHandler: (() -> Void)?
+    
+    @Published var isPresented: Bool = false
+    @Published var selectedObjects: [Relation.Object.Option]
+    
+    let relationName: String
+    
+    private let relationKey: String
+    private let relationsService: RelationsServiceProtocol
+    private var editingActions: [TagRelationEditingAction] = []
+    
+    init(
+        relationObject: Relation.Object,
+        relationsService: RelationsServiceProtocol
+    ) {
+        self.selectedObjects = relationObject.selectedObjects
+        self.relationName = relationObject.name
+        self.relationKey = relationObject.id
+
+        self.relationsService = relationsService
+    }
     
 }
