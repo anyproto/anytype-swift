fd6439a815876668484ea6c36618eb9237815fac
Dmitry <bududomasidet@gmail.com>
2022-01-10 19:14:58 +0800

    TextComponent

--- Anytype/Sources/Design system/Colors/AnytypeColorProtocol.swift
+++ Anytype/Sources/Design system/Colors/AnytypeColorProtocol.swift
@@ -69,3 +69,19 @@ protocol AnytypeColorProtocol {
     static var grayscale10: T { get }
     
 }
+
+
+protocol ComponentColor {
+    associatedtype T
+
+    static var yellow: T { get }
+    static var amber: T { get }
+    static var red: T { get }
+    static var pink: T { get }
+    static var purple: T { get }
+    static var blue: T { get }
+    static var sky: T { get }
+    static var teal: T { get }
+    static var green: T { get }
+    static var grey: T { get }
+}
