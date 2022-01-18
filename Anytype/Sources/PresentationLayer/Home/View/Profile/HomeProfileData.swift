a614ae795efe295486e56a7541beb55a49f438a0
Vladimir Ignatov <abbadoh@mail.ru>
2022-01-18 11:20:05 +0200

    Fix HomeProfileData mutation

--- Anytype/Sources/PresentationLayer/Home/View/Profile/HomeProfileData.swift
+++ Anytype/Sources/PresentationLayer/Home/View/Profile/HomeProfileData.swift
@@ -3,11 +3,15 @@ import BlocksModels
 struct HomeProfileData {
     private static let defaultName = "Username"
     
-    private(set) var name = Self.defaultName
-    private(set) var avatarId: String?
-    private(set) var blockId: BlockId = ""
+    let name: String
+    let avatarId: String?
+    let blockId: BlockId
     
-    mutating func update(details: ObjectDetails) {
+    static let empty: HomeProfileData = HomeProfileData(name: Self.defaultName, avatarId: nil, blockId: "")
+}
+            
+extension HomeProfileData {
+    init(details: ObjectDetails) {
         name = details.name.isNotEmpty ? details.name : Self.defaultName
         avatarId = details.iconImageHash?.value
         blockId = details.id
