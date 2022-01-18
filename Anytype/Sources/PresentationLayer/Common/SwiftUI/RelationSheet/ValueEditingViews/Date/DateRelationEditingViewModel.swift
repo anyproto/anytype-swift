45cb4eb5890c828b03789622d7488b1aebae0edc
Konstantin Mordan <mordan2be@gmail.com>
2022-01-18 17:36:18 +0800

    Implement updating date on row select

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationSheet/ValueEditingViews/Date/DateRelationEditingViewModel.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationSheet/ValueEditingViews/Date/DateRelationEditingViewModel.swift
@@ -5,16 +5,18 @@ import SwiftProtobuf
 final class DateRelationEditingViewModel: ObservableObject {
     
     var dismissHandler: (() -> Void)?
-    
-    @Published var isPresented: Bool = false {
+
+    @Published var selectedValue: DateRelationEditingValue {
         didSet {
-            guard isPresented == false else { return }
-            
             saveValue()
         }
     }
-    @Published var selectedValue: DateRelationEditingValue = .noDate
-    @Published var date: Date
+    
+    @Published var date: Date {
+        didSet {
+            saveValue()
+        }
+    }
     
     let values = DateRelationEditingValue.allCases
     
@@ -28,12 +30,11 @@ final class DateRelationEditingViewModel: ObservableObject {
         value: DateRelationValue?,
         service: RelationsServiceProtocol
     ) {
+        self.selectedValue = value?.dateRelationEditingValue ?? .noDate
         self.relationKey = relationKey
         self.relationName = relationName
         self.date = value?.date ?? Date()
         self.service = service
-        
-        handleInitialValue(value)
     }
     
 }
@@ -65,27 +66,21 @@ extension DateRelationEditingViewModel: RelationEditingViewModelProtocol {
      
 }
 
-private extension DateRelationEditingViewModel {
+private extension DateRelationValue {
     
-    func handleInitialValue(_ value: DateRelationValue?) {
-        var selectedValue = DateRelationEditingValue.noDate
+    var dateRelationEditingValue: DateRelationEditingValue {
+        var value = DateRelationEditingValue.noDate
         
-        guard let value = value else {
-            self.selectedValue = selectedValue
-            return
-        }
-        
-        if Calendar.current.isDateInToday(value.date) {
-            selectedValue = .today
-        } else if Calendar.current.isDateInTomorrow(value.date) {
-            selectedValue = .tomorrow
-        } else if Calendar.current.isDateInYesterday(value.date) {
-            selectedValue = .yesterday
+        if Calendar.current.isDateInToday(self.date) {
+            value = .today
+        } else if Calendar.current.isDateInTomorrow(self.date) {
+            value = .tomorrow
+        } else if Calendar.current.isDateInYesterday(self.date) {
+            value = .yesterday
         } else {
-            selectedValue = .exactDay
+            value = .exactDay
         }
         
-        self.selectedValue = selectedValue
+        return value
     }
-    
 }
