5adcb6f7e5cfe3920bb86f2d3bee08c0dd7c9669
Konstantin Mordan <mordan2be@gmail.com>
2022-01-25 19:18:18 +0800

    RelationOptionsViewModel init refactoring

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationDetailsViews/ValueEditingViews/OptionsList/RelationOptionsViewModel.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationDetailsViews/ValueEditingViews/OptionsList/RelationOptionsViewModel.swift
@@ -8,28 +8,26 @@ final class RelationOptionsViewModel: ObservableObject {
     @Published var isPresented: Bool = false
     @Published var selectedOptions: [RelationOptionProtocol] = []
     
-    let title: String
-    let emptyPlaceholder: String
-    
     private let type: RelationOptionsType
-    private let relationKey: String
-    private let relationsService: RelationsServiceProtocol
+    private let relation: Relation
+    private let service: RelationsServiceProtocol
     private var editingActions: [RelationOptionEditingAction] = []
     
     init(
         type: RelationOptionsType,
-        title: String,
-        relationKey: String,
         selectedOptions: [RelationOptionProtocol],
-        relationsService: RelationsServiceProtocol
+        relation: Relation,
+        service: RelationsServiceProtocol
     ) {
         self.type = type
-        self.title = title
-        self.emptyPlaceholder = type.placeholder
-        self.relationKey = relationKey
         self.selectedOptions = selectedOptions
-        self.relationsService = relationsService
+        self.relation = relation
+        self.service = service
     }
+    
+    var title: String { relation.name }
+    
+    var emptyPlaceholder: String { type.placeholder }
     
 }
 
@@ -49,8 +47,8 @@ extension RelationOptionsViewModel {
             }
         }
         
-        relationsService.updateRelation(
-            relationKey: relationKey,
+        service.updateRelation(
+            relationKey: relation.id,
             value: selectedOptions.map { $0.id }.protobufValue
         )
         
@@ -91,11 +89,11 @@ extension RelationOptionsViewModel {
     
     private func searchViewModel(allTags: [Relation.Tag.Option]) -> TagRelationOptionSearchViewModel {
         TagRelationOptionSearchViewModel(
-            relationKey: relationKey,
+            relationKey: relation.id,
             availableTags: allTags.filter { tag in
                 !selectedOptions.contains { $0.id == tag.id }
             },
-            relationsService: relationsService
+            relationsService: service
         ) { [weak self] ids in
             self?.handleNewOptionIds(ids)
         }
@@ -104,8 +102,8 @@ extension RelationOptionsViewModel {
     private func handleNewOptionIds(_ ids: [String]) {
         let newSelectedOptionsIds = selectedOptions.map { $0.id } + ids
         
-        relationsService.updateRelation(
-            relationKey: relationKey,
+        service.updateRelation(
+            relationKey: relation.id,
             value: newSelectedOptionsIds.protobufValue
         )
         
