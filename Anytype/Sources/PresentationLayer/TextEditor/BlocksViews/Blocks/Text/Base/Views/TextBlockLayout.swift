76f9b7262c4b8a005ca6e43af9b3e3442c18e3d0
Dmitry <bududomasidet@gmail.com>
2022-02-17 14:41:12 +0800

    Content insets

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Text/Base/Views/TextBlockLayout.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Text/Base/Views/TextBlockLayout.swift
@@ -26,9 +26,11 @@ enum TextBlockLayout {
         case .description:
             return .zero
         case .header:
-            return .init(top: 5, leading: 0, bottom: -3, trailing: 0)
+            return .init(top: 5, leading: 0, bottom: -5, trailing: 0)
         case .header2:
             return .init(top: 5, leading: 0, bottom: -5, trailing: 0)
+        case .quote:
+            return .init(top: 10, leading: 0, bottom: -10, trailing: 0)
         default:
             return .init(top: 4, leading: 0, bottom: -4, trailing: 0)
         }
