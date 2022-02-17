59d1a20d951abd10b746e9e156d2795c852b7426
Denis Batvinkin <batvos7@gmail.com>
2022-02-17 15:00:04 +0300

    add feature flag

--- Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Relation/RelationBlockContentConfiguration.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/BlocksViews/Blocks/Relation/RelationBlockContentConfiguration.swift
@@ -1,6 +1,31 @@
-struct RelationBlockContentConfiguration: BlockConfiguration {
-    typealias View = RelationBlockView
+import UIKit
+import AnytypeCore
 
-    let relation: Relation
-    @EquatableNoop private(set) var actionOnValue: ((_ relation: Relation) -> Void)?
+//struct RelationBlockContentConfiguration: BlockConfiguration {
+//    typealias View = RelationBlockView
+//    @EquatableNoop private(set) var actionOnValue: ((_ relation: Relation) -> Void)?
+//    let relation: Relation
+//}
+
+struct RelationBlockContentConfiguration: BlockConfiguration, Hashable {
+    var currentConfigurationState: UICellConfigurationState?
+    var relation: Relation
+    var actionOnValue: ((_ relation: Relation) -> Void)?
+    
+    func makeContentView() -> UIView & UIContentView {
+        if FeatureFlags.uikitRelationBlock {
+            return RelationBlockView(configuration: self)
+        }
+        return RelationBlockViewDepricated(configuration: self)
+    }
+
+    static func == (lhs: RelationBlockContentConfiguration, rhs: RelationBlockContentConfiguration) -> Bool {
+        lhs.relation == rhs.relation &&
+        lhs.currentConfigurationState == rhs.currentConfigurationState
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(relation)
+        hasher.combine(currentConfigurationState)
+    }
 }
