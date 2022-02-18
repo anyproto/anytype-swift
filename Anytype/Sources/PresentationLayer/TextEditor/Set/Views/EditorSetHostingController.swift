d9c2cbeda4d9c5f546d723801ea62eec8e7fbcea
Vladimir Ignatov <abbadoh@mail.ru>
2022-02-18 10:59:28 +0300

    Hides navigation bar in sets

--- Anytype/Sources/PresentationLayer/TextEditor/Set/Views/EditorSetHostingController.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/Set/Views/EditorSetHostingController.swift
@@ -15,4 +15,14 @@ final class EditorSetHostingController: UIHostingController<EditorSetView> {
     @MainActor @objc required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
 }
