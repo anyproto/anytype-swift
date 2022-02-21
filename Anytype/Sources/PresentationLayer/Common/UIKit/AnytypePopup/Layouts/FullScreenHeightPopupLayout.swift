10bdfded05275fc0515770646ab54cd384bd22a6
Konstantin Mordan <mordan2be@gmail.com>
2022-02-21 15:36:39 +0800

    Fixed status relation details view layout

--- Anytype/Sources/PresentationLayer/Common/UIKit/AnytypePopup/Layouts/FullScreenHeightPopupLayout.swift
+++ Anytype/Sources/PresentationLayer/Common/UIKit/AnytypePopup/Layouts/FullScreenHeightPopupLayout.swift
@@ -1,6 +1,7 @@
 import Foundation
 import FloatingPanel
 import CoreGraphics
+import UIKit
 
 final class FullScreenHeightPopupLayout: FloatingPanelLayout {
     
@@ -15,6 +16,20 @@ final class FullScreenHeightPopupLayout: FloatingPanelLayout {
     
     func backdropAlpha(for state: FloatingPanelState) -> CGFloat {
         0.3
+    }
+    
+    func prepareLayout(surfaceView: UIView, in view: UIView) -> [NSLayoutConstraint] {
+        if UIDevice.isPad {
+            return [
+                surfaceView.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor),
+                surfaceView.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor)
+            ]
+        } else {
+            return [
+                surfaceView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                surfaceView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ]
+        }
     }
     
 }
