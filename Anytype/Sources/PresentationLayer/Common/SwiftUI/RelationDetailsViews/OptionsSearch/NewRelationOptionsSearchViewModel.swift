fce01159040ba1656b0425939c89b3554d23b7f9
Konstantin Mordan <mordan2be@gmail.com>
2022-03-07 15:45:40 +0800

    Add empty classes

---
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/RelationDetailsViews/OptionsSearch/NewRelationOptionsSearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NewRelationOptionsSearchViewModel.swift
+//  Anytype
+//
+//  Created by Konstantin Mordan on 05.03.2022.
+//  Copyright © 2022 Anytype. All rights reserved.
+//
+
+import Foundation
+
+final class NewRelationOptionsSearchViewModel {
+    
+}
