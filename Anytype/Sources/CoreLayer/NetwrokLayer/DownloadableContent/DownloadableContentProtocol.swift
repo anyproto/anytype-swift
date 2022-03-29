7cdffaaca83c81764ed21ba607ec5c8eee8c60e8
Konstantin Mordan <mordan2be@gmail.com>
2022-03-29 17:15:28 +0300

    Implemented DownloadingUrlBuilder

--- Anytype/Sources/CoreLayer/NetwrokLayer/DownloadableContent/DownloadableContentProtocol.swift
+++ Anytype/Sources/CoreLayer/NetwrokLayer/DownloadableContent/DownloadableContentProtocol.swift
@@ -1,7 +1,28 @@
 import Foundation
+import BlocksModels
 
 protocol DownloadableContentProtocol {
     
     var downloadingUrl: URL? { get }
+    
+}
+
+// MARK: - ImageMetadata
+
+extension ImageMetadata: DownloadableContentProtocol {
+    
+    var downloadingUrl: URL? {
+        DownloadingUrlBuilder.imageUrl(imageMetadata: self)
+    }
+    
+}
+
+// MARK: - FileMetadata
+
+extension FileMetadata: DownloadableContentProtocol {
+    
+    var downloadingUrl: URL? {
+        DownloadingUrlBuilder.fileUrl(fileId: self.hash)
+    }
     
 }
--- Anytype/Sources/Models/ImageMetadata.swift
+++ Anytype/Sources/Models/ImageMetadata.swift
@@ -2,20 +2,6 @@ import Foundation
 import UIKit
 
 struct ImageMetadata {
-    
-    private let id: String
-    private let width: ImageWidth
-    
-    init(id: String, width: ImageWidth) {
-        self.id = id
-        self.width = width
-    }
-}
-
-extension ImageMetadata: DownloadableContentProtocol {
-    
-    var downloadingUrl: URL? {
-        UrlResolver.resolvedUrl(.image(id: id, width: width))
-    }
-    
+    let id: String
+    let width: ImageWidth
 }
