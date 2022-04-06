870bb2c664ea931833f281297cac80aedb18c894
Konstantin Mordan <mordan2be@gmail.com>
2022-04-06 18:26:43 +0300

    Remove SearchKind

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/Search/Model/ObjectSearchData.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/Search/Model/ObjectSearchData.swift
@@ -11,12 +11,10 @@ struct ObjectSearchData: SearchDataProtocol {
     
     let blockId: BlockId
     
-    private let searchKind: SearchKind
     private let details: ObjectDetails
     
-    init(searchKind: SearchKind, details: ObjectDetails) {
+    init(details: ObjectDetails) {
         self.details = details
-        self.searchKind = searchKind
         self.title = details.title
         self.description = details.description
         self.callout = details.objectType.name
@@ -33,27 +31,19 @@ extension ObjectSearchData {
     }
     
     var descriptionTextColor: Color {
-        switch searchKind {
-        case .objects: return .textPrimary
-        }
+        .textPrimary
     }
     
     var descriptionFont: AnytypeFont {
-        switch searchKind {
-        case .objects: return .relation3Regular
-        }
+        .relation3Regular
     }
     
     var shouldShowCallout: Bool {
-        switch searchKind {
-        case .objects: return callout.isNotEmpty
-        }
+        callout.isNotEmpty
     }
     
     var verticalInset: CGFloat {
-        switch searchKind {
-        case .objects: return 16
-        }
+        16
     }
 
     var usecase: ObjectIconImageUsecase {
--- Anytype/Sources/PresentationLayer/Common/SwiftUI/Search/ObjectSearchViewModel.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/Search/ObjectSearchViewModel.swift
@@ -1,50 +1,25 @@
 import SwiftUI
 import BlocksModels
 
-enum SearchKind {
-    case objects
-    
-    var descriptionTextColor: Color {
-        switch self {
-        case .objects:
-            return .textPrimary
-        }
-    }
-    
-    var shouldShowCallout: Bool {
-        switch self {
-        case .objects:
-            return true
-        }
-    }
-}
-
 /// https://www.figma.com/file/TupCOWb8sC9NcjtSToWIkS/Mobile---main?node-id=6455%3A4097
 final class ObjectSearchViewModel: SearchViewModelProtocol {
+    
     @Published var searchData: [SearchDataSection<ObjectSearchData>] = []
+    
     var onSelect: (ObjectSearchData) -> ()
     var onDismiss: () -> () = {}
 
-    var placeholder: String {
-        return "Search".localized
-    }
-    
-    lazy var descriptionTextColor = searchKind.descriptionTextColor
-    lazy var shouldShowCallout = searchKind.shouldShowCallout
+    let placeholder: String = "Search".localized
     
     private let service = SearchService()
-    private let searchKind: SearchKind
     
-    init(searchKind: SearchKind, onSelect: @escaping (SearchDataType) -> ()) {
-        self.searchKind = searchKind
+    init(onSelect: @escaping (SearchDataType) -> ()) {
         self.onSelect = onSelect
     }
     
     func search(text: String) {
         let result = searchDetails(text: text)
-        let objectsSearchData = result?.compactMap { details in
-            ObjectSearchData(searchKind: searchKind, details: details)
-        }
+        let objectsSearchData = result?.compactMap { ObjectSearchData(details: $0) }
 
         guard let objectsSearchData = objectsSearchData, objectsSearchData.isNotEmpty else {
             searchData = []
@@ -55,9 +30,6 @@ final class ObjectSearchViewModel: SearchViewModelProtocol {
     }
     
     private func searchDetails(text: String) -> [ObjectDetails]? {
-        switch searchKind {
-        case .objects:
-            return service.search(text: text)
-        }
+        service.search(text: text)
     }
 }
