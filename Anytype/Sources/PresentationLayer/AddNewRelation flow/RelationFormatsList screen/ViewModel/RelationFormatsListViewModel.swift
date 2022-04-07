dceb0c0a51bc6c87aaa2724498d741842cfc840f
Konstantin Mordan <mordan2be@gmail.com>
2022-04-07 12:03:40 +0300

    Fixed code review

--- Anytype/Sources/PresentationLayer/AddNewRelation flow/RelationFormatsList screen/ViewModel/RelationFormatsListViewModel.swift
+++ Anytype/Sources/PresentationLayer/AddNewRelation flow/RelationFormatsList screen/ViewModel/RelationFormatsListViewModel.swift
@@ -8,14 +8,7 @@ final class RelationFormatsListViewModel: ObservableObject {
     
     init(output: RelationFormatsListModuleOutput) {
         self.output = output
-        self.supportedFormatModels = SupportedRelationFormat.allCases.map {
-            RelationFormatListCell.Model(
-                id: $0.id,
-                title: $0.title,
-                icon: $0.icon,
-                isSelected: false
-            )
-        }
+        self.supportedFormatModels = SupportedRelationFormat.allCases.asRelationFormatListCellModels
     }
     
 }
@@ -25,6 +18,21 @@ extension RelationFormatsListViewModel {
     func didSelectFormat(id: String) {
         guard let format = SupportedRelationFormat(rawValue: id) else { return }
         output?.didSelectFormat(format)
+    }
+    
+}
+
+private extension Array where Element == SupportedRelationFormat {
+    
+    var asRelationFormatListCellModels: [RelationFormatListCell.Model] {
+        self.map {
+            RelationFormatListCell.Model(
+                id: $0.id,
+                title: $0.title,
+                icon: $0.icon,
+                isSelected: false
+            )
+        }
     }
     
 }
