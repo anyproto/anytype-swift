edbaa792057a94f80e63585fc4c11cbfedbb162a
Konstantin Mordan <mordan2be@gmail.com>
2022-04-13 12:33:45 +0300

    Add AnytypeID

--- Modules/AnytypeCore/AnytypeCore/Structs/AnytypeID.swift
+++ Modules/AnytypeCore/AnytypeCore/Structs/AnytypeID.swift
@@ -1,8 +1,17 @@
-//
-//  AnytypeID.swift
-//  AnytypeCore
-//
-//  Created by Konstantin Mordan on 12.04.2022.
-//
-
 import Foundation
+
+public struct AnytypeID: Hashable {
+    
+    public let value: String
+    
+    // MARK: - Initializer
+    
+    public init?(_ value: String?) {
+        guard let value = value?.trimmed, value.isNotEmpty else {
+            return nil
+        }
+        
+        self.value = value
+    }
+    
+}
