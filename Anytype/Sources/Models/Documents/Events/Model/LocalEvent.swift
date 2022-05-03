13bb8772add642e2c0d1514c391aa25c7bbea879
Dmitry <bududomasidet@gmail.com>
2022-05-03 15:19:03 +0800

    Carret doesn't move to description block on enter

--- Anytype/Sources/Models/Documents/Events/Model/LocalEvent.swift
+++ Anytype/Sources/Models/Documents/Events/Model/LocalEvent.swift
@@ -1,6 +1,8 @@
 import BlocksModels
 
 enum LocalEvent {
+    case general
+    case setStyle(blockId: AnytypeId)
     case setToggled(blockId: BlockId)
     case setText(blockId: BlockId, text: MiddlewareString)
     case setLoadingState(blockId: BlockId)
