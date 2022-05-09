ec9d31ce86b8a3cd4d1c38379a3a2ebbda2a19ce
Dmitry <bududomasidet@gmail.com>
2022-05-09 18:09:18 +0800

    Separate view

--- Anytype/Sources/PresentationLayer/ToastPresenter/ToastPresenter.swift
+++ Anytype/Sources/PresentationLayer/ToastPresenter/ToastPresenter.swift
@@ -4,6 +4,7 @@ class ToastPresenter {
     private var isShowing: Bool = false
 
     private let rootViewController: UIViewController
+    private lazy var toastView = ToastView(frame: .zero)
 
     init(rootViewController: UIViewController) {
         self.rootViewController = rootViewController
@@ -13,61 +14,24 @@ class ToastPresenter {
         if isShowing { return }
 
         isShowing = true
+        toastView.alpha = 0.0
+        toastView.setMessage(message)
 
-        let toastContainer = ThroughHitView(frame: .zero)
-        toastContainer.backgroundColor = .clear
-        toastContainer.alpha = 0.0
-
-        let toastLabel = UILabel(frame: CGRect())
-        toastLabel.textColor = .textWhite
-        toastLabel.textAlignment = .center;
-        toastLabel.font = AnytypeFont.caption1Medium.uiKitFont
-        toastLabel.text = message
-        toastLabel.numberOfLines = 0
-
-        let decoratedView = UIView()
-        decoratedView.backgroundColor = .buttonActive
-        decoratedView.layer.cornerRadius = 8
-        decoratedView.layer.masksToBounds = true
-
-        toastContainer.addSubview(decoratedView) {
-            $0.centerX.equal(to: toastContainer.centerXAnchor)
-        }
-
-        NSLayoutConstraint(
-            item: decoratedView,
-            attribute: .centerY,
-            relatedBy: .equal,
-            toItem: toastContainer,
-            attribute: .centerY,
-            multiplier: 1.5,
-            constant: 0
-        ).isActive = true
-
-
-        decoratedView.addSubview(toastLabel) {
-            $0.pinToSuperview(insets: .init(top: 8, left: 8, bottom: -8, right: -8))
-        }
-
-        rootViewController.topPresentedController.view.addSubview(toastContainer) {
+        rootViewController.topPresentedController.view.addSubview(toastView) {
             $0.pinToSuperview()
         }
 
         UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
-            toastContainer.alpha = 1.0
-        }, completion: { [weak self] _ in
+            self.toastView.alpha = 1.0
+        }, completion: { _ in
             UIView.animate(withDuration: 0.2, delay: 0.5, options: .curveEaseOut, animations: {
-                toastContainer.alpha = 0.0
-                self?.isShowing = false
+                self.toastView.alpha = 0.0
+                self.isShowing = false
             }, completion: { _ in
-                toastContainer.removeFromSuperview()
+                self.toastView.removeFromSuperview()
             })
         })
     }
 }
 
-private class ThroughHitView: UIView {
-    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        return nil
-    }
-}
+
---
+++ Anytype/Sources/PresentationLayer/ToastPresenter/ToastView.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+// Non-final toast view. Should be designed by design team
+final class ToastView: ThroughHitView {
+    private lazy var label = UILabel(frame: .zero)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        setupView()
+    }
+
+    func setMessage(_ message: String) {
+        label.text = message
+    }
+
+    private func setupView() {
+        backgroundColor = .clear
+
+        label.textColor = .textWhite
+        label.textAlignment = .center;
+        label.font = AnytypeFont.caption1Medium.uiKitFont
+        label.numberOfLines = 0
+
+        let decoratedView = UIView()
+        decoratedView.backgroundColor = .buttonActive
+        decoratedView.layer.cornerRadius = 8
+        decoratedView.layer.masksToBounds = true
+
+        addSubview(decoratedView) {
+            $0.centerX.equal(to: centerXAnchor)
+        }
+
+        NSLayoutConstraint(
+            item: decoratedView,
+            attribute: .centerY,
+            relatedBy: .equal,
+            toItem: self,
+            attribute: .centerY,
+            multiplier: 1.5,
+            constant: 0
+        ).isActive = true
+
+
+        decoratedView.addSubview(label) {
+            $0.pinToSuperview(insets: .init(top: 8, left: 8, bottom: -8, right: -8))
+        }
+    }
+}
+
+class ThroughHitView: UIView {
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        return nil
+    }
+}
