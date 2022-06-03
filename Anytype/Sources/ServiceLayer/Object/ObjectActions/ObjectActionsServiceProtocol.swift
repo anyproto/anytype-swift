7c48eb5dba065fd36cb093737e1f055cad8ea9c9
Denis Batvinkin <batvos7@gmail.com>
2022-06-03 18:56:34 +0300

    add event handling in ListConvertToObjects

--- Anytype/Sources/ServiceLayer/Object/ObjectActions/ObjectActionsServiceProtocol.swift
+++ Anytype/Sources/ServiceLayer/Object/ObjectActions/ObjectActionsServiceProtocol.swift
@@ -9,7 +9,7 @@ protocol ObjectActionsServiceProtocol {
     func setArchive(objectId: BlockId, _ isArchived: Bool)
     func setArchive(objectIds: [BlockId], _ isArchived: Bool)
     func setFavorite(objectId: BlockId, _ isFavorite: Bool)
-    func convertChildrenToPages(contextID: BlockId, blocksIds: [BlockId]) -> BlockId?
+    func convertChildrenToPages(contextID: BlockId, blocksIds: [BlockId], objectType: String) -> [BlockId]?
     func updateBundledDetails(contextID: BlockId, details: [BundledDetails])
     func move(dashboadId: BlockId, blockId: BlockId, dropPositionblockId: BlockId, position: Anytype_Model_Block.Position)
     func setLocked(_ isLocked: Bool, objectId: BlockId)
