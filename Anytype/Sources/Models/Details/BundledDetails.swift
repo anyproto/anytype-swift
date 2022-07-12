644b43f88aaebcbc74667d0fd1dab92f6558e391
Anna Zakharova <joe_pusya@anytype.io>
2022-07-12 17:58:29 +0300

    Add possibility to check/uncheck tasks from set

--- Anytype/Sources/Models/Details/BundledDetails.swift
+++ Anytype/Sources/Models/Details/BundledDetails.swift
@@ -20,6 +20,7 @@ enum BundledDetails {
     case coverType(CoverType)
     case type(ObjectTypeUrl)
     case isDraft(Bool)
+    case done(Bool)
 }
 
 extension BundledDetails {
@@ -33,6 +34,7 @@ extension BundledDetails {
         case .coverType: return BundledRelationKey.coverType.rawValue
         case .type: return BundledRelationKey.type.rawValue
         case .isDraft: return BundledRelationKey.isDraft.rawValue
+        case .done: return BundledRelationKey.done.rawValue
         }
     }
     
@@ -45,6 +47,7 @@ extension BundledDetails {
         case .coverType(let coverType): return coverType.rawValue.protobufValue
         case .type(let ObjectTypeUrl): return ObjectTypeUrl.rawValue.protobufValue
         case .isDraft(let bool): return bool.protobufValue
+        case .done(let bool): return bool.protobufValue
         }
     }
     
