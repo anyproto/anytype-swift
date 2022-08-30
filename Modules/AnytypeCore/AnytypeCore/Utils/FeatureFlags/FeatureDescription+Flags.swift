9a0408fb98bc21840d2f9a6a430f1cf12ab58325
Konstantin Mordan <mordan2be@gmail.com>
2022-08-30 16:04:21 +0300

    IOS-388 Debug | Fix | Disable Object Preview for debug builds

--- Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
+++ Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
@@ -5,8 +5,9 @@ extension FeatureDescription {
     static let objectPreview = FeatureDescription(
         title: "Object preview",
         author: "k@anytype.io",
-        releaseVersion: "?",
-        defaultValue: false
+        releaseVersion: "-",
+        defaultValue: false,
+        debugValue: false
     )
     
     static let setFilters = FeatureDescription(
