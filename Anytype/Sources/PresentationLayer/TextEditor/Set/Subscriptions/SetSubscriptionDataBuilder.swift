d7fd8189cb84eccd31f71c1f303fa5b621146acc
Milkhail Golovko <mihailmvg@gmail.com>
2022-09-15 15:39:33 +0300

    IOS-474 Add set builder

---
+++ Anytype/Sources/PresentationLayer/TextEditor/Set/Subscriptions/SetSubscriptionDataBuilder.swift
@@ -0,0 +1,51 @@
+import Foundation
+import BlocksModels
+
+final class SetSubscriptionDataBuilder: SetSubscriptionDataBuilderProtocol {
+    
+    // MARK: - SetSubscriptionDataBuilderProtocol
+    
+    func set(_ data: SetSubsriptionData) -> SubscriptionData {
+        let numberOfRowsPerPageInSubscriptions = UserDefaultsConfig.rowsPerPageInSet
+
+        let keys = buildKeys(with: data)
+        
+        let offset = (data.currentPage - 1) * numberOfRowsPerPageInSubscriptions
+        
+        return .search(
+            SubscriptionDescriptionSearch(
+                identifier: SubscriptionId.set,
+                sorts: data.sorts,
+                filters: data.filters,
+                limit: numberOfRowsPerPageInSubscriptions,
+                offset: offset,
+                keys: keys,
+                source: data.source
+            )
+        )
+    }
+    
+    
+    func buildKeys(with data: SetSubsriptionData) -> [String] {
+        
+        var keys = [
+            BundledRelationKey.id.rawValue,
+            BundledRelationKey.iconEmoji.rawValue,
+            BundledRelationKey.iconImage.rawValue,
+            BundledRelationKey.name.rawValue,
+            BundledRelationKey.snippet.rawValue,
+            BundledRelationKey.description.rawValue,
+            BundledRelationKey.type.rawValue,
+            BundledRelationKey.layout.rawValue,
+            BundledRelationKey.isArchived.rawValue,
+            BundledRelationKey.isDeleted.rawValue,
+            BundledRelationKey.done.rawValue,
+            BundledRelationKey.isFavorite.rawValue
+        ]
+        
+        keys.append(contentsOf: data.options.map { $0.key })
+        keys.append(data.coverRelationKey)
+        
+        return keys
+    }
+}
---
+++ Anytype/Sources/PresentationLayer/TextEditor/Set/Subscriptions/SetSubscriptionDataBuilderProtocol.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol SetSubscriptionDataBuilderProtocol: AnyObject {
+    func set(_ data: SetSubsriptionData) -> SubscriptionData
+}
