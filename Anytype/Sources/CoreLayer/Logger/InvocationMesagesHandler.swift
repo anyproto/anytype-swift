c4a2d6080c7e561311c0a293571355224ff9d6fe
Milkhail Golovko <mihailmvg@gmail.com>
2022-11-22 11:51:04 +0300

    IOS-683 Fix crash

--- Anytype/Sources/CoreLayer/Logger/InvocationMesagesHandler.swift
+++ Anytype/Sources/CoreLayer/Logger/InvocationMesagesHandler.swift
@@ -5,11 +5,40 @@ import ProtobufMessages
 final class InvocationMesagesHandler: InvocationMesagesHandlerProtocol {
     
     func handle(message: InvocationMessage) {
+        
+        // Delete emoji. Fix pulse crash https://github.com/kean/PulsePro/issues/22
+        let stringWithoutCombinedEmoji = message.responseJsonData
+            .flatMap { String(data: $0, encoding: .utf8) }?
+            .map { String($0.map { $0.isEmoji ? Character(" ") : $0 }) }
+
+        let responseJsonData = stringWithoutCombinedEmoji?.data(using: .utf8)
+
         NetworkingLogger.logNetwork(
             name: message.name,
             requestData: message.requestJsonData,
-            responseData: message.responseJsonData,
+            responseData: responseJsonData,
             responseError: message.responseError
         )
     }
+}
+
+private extension Character {
+   var isSimpleEmoji: Bool {
+      guard let firstProperties = unicodeScalars.first?.properties else {
+        return false
+      }
+      return unicodeScalars.count == 1 &&
+          (firstProperties.isEmojiPresentation ||
+             firstProperties.generalCategory == .otherSymbol)
+   }
+   var isCombinedIntoEmoji: Bool {
+      return unicodeScalars.count > 1 &&
+             unicodeScalars.contains {
+                $0.properties.isJoinControl ||
+                $0.properties.isVariationSelector
+             }
+   }
+   var isEmoji: Bool {
+      return isSimpleEmoji || isCombinedIntoEmoji
+   }
 }
