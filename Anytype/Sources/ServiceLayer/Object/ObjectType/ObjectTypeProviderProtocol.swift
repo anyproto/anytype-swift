41bfcc0a3142a66aaefa39f80ec4708b8e27d585
Milkhail Golovko <mihailmvg@gmail.com>
2022-11-30 18:53:22 +0300

    IOS-708 Migrate to black type list for search

--- Anytype/Sources/ServiceLayer/Object/ObjectType/ObjectTypeProviderProtocol.swift
+++ Anytype/Sources/ServiceLayer/Object/ObjectType/ObjectTypeProviderProtocol.swift
@@ -1,31 +1,16 @@
-//
-//  ObjectTypeProviderProtocol.swift
-//  Anytype
-//
-//  Created by Konstantin Mordan on 03.06.2022.
-//  Copyright © 2022 Anytype. All rights reserved.
-//
-
 import Foundation
 import BlocksModels
 
 protocol ObjectTypeProviderProtocol: AnyObject {
-    var supportedTypeIds: [String] { get }
+
     var defaultObjectType: ObjectType { get }
     
-    func isSupported(typeId: String) -> Bool
+    func isSupportedForEdit(typeId: String) -> Bool
     func objectType(id: String) -> ObjectType?
     
     func objectTypes(smartblockTypes: Set<SmartBlockType>) -> [ObjectType]
-    func visibleSupportedTypeIds(excludeTypeIds: [String]) -> [String]
+    func notVisibleTypeIds() -> [String]
     
     func startSubscription()
     func stopSubscription()
-}
-
-// Default arguments
-extension ObjectTypeProviderProtocol {
-    func visibleSupportedTypeIds() -> [String] {
-        visibleSupportedTypeIds(excludeTypeIds: [])
-    }
 }
