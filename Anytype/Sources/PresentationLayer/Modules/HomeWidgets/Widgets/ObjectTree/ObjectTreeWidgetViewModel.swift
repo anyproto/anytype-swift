da0df9037b21c5f15e369151de08957e4433e11c
Milkhail Golovko <mihailmvg@gmail.com>
2022-12-23 11:54:30 +0300

    IOS-753 Delete field

--- Anytype/Sources/PresentationLayer/Modules/HomeWidgets/Widgets/ObjectTree/ObjectTreeWidgetViewModel.swift
+++ Anytype/Sources/PresentationLayer/Modules/HomeWidgets/Widgets/ObjectTree/ObjectTreeWidgetViewModel.swift
@@ -6,8 +6,6 @@ final class ObjectTreeWidgetViewModel: ObservableObject {
     @Published var name: String
     @Published var isEexpanded: Bool
     
-    var objectId: UUID = UUID()
-    
     init(name: String) {
         self.name = name
         self.isEexpanded = false
