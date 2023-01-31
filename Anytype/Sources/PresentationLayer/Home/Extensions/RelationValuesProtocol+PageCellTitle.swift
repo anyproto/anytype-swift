058afe90c63f155c3e0b847d8ee6d2e36c6177dc
Mikhail Golovko <m@anytype.io>
2023-01-31 18:48:28 +0300

    IOS-844 Fix description logic

--- Anytype/Sources/PresentationLayer/Home/Extensions/RelationValuesProtocol+PageCellTitle.swift
+++ Anytype/Sources/PresentationLayer/Home/Extensions/RelationValuesProtocol+PageCellTitle.swift
@@ -22,12 +22,17 @@ extension BundledRelationsValueProvider {
 
         switch layoutValue {
         case .note:
-            title = snippet
+            title = firstLineSnippet
         default:
             title = name
         }
 
         return title.isEmpty ? Loc.untitled : title
+    }
+    
+    var subtitle: String {
+        return (type == ObjectTypeId.BundledTypeId.note.rawValue || description.isNotEmpty)
+            ? description : firstLineSnippet
     }
     
     var homeLayout: HomeCellData.TitleLayout {
@@ -41,5 +46,9 @@ extension BundledRelationsValueProvider {
     
     var mentionTitle: String {
         String(title.prefix(30)).replacingOccurrences(of: "\n", with: " ")
+    }
+    
+    private var firstLineSnippet: String {
+        return snippet.components(separatedBy: CharacterSet.newlines).first ?? snippet
     }
 }
