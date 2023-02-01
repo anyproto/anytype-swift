334361d21a6118b92194b6831318103fc10d0a04
Mikhail Golovko <m@anytype.io>
2023-02-01 12:40:46 +0300

    IOS-808 Add keys

---
+++ Modules/AnytypeCore/AnytypeCore/Extensions/Array+Extensions.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+public extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+    
+    func uniqued() -> [Element] where Element: Hashable {
+        var result: [Element] = []
+        for element in self {
+            if !result.contains(element) {
+                result.append(element)
+            }
+        }
+        return result
+    }
+}
