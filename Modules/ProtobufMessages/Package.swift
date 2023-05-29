b07421bd9ec0fb1fb62c912a1fc276ef682f7a4c
Mikhail Golovko <m@anytype.io>
2023-05-29 18:06:13 +0300

    IOS-1281 Fix repo

--- Modules/ProtobufMessages/Package.swift
+++ Modules/ProtobufMessages/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf", revision: "1.21.0"),
-        .package(url: "git@github.com:anytypeio/anytype-swift-codegen.git", revision: "0.0.11"),
+        .package(url: "git@github.com:anyproto/anytype-swift-codegen.git", revision: "0.0.11"),
         .package(url: "https://github.com/anyproto/SourceryGenPlugin.git", revision: "1.9.2")
     ],
     targets: [
