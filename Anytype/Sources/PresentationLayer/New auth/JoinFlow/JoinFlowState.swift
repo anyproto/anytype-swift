0393c44ab621afec47d6a054b5a3eab3b715a68e
Anna Zakharova <joe_pusya@anytype.io>
2023-06-06 18:22:56 +0100

    IOS-1256 Fix keyShown state

--- Anytype/Sources/PresentationLayer/New auth/JoinFlow/JoinFlowState.swift
+++ Anytype/Sources/PresentationLayer/New auth/JoinFlow/JoinFlowState.swift
@@ -3,6 +3,10 @@ import SwiftUI
 final class JoinFlowState {
     var soul = ""
     var inviteCode = "elbrus" // temp, wait for the middle release
-    var mnemonic = ""
+    var mnemonic = "" {
+        didSet {
+            keyShown = false
+        }
+    }
     var keyShown = false
 }
