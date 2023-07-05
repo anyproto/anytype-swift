90da2b37c23ee9d4fedd673673a50372b3b00a07
Anna Zakharova <joe_pusya@anytype.io>
2023-07-05 17:23:01 +0100

    IOS-1491 Remove extension UINavigationController

--- Anytype/Sources/PresentationLayer/Common/UIKit/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
+++ Anytype/Sources/PresentationLayer/Common/UIKit/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -1,16 +1,5 @@
 import UIKit
 
-//extension UINavigationController: UIGestureRecognizerDelegate {
-//    override open func viewDidLoad() {
-//        super.viewDidLoad()
-//        interactivePopGestureRecognizer?.delegate = self
-//    }
-//
-//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//        return viewControllers.count > 1
-//    }
-//}
-
 final class BaseNavigationController: UINavigationController, UIGestureRecognizerDelegate {
     var disableBackSwipe = false
     
