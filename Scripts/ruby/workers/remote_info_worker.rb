cb793dddbe3363ea7081cbc22697babd114ca6ef
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-07-24 19:47:51 +0200

    Remove github token for downloading middleware.

--- Scripts/ruby/workers/remote_info_worker.rb
+++ Scripts/ruby/workers/remote_info_worker.rb
@@ -3,34 +3,17 @@ require 'json'
 
 require_relative '../constants'
 
-# version=`curl -H "Authorization: token $token" -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases | jq ".[] | select(.tag_name == \"$MIDDLEWARE_VERSION_BY_TAG_NAME\")"`
+# version=`curl -H "Accept: application/vnd.github.v3+json" -sL https://$GITHUB/repos/$REPO/releases | jq ".[] | select(.tag_name == \"$MIDDLEWARE_VERSION_BY_TAG_NAME\")"`
 class GetRemoteInformationWorker
   attr_accessor :token
   def initialize(token)
     self.token = token
   end
 
-  def is_valid?
-    (self.token || '').empty? == false
-  end
-
   def work
-    unless is_valid?
-      puts <<-__REASON__
-      Access token does not exist. 
-      Please, provide it by cli argument or environment variable. 
-      Run `ruby #{$0} --help`
-      __REASON__
-      exit(1)
-    end
-    perform_work
-  end
-  
-  def perform_work
-    # fetch curl -H "Authorization: token Token" -H "Accept: application/vnd.github.v3+json" -sL https://api.github.com/repos/anytypeio/go-anytype-middleware/releases
+    # fetch curl -H "Accept: application/vnd.github.v3+json" -sL https://api.github.com/repos/anytypeio/go-anytype-middleware/releases
     uri = URI(Constants::REPOSITORY_URL)
     request = Net::HTTP::Get.new(uri)
-    request["Authorization"] = "token #{token}"
     request["Accept"] = "application/vnd.github.v3+json"
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
       http.request(request)
