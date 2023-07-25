7cf3cb5989d1ca96af46371186757e1441db018c
willshtorm <140494125+willshtorm@users.noreply.github.com>
2023-07-25 11:29:06 +0300

    Update AppDelegate.swift

--- Anytype/Sources/ApplicationLayer/AppDelegate.swift
+++ Anytype/Sources/ApplicationLayer/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        
+        // 1
         // Fix SIGPIPE crashes
         signal(SIGPIPE, SIG_IGN)
         
