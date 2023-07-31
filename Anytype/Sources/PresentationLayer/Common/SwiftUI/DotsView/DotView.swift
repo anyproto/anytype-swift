d96ef7d8456a67ac618ffd2f7c4c71edad9e55e6
Mikhail Golovko <m@anytype.io>
2023-07-31 13:23:50 +0300

    IOS-1185 Update button dots animation

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/DotsView/DotView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/DotsView/DotView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct DotView: View {
-    var delay: Double = 0
-    @State private var filled = false
+    
+    var filled = false
     
     var body: some View {
         Circle()
@@ -11,11 +11,6 @@ struct DotView: View {
                 if filled {
                     Circle().fill(.foreground)
                 }
-            }
-            .animation(Animation.linear(duration: 0.5).repeatForever().delay(delay), value: UUID())
-            .task {
-                try? await Task.sleep(seconds: 0.1)
-                filled.toggle()
             }
     }
 }
