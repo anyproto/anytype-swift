e3499edafd55a911b04f433285aeca61ae5d0d69
Anna Zakharova <joe_pusya@anytype.io>
2023-09-11 10:33:43 +0100

    IOS-1641 Fix }

--- Anytype/Sources/PresentationLayer/Assemblies/ModulesDI.swift
+++ Anytype/Sources/PresentationLayer/Assemblies/ModulesDI.swift
@@ -220,6 +220,7 @@ final class ModulesDI: ModulesDIProtocol {
 
     func shareAssembly() -> ShareModuleAssemblyProtocol {
         return ShareModuleAssembly(serviceLocator: serviceLocator)
+    }
     
     func setViewPicker() -> SetViewPickerModuleAssemblyProtocol {
         SetViewPickerModuleAssembly(serviceLocator: serviceLocator)
