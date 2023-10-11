d191f24e3accdf1bfe040ea93f94e3be4d2e1c26
Mikhail Golovko <m@anytype.io>
2023-10-11 15:33:11 +0300

    IOS-1816 Fixes

--- Scripts/middle-install.sh
+++ Scripts/middle-install.sh
@@ -14,14 +14,14 @@ if [ "$CI" = true ] ; then
         printf "MIDDLEWARE_TOKEN is not set\n"
         exit 1
     fi
+    token=${MIDDLEWARE_TOKEN}
 else
     has_token=$(has-keychain-environment-variable MIDDLEWARE_TOKEN)
     if [ ${has_token} == 0 ] ; then
         set-keychain-environment-variable MIDDLEWARE_TOKEN
     fi
+    token=$(keychain-environment-variable MIDDLEWARE_TOKEN)
 fi
-
-token=$(keychain-environment-variable MIDDLEWARE_TOKEN)
 
 curl https://maven.pkg.github.com/anyproto/anytype-heart/io.anyproto/anytype-heart-ios/${MIDDLE_VERSION}/anytype-heart-ios-${MIDDLE_VERSION}.gz -f --header "Authorization: token ${token}" -L --output ${PROJECT_DIR}/lib.gz
 rm -rf ${PROJECT_DIR}/Dependencies/Middleware/*
