117bd517a3d29a864c2f5126b5547d8a89cba2c4
Mikhail Golovko <m@anytype.io>
2023-11-02 18:30:33 +0300

    IOS-1796 Fix

--- Anytype/Sources/ApplicationLayer/AppConfigurators/Configurators/TipsConfiguration.swift
+++ Anytype/Sources/ApplicationLayer/AppConfigurators/Configurators/TipsConfiguration.swift
@@ -5,15 +5,18 @@ import AnytypeCore
 final class TipsConfiguration: AppConfiguratorProtocol {
         
     func configure() {
-        if #available(iOS 17, *) {
-            if FeatureFlags.resetTips {
-                try? Tips.resetDatastore()
+        if #available(iOS 17.0, *) {
+            do {
+                if FeatureFlags.resetTips {
+                    try Tips.resetDatastore()
+                }
+                if FeatureFlags.showAllTips {
+                    Tips.showAllTipsForTesting()
+                }
+                try Tips.configure([.displayFrequency(.daily), .datastoreLocation(.applicationDefault)])
+            } catch {
+                anytypeAssertionFailure("Tips setup error", info: ["tipsError": error.localizedDescription])
             }
-            if FeatureFlags.showAllTips {
-                Tips.showAllTipsForTesting()
-            }
-            try? Tips.configure([.displayFrequency(.daily)])
         }
-
     }
 }
