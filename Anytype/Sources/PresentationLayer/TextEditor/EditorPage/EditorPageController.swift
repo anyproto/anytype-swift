11aeef153bd3ad9365ac4edfe74faaaa3e0d7785
Mikhail Golovko <m@anytype.io>
2023-11-20 17:07:45 +0300

    IOS-1574 Fix bottom dnd

--- Anytype/Sources/PresentationLayer/TextEditor/EditorPage/EditorPageController.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/EditorPage/EditorPageController.swift
@@ -458,8 +458,7 @@ private extension EditorPageController {
     
     func setupView() {
         view.backgroundColor = .Background.primary
-        // 44 For custom navigation view
-        additionalSafeAreaInsets = UIEdgeInsets(top: 44, left: 0, bottom: 0, right: 0)
+        additionalSafeAreaInsets = UIEdgeInsets(top: 44, left: 0, bottom: 60, right: 0)
         setupCollectionView()
         
         setupInteractions()
