dcfa451506088ea9aabd18f1f3ab1e5e1057b6e3
Anna Zakharova <joe_pusya@anytype.io>
2024-01-08 16:44:22 +0000

    IOS-2144 Support new useCases like on desktop

--- Anytype/Sources/ServiceLayer/Object/Usecase/UsecaseService.swift
+++ Anytype/Sources/ServiceLayer/Object/Usecase/UsecaseService.swift
@@ -2,14 +2,14 @@ import ProtobufMessages
 import Combine
 
 protocol UsecaseServiceProtocol {
-    func setObjectImportUseCaseToSkip(spaceId: String) async throws
+    func setObjectImportDefaultUseCase(spaceId: String) async throws
 }
 
 final class UsecaseService: UsecaseServiceProtocol {
     
-    func setObjectImportUseCaseToSkip(spaceId: String) async throws {
+    func setObjectImportDefaultUseCase(spaceId: String) async throws {
         try await ClientCommands.objectImportUseCase(.with {
-            $0.useCase = .skip
+            $0.useCase = .getStarted
             $0.spaceID = spaceId
         }).invoke()
     }
