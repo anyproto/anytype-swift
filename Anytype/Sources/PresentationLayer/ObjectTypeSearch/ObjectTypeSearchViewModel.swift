bf3a2774b71711bf6393778e974ed4f769fbfaf6
Vova Ignatov <abbadoh@gmail.com>
2024-01-30 17:51:23 +0000

    IOS-2091 Add support for multi-sectioned types representation

--- Anytype/Sources/PresentationLayer/ObjectTypeSearch/ObjectTypeSearchViewModel.swift
+++ Anytype/Sources/PresentationLayer/ObjectTypeSearch/ObjectTypeSearchViewModel.swift
@@ -1,41 +1,71 @@
 import SwiftUI
 import Services
 
-extension ObjectTypeSearchViewModel {
-    enum State {
-        case showTypes([ObjectType])
-        case emptyScreen
-    }
-}
 
 @MainActor
 final class ObjectTypeSearchViewModel: ObservableObject {
-    @Published var state = State.showTypes([])
+    @Published var state = State.searchResults([])
     
     private let onSelect: (_ type: ObjectType) -> Void
     
     private let interactor: ObjectTypeSearchInteractor
+    private let toastPresenter: ToastPresenterProtocol
     
     nonisolated init(
         onSelect: @escaping (_ type: ObjectType) -> Void,
-        interactor: ObjectTypeSearchInteractor
+        interactor: ObjectTypeSearchInteractor,
+        toastPresenter: ToastPresenterProtocol
     ) {
         self.onSelect = onSelect
         self.interactor = interactor
+        self.toastPresenter = toastPresenter
     }
     
     func search(text: String) {
         Task {
-            let objectTypes = await interactor.search(text: text)
+            var sectionData: [SectionData] = []
+            
+            let listTypes = await interactor.searchListTypes(text: text)
+            if listTypes.isNotEmpty {
+                sectionData.append(
+                    SectionData(section: .lists, types: listTypes)
+                )
+            }
+            
+            let objectTypes = await interactor.searchObjectTypes(text: text)
             if objectTypes.isNotEmpty {
-                state = .showTypes(objectTypes)
+                sectionData.append(
+                    SectionData(section: .objects, types: objectTypes)
+                )
+            }
+            
+            if text.isNotEmpty {
+                let libraryTypes = await interactor.searchLibraryTypes(text: text)
+                if libraryTypes.isNotEmpty {
+                    sectionData.append(
+                        SectionData(section: .library, types: libraryTypes)
+                    )
+                }
+            }
+            
+            if sectionData.isNotEmpty {
+                state = .searchResults(sectionData)
             } else {
                 state = .emptyScreen
             }
         }
     }
     
-    func didSelectType(_ type: ObjectType) {
-        onSelect(type)
+    func didSelectType(_ type: ObjectType, section: SectionType) {
+        Task {
+            do {
+                if section == .library {
+                    try await interactor.installType(objectId: type.id)
+                    toastPresenter.show(message: Loc.ObjectType.addedToLibrary(type.name))
+                }
+                
+                onSelect(type)
+            }
+        }
     }
 }
