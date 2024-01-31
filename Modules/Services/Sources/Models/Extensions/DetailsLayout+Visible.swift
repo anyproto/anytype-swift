f37a834421a7a6d4fd891471f2a32769c1c40846
Mikhail Golovko <m@anytype.io>
2024-01-31 12:39:51 +0300

    IOS-2128 Rollback visibility

--- Modules/Services/Sources/Models/Extensions/DetailsLayout+Visible.swift
+++ Modules/Services/Sources/Models/Extensions/DetailsLayout+Visible.swift
@@ -1,8 +1,7 @@
 import AnytypeCore
 
 public extension DetailsLayout {
-    // TODO: Rollback
-    static var visibleLayouts: [DetailsLayout] = [.basic, .bookmark, .collection, .note, .profile, .set, .todo, .file, .image]
+    static var visibleLayouts: [DetailsLayout] = [.basic, .bookmark, .collection, .note, .profile, .set, .todo]
     static var supportedForEditLayouts: [DetailsLayout] = [.basic, .bookmark, .collection, .file, .image, .note, .profile, .set, .todo]
     static var supportedForCreationInSets: [DetailsLayout] = pageLayouts
 }
