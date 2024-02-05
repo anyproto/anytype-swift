1775e9e94b47423e1c15f76364f21466e192acc5
Mikhail Golovko <m@anytype.io>
2024-02-05 19:18:48 +0300

    IOS-2184 Fix test

--- AnytypeTests/Markdown/Mocks/BlockListServiceMock.swift
+++ AnytypeTests/Markdown/Mocks/BlockListServiceMock.swift
@@ -71,6 +71,7 @@ final class BlockServiceMock: BlockServiceProtocol {
     
     func add(contextId: String, targetId: Services.BlockId, info: Services.BlockInformation, position: Services.BlockPosition) async throws -> Services.BlockId {
         assertionFailure()
+        return ""
     }
     
     func delete(contextId: String, blockIds: [Services.BlockId]) async throws {
