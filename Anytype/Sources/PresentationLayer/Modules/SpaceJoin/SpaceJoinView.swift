01d936f6f7e6e680c0907e715478710d21a819c5
Mikhail Golovko <m@anytype.io>
2024-02-16 12:26:16 +0300

    IOS-2164 Space Join - empty module

---
+++ Anytype/Sources/PresentationLayer/Modules/SpaceJoin/SpaceJoinView.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+struct SpaceJoinView: View {
+    
+    @StateObject var model: SpaceJoinViewModel
+    
+    var body: some View {
+        EmptyView()
+    }
+}
