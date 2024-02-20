cb9a6a5811b817b814b03dc435e4011517c3ee1a
Mikhail Golovko <m@anytype.io>
2024-02-20 19:11:41 +0300

    IOS-2222 Fix debug

--- Anytype/Sources/ApplicationLayer/AppDelegate.swift
+++ Anytype/Sources/ApplicationLayer/AppDelegate.swift
@@ -13,6 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         
+        // Target contains GODEBUG=asyncpreemptoff=1 for fix debug on device
+        // More https://github.com/golang/go/issues/57651
+        // Delete GODEBUG after fix issue
+        
         // Fix SIGPIPE crashes
         signal(SIGPIPE, SIG_IGN)
         
