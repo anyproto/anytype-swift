4e51d6866fcdf83b528ebeb743efaae8275ed00e
Vova Ignatov <abbadoh@gmail.com>
2024-03-06 17:00:00 +0000

    IOS-2433 Add hardcoded pricing

--- Anytype/Sources/PresentationLayer/Modules/Membership/InitialScreen/Views/MembershipTierListView.swift
+++ Anytype/Sources/PresentationLayer/Modules/Membership/InitialScreen/Views/MembershipTierListView.swift
@@ -10,12 +10,7 @@ struct MembershipTierListView: View {
                     Spacer.fixedWidth(0)
                     
                     ForEach(MembershipTier.allCases) { tier in
-                        MembershipTeirView(
-                            title: tier.title,
-                            subtitle: tier.subtitle,
-                            image: tier.smallIcon,
-                            gradient: tier.gradient
-                        ) {
+                        MembershipTeirView(tier: tier) {
                             onTierTap(tier)
                         }
                         .id(tier)
