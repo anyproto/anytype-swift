0f4213703783e2a8603073442785b1b7fbd38a54
Vova Ignatov <abbadoh@gmail.com>
2024-04-08 13:45:08 +0100

    IOS-2594 Use dedicated StoreKitService for SK related actions

---
+++ Modules/Services/Sources/Services/StoreKitService/StoreKitService.swift
@@ -0,0 +1,64 @@
+import StoreKit
+import AnytypeCore
+
+
+public protocol StoreKitServiceProtocol {
+    func startListenForTransactions()
+    func purchase(product: Product) async throws
+}
+
+final class StoreKitService: StoreKitServiceProtocol {
+    func startListenForTransactions() {
+        Task.detached {
+            ///Iterate through any transactions that don't come from a direct call to `purchase()`.
+            for await verificationResult in Transaction.updates {
+                do {
+                    let transaction = try self.checkVerified(verificationResult)
+                    
+                    ///Deliver products to the user.
+                    try await self.notifyMiddleware()
+                    
+                    ///Always finish a transaction.
+                    await transaction.finish()
+                } catch {
+                    ///StoreKit has a transaction that fails verification. Don't deliver content to the user.
+                    anytypeAssertionFailure("Error in StoreKit transaction updates", info: ["Error": error.localizedDescription])
+                }
+            }
+        }
+    }
+    
+    func purchase(product: Product) async throws {
+        let result = try await product.purchase(options: [
+            .appAccountToken(UUID()),
+            .custom(key: "TODO", value: "SEND_DATA") // TODO
+        ])
+        
+        switch result {
+        case .success(let verificationResult):
+            let transaction = try checkVerified(verificationResult)
+            try await notifyMiddleware()
+            await transaction.finish()
+        case .userCancelled:
+            break // TODO
+        case .pending:
+            break // TODO
+        @unknown default:
+            anytypeAssertionFailure("Unsupported purchase result \(result)")
+            fatalError()
+        }
+    }
+    
+    private func checkVerified<T>(_ verificationResult: VerificationResult<T>) throws -> T {
+        switch verificationResult {
+        case .unverified(_, let verificationError):
+            throw verificationError
+        case .verified(let signedType):
+            return signedType
+        }
+    }
+    
+    private func notifyMiddleware() async throws {
+        // TODO
+    }
+}
--- Modules/Services/Sources/ServicesDI.swift
+++ Modules/Services/Sources/ServicesDI.swift
@@ -119,4 +119,8 @@ public extension Container {
     var participantService: Factory<ParticipantServiceProtocol> {
         self { ParticipantService() }.shared
     }
+    
+    var storeKitService: Factory<StoreKitServiceProtocol> {
+        self { StoreKitService() }.shared
+    }
 }
