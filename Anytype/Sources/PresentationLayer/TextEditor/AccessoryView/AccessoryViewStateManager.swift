d0b914f573649d2e654a8630ada54c60d5b048b9
Mikhail Golovko <m@anytype.io>
2024-04-16 17:23:07 +0300

    IOS-2592 Rename method

--- Anytype/Sources/PresentationLayer/TextEditor/AccessoryView/AccessoryViewStateManager.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/AccessoryView/AccessoryViewStateManager.swift
@@ -250,14 +250,14 @@ final class AccessoryViewStateManagerImpl: AccessoryViewStateManager, CursorMode
         return textView.compare(triggerSymbolPosition, to: caretPosition) == .orderedDescending
     }
     
-    private var isTwoSpacesInSearch: Bool {
+    private var searchContainsMultipleSpaces: Bool {
         guard let text = searchText() else { return false }
         let spaces = text.filter { $0 == " " }.count
         return spaces > 1
     }
     
     func dismissViewIfNeeded(forceDismiss: Bool = false) {
-        if forceDismiss || isTriggerSymbolDeleted || isTwoSpacesInSearch {
+        if forceDismiss || isTriggerSymbolDeleted || searchContainsMultipleSpaces {
             configuration?.output?.accessoryState = .none
             switcher.showDefaultView()
         }
