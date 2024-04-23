f44d3e5c9ff00c956464f8559217850df7e196e0
Mikhail Golovko <m@anytype.io>
2024-04-23 16:34:34 +0300

    IOS-2748 Remove space

--- Modules/Services/Sources/Models/Extensions/DetailsLayout+Visible.swift
+++ Modules/Services/Sources/Models/Extensions/DetailsLayout+Visible.swift
@@ -56,7 +56,7 @@ public extension DetailsLayout {
         DetailsLayout.pageLayouts.contains(self)
     }
     
-    static var layoutsWithIcon: [DetailsLayout] = [.basic, .profile, .set, .collection, .space, .file, .image]
+    static var layoutsWithIcon: [DetailsLayout] = [.basic, .profile, .set, .collection, .file, .image]
     static var layoutsWithCover: [DetailsLayout] = layoutsWithIcon + [.bookmark, .todo]
     static var layoutsWithChangeLayout: [DetailsLayout] = [.basic, .profile, .file, .image, .bookmark, .todo, .note]
 }
