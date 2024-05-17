ebafe14cdff47302c9426e11f64f85a1133b64eb
Mikhail Golovko <m@anytype.io>
2024-05-17 11:50:04 +0300

    IOS-2053 Fix

--- Modules/Services/Sources/Models/Block/IndentationBuilder.swift
+++ Modules/Services/Sources/Models/Block/IndentationBuilder.swift
@@ -1,20 +1,23 @@
 import Foundation
 
 public enum IndentationBuilder {
-    public static func build(container: InfoContainerProtocol, id: String) {
-        privateBuild(container: container, id: id)
+    public static func build(container: InfoContainerProtocol, id: String) -> [String] {
+        var numberValue = 0
+        return privateBuild(container: container, numberValue: &numberValue, id: id)
     }
 
     private static func privateBuild(
         container: InfoContainerProtocol,
+        numberValue: inout Int,
         id: String
-    ) {
+    ) -> [String] {
+        var changedIds = [String]()
         if let parent = container.get(id: id) {
-            var updatedBlockNumber = 0
 
             parent.childrenIds.forEach { childrenId in
+                
                 guard let child = container.get(id: childrenId) else { return }
-
+                
                 let updatedMetadataChild = child.updated(
                     metadata: BlockInformationMetadata(
                         parentId: parent.id,
@@ -24,26 +27,43 @@ public enum IndentationBuilder {
 
 
                 let updatedChild = updatedNumberedValueIfNeeded(
-                    container: container,
                     child: updatedMetadataChild,
-                    numberValue: &updatedBlockNumber
+                    numberValue: &numberValue
                 )
 
                 if child != updatedChild {
                     container.add(updatedChild)
+                    changedIds.append(childrenId)
                 }
+                
+                // Don't reset value between parent-child for div
+                // Example.
+                // div(1)
+                //   text num
+                //   text num <- Copy this value to next parent div(2)
+                // div(2) <- Use value here
+                //   text num
+                //   text num
+                let useNumberFromParent = child.content == BlockContent.layout(BlockLayout(style: .div))
+                var childNumberValue = useNumberFromParent ? numberValue : 0
 
-                privateBuild(
+                let childChangesIds = privateBuild(
                     container: container,
+                    numberValue: &childNumberValue,
                     id: childrenId
                 )
+                
+                changedIds.append(contentsOf: childChangesIds)
+                
+                if useNumberFromParent {
+                    numberValue = childNumberValue
+                }
             }
         }
+        return changedIds
     }
-
     
     private static func updatedNumberedValueIfNeeded(
-        container: InfoContainerProtocol,
         child: BlockInformation,
         numberValue: inout Int
     ) -> BlockInformation {
@@ -53,6 +73,16 @@ public enum IndentationBuilder {
             let content = BlockContent.text(text.updated(number: numberValue))
 
             return child.updated(content: content)
+        // Ignore reset value between items in one level.
+        // Example. Two div, but there are one numeric list.
+        // div(2)
+        //   text num
+        //   text num
+        // div(3) <- Don't reset num value here for use numberValue in childs
+        //   text num
+        //   text num
+        case let .layout(style) where style == BlockLayout(style: .div):
+            break
         default:
             numberValue = 0
         }
