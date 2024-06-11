5104b996b30f6352499f21d1026e8b20eb29c420
Anna Zakharova <joe_pusya@anytype.io>
2024-06-11 17:01:05 +0100

    IOS-2968 Call blockDataviewViewSetActive to save view in Sets/Collections between sessions

--- Modules/Services/Sources/Services/Dataview/DataviewServiceProtocol.swift
+++ Modules/Services/Sources/Services/Dataview/DataviewServiceProtocol.swift
@@ -2,6 +2,7 @@ import ProtobufMessages
 
 public protocol DataviewServiceProtocol: Sendable {
     func updateView(objectId: String, blockId: String, view: DataviewView) async throws
+    func setActiveView(objectId: String, blockId: String, viewId: String) async throws
     
     // MARK: - Filters
     func addFilter(objectId: String, blockId: String, filter: DataviewFilter, viewId: String) async throws
