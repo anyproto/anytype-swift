a31eb59fae0b18f9ac5e53e02391dd3f6c4603d5
Mikhail Golovko <m@anytype.io>
2024-06-18 10:52:37 +0300

    IOS-3009 Extract

--- Anytype/Sources/PresentationLayer/Modules/HomeWidgets/Widgets/List/Common/ListWidgetView.swift
+++ Anytype/Sources/PresentationLayer/Modules/HomeWidgets/Widgets/List/Common/ListWidgetView.swift
@@ -67,43 +67,7 @@ private struct ListWidgetInternalView: View {
     private var bodyContent: some View {
         VStack(spacing: 0) {
             ViewWidgetTabsView(items: model.headerItems)
-            content
-        }
-    }
-        
-    private var content: some View {
-        ZStack {
-            if let rows = model.rows {
-                if rows.isEmpty {
-                    VStack(spacing: 0) {
-                        WidgetEmptyView(title: model.emptyTitle)
-                            .frame(height: 72)
-                        Spacer.fixedHeight(8)
-                    }
-                    .transition(.opacity)
-                }
-                else {
-                    VStack(spacing: 0) {
-                        ForEach(rows) {
-                            rowView(row: $0, showDivider: $0.id != rows.last?.id)
-                        }
-                        Spacer.fixedHeight(8)
-                    }
-                    .transition(.opacity)
-                }
-            }
-        }
-        // This fixes the tap area for header in bottom side
-        .fixTappableArea()
-    }
-    
-    @ViewBuilder
-    private func rowView(row: ListWidgetRowModel, showDivider: Bool) -> some View {
-        switch model.style {
-        case .compactList:
-            ListWidgetCompactRow(model: row, showDivider: showDivider)
-        case .list:
-            ListWidgetRow(model: row, showDivider: showDivider)
+            ListWidgetContentView(style: model.style, rows: model.rows, emptyTitle: model.emptyTitle)
         }
     }
 }
