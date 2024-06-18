9c86996a94ef70b03a6cba58926a9890a2392ebb
Mikhail Golovko <m@anytype.io>
2024-06-18 11:54:44 +0300

    IOS-2972 Copy model

--- Anytype/Sources/PresentationLayer/Modules/HomeWidgets/Widgets/View/SetObjectViewWidgetSubmoduleView.swift
+++ Anytype/Sources/PresentationLayer/Modules/HomeWidgets/Widgets/View/SetObjectViewWidgetSubmoduleView.swift
@@ -3,7 +3,16 @@ import SwiftUI
 
 struct SetObjectViewWidgetSubmoduleView: View {
     
+    private let data: WidgetSubmoduleData
+    @StateObject private var model: SetObjectWidgetInternalViewModel
+    
+    init(data: WidgetSubmoduleData) {
+        self.data = data
+        self._model = StateObject(wrappedValue: SetObjectWidgetInternalViewModel(data: data))
+    }
+    
     var body: some View {
+        // TODO: Adopt SetObjectWidgetInternalViewModel for SetWidgetView
         SetObjectGalleryWidgetSubmoduleView()
     }
 }
