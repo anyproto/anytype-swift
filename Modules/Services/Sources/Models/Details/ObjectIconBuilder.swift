d80bd254d008a5ae4ecfe60a277583452ea33d92
Vova Ignatov <abbadoh@gmail.com>
2024-06-19 10:40:33 +0100

    IOS-3001 Add ObjectIconBuilderProtocol and reuse it in Notifications

---
+++ Modules/Services/Sources/Models/Details/ObjectIconBuilder.swift
@@ -0,0 +1,88 @@
+import AnytypeCore
+
+public protocol ObjectIconBuilderProtocol {
+    func objectIcon(relations: BundledRelationsValueProvider) -> ObjectIcon?
+    func profileIcon(iconImage: String, objectName: String) -> ObjectIcon?
+}
+
+public final class ObjectIconBuilder: ObjectIconBuilderProtocol {
+    public init() { }
+    
+    public func objectIcon(relations: BundledRelationsValueProvider) -> ObjectIcon? {
+        guard !relations.isDeleted else {
+            return .deleted
+        }
+        
+        if let objectIcon = icon(relations: relations) {
+            return objectIcon
+        }
+        
+        if DetailsLayout.fileLayouts.contains(relations.layoutValue) {
+            return fileIcon(fileMimeType: relations.fileMimeType, name: relations.name)
+        }
+        
+        if relations.layoutValue == .todo {
+            return .todo(relations.isDone)
+        }
+        
+        return nil
+    }
+    
+    public func profileIcon(iconImage: String, objectName: String) -> ObjectIcon? {
+        if iconImage.isNotEmpty {
+            return .profile(.imageId(iconImage))
+        }
+        
+        return .profile(.name(objectName))
+    }
+    
+    // MARK: - Private
+    
+    private func icon(relations: BundledRelationsValueProvider) -> ObjectIcon? {
+        switch relations.layoutValue {
+        case .basic, .set, .collection, .image, .objectType:
+            return basicIcon(iconImage: relations.iconImage, iconEmoji: relations.iconEmoji)
+        case .profile, .participant:
+            return profileIcon(iconImage: relations.iconImage, objectName: relations.objectName)
+        case .bookmark:
+            return bookmarkIcon(iconImage: relations.iconImage)
+        case .todo, .note, .file, .UNRECOGNIZED, .relation, .relationOption, .dashboard, .relationOptionsList,
+                .audio, .video, .pdf, .date:
+            return nil
+        case .space, .spaceView:
+            return spaceIcon(iconImage: relations.iconImage, iconOptionValue: relations.iconOptionValue, objectName: relations.objectName)
+        }
+    }
+    
+    private func basicIcon(iconImage: String, iconEmoji: Emoji?) -> ObjectIcon? {
+        if iconImage.isNotEmpty {
+            return .basic(iconImage)
+        }
+        
+        if let iconEmoji = iconEmoji {
+            return .emoji(iconEmoji)
+        }
+        
+        return nil
+    }
+    
+    private func bookmarkIcon(iconImage: String) -> ObjectIcon? {
+        return iconImage.isNotEmpty ? .bookmark(iconImage) : nil
+    }
+    
+    private func spaceIcon(iconImage: String, iconOptionValue: GradientId?, objectName: String) -> ObjectIcon? {
+        if iconImage.isNotEmpty {
+            return .space(.imageId(iconImage))
+        }
+        
+        if let iconOptionValue {
+            return .space(.gradient(iconOptionValue))
+        }
+        
+        return .space(.name(objectName))
+    }
+    
+    private func fileIcon(fileMimeType: String, name: String) -> ObjectIcon {
+        return .file(mimeType: fileMimeType, name: name)
+    }
+}
