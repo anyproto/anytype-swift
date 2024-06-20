7785a84db9e7f6a552485c15db2416f4b4f1a97d
Mikhail Golovko <m@anytype.io>
2024-06-20 18:21:10 +0300

    IOS-3019 Disable for prod

--- Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
+++ Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
@@ -57,7 +57,8 @@ public extension FeatureDescription {
     static let galleryWidget = FeatureDescription(
         title: "Gallery Widget - IOS-2964",
         type: .feature(author: "m@anytype.io", releaseVersion: "6"),
-        defaultValue: true
+        defaultValue: false,
+        debugValue: true
     )
     
     // MARK: - Debug
