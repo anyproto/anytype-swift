3569b8213fbac2ea3187a380b229135bfbebb10e
Mikhail Golovko <m@anytype.io>
2024-07-08 12:37:31 +0300

    IOS-3099 Add style

--- Anytype/Sources/PresentationLayer/Modules/Discussion/Input/DiscussionTextView.swift
+++ Anytype/Sources/PresentationLayer/Modules/Discussion/Input/DiscussionTextView.swift
@@ -12,19 +12,27 @@ struct DiscussionTextView: UIViewRepresentable {
     func makeUIView(context: Context) -> UITextView {
         let textView = UITextView(usingTextLayoutManager: true)
         textView.delegate = context.coordinator
+        // Text style
+        let style = AnytypeFont.bodyRegular
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.lineHeightMultiple = style.lineHeightMultiple
+        textView.typingAttributes = [
+            .font: UIKitFontBuilder.uiKitFont(font: style),
+            .paragraphStyle: paragraph
+        ]
         return textView
     }
     
     func updateUIView(_ textView: UITextView, context: Context) {
         if editing {
             if !textView.isFirstResponder {
-                DispatchQueue.main.async { // Async for fix "AttributeGraph: cycle detected through attribute"
+                Task { @MainActor in // Async for fix "AttributeGraph: cycle detected through attribute"
                     textView.becomeFirstResponder()
                 }
             }
         } else {
             if textView.isFirstResponder {
-                DispatchQueue.main.async { // Async for fix "AttributeGraph: cycle detected through attribute"
+                Task { @MainActor in // Async for fix "AttributeGraph: cycle detected through attribute"
                     textView.resignFirstResponder()
                 }
             }
