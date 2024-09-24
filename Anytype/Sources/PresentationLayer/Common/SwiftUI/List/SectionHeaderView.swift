97b2ff7d0c351bcf18e511e682307466a3b59a43
Anna Zakharova <joe_pusya@anytype.io>
2024-09-24 18:50:35 +0100

    IOS-3483 List design updates

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/List/SectionHeaderView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/List/SectionHeaderView.swift
@@ -4,11 +4,13 @@ import SwiftUI
 struct SectionHeaderView<Content>: View where Content: View {
     let title: String
     let increasedTopPadding: Bool
-    let rightContent: () -> Content?
+    let hasRightContent: Bool
+    let rightContent: () -> Content
     
-    init(title: String, increasedTopPadding: Bool = true, @ViewBuilder rightContent: @escaping () -> Content? = { EmptyView() }) {
+    init(title: String, increasedTopPadding: Bool = true, @ViewBuilder rightContent: @escaping () -> Content) {
         self.title = title
         self.increasedTopPadding = increasedTopPadding
+        self.hasRightContent = true
         self.rightContent = rightContent
     }
     
@@ -16,11 +18,11 @@ struct SectionHeaderView<Content>: View where Content: View {
         HStack(spacing: 0) {
             AnytypeText(title, style: .caption1Regular)
                 .foregroundColor(.Text.secondary)
-                .if(rightContent().isNotNil) {
+                .if(hasRightContent) {
                     $0.lineLimit(1)
                 }
             
-            if rightContent().isNotNil {
+            if hasRightContent {
                 Spacer.init(minLength: 16)
             }
             
@@ -29,5 +31,14 @@ struct SectionHeaderView<Content>: View where Content: View {
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding(.top, increasedTopPadding ? 26 : 8)
         .padding(.bottom, 8)
+    }
+}
+
+extension SectionHeaderView where Content == EmptyView {
+    init(title: String, increasedTopPadding: Bool = true) {
+        self.title = title
+        self.increasedTopPadding = increasedTopPadding
+        self.hasRightContent = false
+        self.rightContent = { EmptyView() }
     }
 }
