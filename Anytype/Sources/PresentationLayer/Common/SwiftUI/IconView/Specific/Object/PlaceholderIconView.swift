254e08a11c8add27f0c62df7eb1e057ce57dd652
Anna Zakharova <joe_pusya@anytype.io>
2024-10-03 12:11:39 +0100

    IOS-3499 Change background color to Color.Shape.transperentTertiary

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/IconView/Specific/Object/PlaceholderIconView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/IconView/Specific/Object/PlaceholderIconView.swift
@@ -7,7 +7,7 @@ struct PlaceholderIconView: View {
     
     var body: some View {
         ImageCharIconView(text: text)
-            .background(Color.Shape.secondary)
+            .background(Color.Shape.transperentTertiary)
             .objectIconCornerRadius()
     }
 }
