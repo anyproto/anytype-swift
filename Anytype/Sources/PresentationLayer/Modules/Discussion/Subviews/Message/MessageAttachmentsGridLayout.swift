fd8285acae1d7e2e939ad528ac86ba463ed5b5ff
Mikhail Golovko <m@anytype.io>
2024-10-08 15:48:07 +0300

    IOS-3181 in progress

---
+++ Anytype/Sources/PresentationLayer/Modules/Discussion/Subviews/Message/MessageAttachmentsGridLayout.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+final class MessageAttachmentsGridLayoutBuilder {
+    func makeGridRows(countItems: Int) -> [Int] {
+        if countItems == 0 {
+            return []
+        }
+        
+        if countItems == 1 {
+            return [1]
+        }
+        
+        if
+    }
+}
