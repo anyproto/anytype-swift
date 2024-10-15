b31098f26704b2eb19e0db60fdb6278d0e5cf524
Vova Ignatov <abbadoh@gmail.com>
2024-10-15 15:01:12 +0100

    IOS-3518 renamed setLayouts -> listLayouts

--- Modules/Services/Sources/Models/Extensions/DetailsLayout+Visible.swift
+++ Modules/Services/Sources/Models/Extensions/DetailsLayout+Visible.swift
@@ -3,23 +3,21 @@ import AnytypeCore
 public extension DetailsLayout {
 
     static let editorLayouts: [DetailsLayout] = [ .note, .basic, .profile, .todo ]
+    static let listLayouts: [DetailsLayout] = [ .collection, .set ]
     
     static let fileLayouts: [DetailsLayout] = [ .file, .pdf ]
     static let mediaLayouts: [DetailsLayout] = [ .image, .audio, .video ]
     static let fileAndMediaLayouts = DetailsLayout.fileLayouts + DetailsLayout.mediaLayouts
     
-    static let setLayouts: [DetailsLayout] = [ .collection, .set ] // TODO: Rename
-    
-    static let layoutsWithIcon: [DetailsLayout] = setLayouts + fileAndMediaLayouts + [.basic, .profile]
+    static let layoutsWithIcon: [DetailsLayout] = listLayouts + fileAndMediaLayouts + [.basic, .profile]
     static let layoutsWithCover: [DetailsLayout] = layoutsWithIcon + [.bookmark, .todo]
     
-    static let visibleLayouts: [DetailsLayout] = setLayouts + editorLayouts + [.bookmark] + [.participant] // TODO: Rename
+    static let visibleLayouts: [DetailsLayout] = listLayouts + editorLayouts + [.bookmark] + [.participant]
     static let visibleLayoutsWithFiles = visibleLayouts + fileAndMediaLayouts
     
     static let supportedForCreationInSets: [DetailsLayout] = editorLayouts + [.bookmark]
     
     
-    // MARK: - Templates
     var isTemplatesAvailable: Bool {
         DetailsLayout.editorLayouts.contains(self)
     }
