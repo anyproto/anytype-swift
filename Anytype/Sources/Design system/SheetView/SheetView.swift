f22d931277809fbc4a713981e0bba6dc7e9966fd
Stan Sidel <stansidel@gmail.com>
2024-10-15 08:02:44 +0500

    #2208 Add interactive dragging to anytypeSheet

--- Anytype/Sources/Design system/SheetView/SheetView.swift
+++ Anytype/Sources/Design system/SheetView/SheetView.swift
@@ -6,7 +6,9 @@ struct SheetView<Content: View>: View {
     private let dismissOnBackgroundView: Bool
     private var content: Content
     private let cancelAction: (() -> Void)?
-    
+
+    @State private var spacerHeight: CGFloat = 0
+
     init(
         dismissOnBackgroundView: Bool,
         content: () -> Content,
@@ -35,19 +37,30 @@ struct SheetView<Content: View>: View {
     }
     
     private var contentView: some View {
-        content
+        VStack(spacing: 0) {
+            Spacer.fixedHeight(max(0, spacerHeight))
+            VStack(spacing: 0) {
+                DragIndicator()
+                Spacer.fixedHeight(12)
+                content
+            }
+            .background(Color.Background.secondary)
             .cornerRadius(16, style: .continuous)
             .shadow(radius: 20)
             .padding(.horizontal, 8)
-            .gesture(
-                DragGesture()
-                    .onEnded { value in
-                        if value.translation.height > 0 {
-                            dismiss()
-                            cancelAction?()
-                        }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    spacerHeight = value.translation.height
+                }
+                .onEnded { value in
+                    if value.translation.height > 0 {
+                        dismiss()
+                        cancelAction?()
                     }
-            )
-            .fitIPadToReadableContentGuide()
+                }
+        )
+        .fitIPadToReadableContentGuide()
     }
 }
