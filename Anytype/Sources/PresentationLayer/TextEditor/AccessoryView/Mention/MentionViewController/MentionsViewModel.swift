9dd2f4cb618796f134f612c8bbb96d668452b41e
Anna Zakharova <joe_pusya@anytype.io>
2024-10-25 17:37:54 +0100

    IOS-3371 Add select date section to mentions

--- Anytype/Sources/PresentationLayer/TextEditor/AccessoryView/Mention/MentionViewController/MentionsViewModel.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/AccessoryView/Mention/MentionViewController/MentionsViewModel.swift
@@ -5,22 +5,34 @@ import UIKit
 import AnytypeCore
 import Combine
 
+@MainActor
 final class MentionsViewModel {
     @Published private(set) var mentions = [MentionDisplayData]()
     let dismissSubject = PassthroughSubject<Void, Never>()
     
     var onSelect: RoutingAction<MentionObject>?
     
+    var searchString = ""
+    
     private let document: any BaseDocumentProtocol
     @Injected(\.mentionObjectsService)
     private var mentionService: any MentionObjectsServiceProtocol
     @Injected(\.defaultObjectCreationService)
     private var defaultObjectService: any DefaultObjectCreationServiceProtocol
-    private var searchTask: Task<(), any Error>?
-    var searchString = ""
+    @Injected(\.objectActionsService)
+    private var objectService: any ObjectActionsServiceProtocol
+    @Injected(\.objectTypeProvider)
+    private var objectTypeProvider: any ObjectTypeProviderProtocol
     
-    init(document: some BaseDocumentProtocol) {
+    private var searchTask: Task<(), any Error>?
+
+    private let formatter = DateFormatter.relationDateFormatter
+    
+    private let router: any EditorRouterProtocol
+    
+    init(document: some BaseDocumentProtocol, router: some EditorRouterProtocol) {
         self.document = document
+        self.router = router
     }
     
     func obtainMentions(filterString: String) {
@@ -41,6 +53,7 @@ final class MentionsViewModel {
             if dateMentions.isNotEmpty {
                 updatedMentions.append(.header(title: Loc.dates))
                 updatedMentions.append(contentsOf: dateMentions.map { .mention($0) })
+                updatedMentions.append(.selectDate)
             }
             
             let objectsMentions = try await mentionService.searchMentions(
@@ -68,12 +81,40 @@ final class MentionsViewModel {
         dismissSubject.send(())
     }
     
+    func didSelectCustomDate() {
+        router.showDatePicker { [weak self] date in
+            self?.createDateObject(with: date)
+        }
+    }
+    
     func didSelectCreateNewMention() {
         Task { @MainActor in
             guard let newBlockDetails = try? await defaultObjectService.createDefaultObject(name: searchString, shouldDeleteEmptyObject: false, spaceId: document.spaceId) else { return }
             
             AnytypeAnalytics.instance().logCreateObject(objectType: newBlockDetails.analyticsType, spaceId: newBlockDetails.spaceId, route: .mention)
             let mention = MentionObject(details: newBlockDetails)
+            didSelectMention(mention)
+        }
+    }
+    
+    private func createDateObject(with date: Date) {
+        Task { @MainActor in
+            let title = formatter.string(from: date)
+            let dateObjectType = try objectTypeProvider.objectType(uniqueKey: ObjectTypeUniqueKey.date, spaceId: document.spaceId)
+            let details = try? await objectService.createObject(
+                name: title,
+                typeUniqueKey: dateObjectType.uniqueKey,
+                shouldDeleteEmptyObject: false,
+                shouldSelectType: true,
+                shouldSelectTemplate: true,
+                spaceId: document.spaceId,
+                origin: .none,
+                templateId: dateObjectType.defaultTemplateId
+            )
+            guard let details else { return }
+            
+            AnytypeAnalytics.instance().logCreateObject(objectType: details.analyticsType, spaceId: details.spaceId, route: .mention)
+            let mention = MentionObject(details: details)
             didSelectMention(mention)
         }
     }
--- Anytype/Sources/PresentationLayer/TextEditor/AccessoryView/Mention/MentionViewController/Models/MentionDisplayData.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/AccessoryView/Mention/MentionViewController/Models/MentionDisplayData.swift
@@ -4,4 +4,5 @@ enum MentionDisplayData: Hashable {
     case header(title: String)
     case mention(MentionObject)
     case createNewObject(objectName: String)
+    case selectDate
 }
