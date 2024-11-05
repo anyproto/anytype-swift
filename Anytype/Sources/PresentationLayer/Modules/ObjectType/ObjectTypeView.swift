1c5da22ae32fa3342ead4f1ee05efcf55a678830
Vova Ignatov <abbadoh@gmail.com>
2024-11-05 16:10:14 +0000

    IOS-3652 Integrated template component

--- Anytype/Sources/PresentationLayer/Modules/ObjectType/ObjectTypeView.swift
+++ Anytype/Sources/PresentationLayer/Modules/ObjectType/ObjectTypeView.swift
@@ -9,13 +9,52 @@ struct ObjectTypeView: View {
     
     var body: some View {
         content
-            .task { await model.setup() }
+            .animation(.default, value: model.templates)
+            .task { await model.setupSubscriptions() }
     }
     
     private var content: some View {
         VStack {
-            AnytypeText(model.title, style: .title)
-            AnytypeText("TBD", style: .subheading).foregroundColor(.Text.secondary)
+            Rectangle().frame(height: 48).foregroundStyle(Color.Text.secondary) // Navbar placeholder
+            VStack(spacing: 0) {
+                Spacer.fixedHeight(32)
+                header
+                templates
+                Spacer()
+            }.padding(.horizontal, 20)
+        }
+    }
+    
+    private var header: some View {
+        AnytypeText(model.title, style: .title)
+    }
+    
+    private var templates: some View {
+        VStack {
+            HStack(spacing: 8) {
+                AnytypeText(Loc.templates, style: .subheading)
+                AnytypeText("\(model.templates.count)", style: .previewTitle1Regular)
+                    .foregroundColor(Color.Text.secondary)
+                Spacer()
+            }.padding(10)
+            templatesList
+        }
+    }
+    
+    private var templatesList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(model.templates) { item in
+                    EditableView<TemplatePreview>(
+                        content: TemplatePreview(viewModel: item),
+                        onTap: { model.onTemplateTap(model: item.model) },
+                        canBeEdited: false, //item.model.isEditable,
+                        isEditing: .constant(false) //$model.isEditingState
+                    )
+                }
+            }
+            .padding(.horizontal, 16)
+            .frame(height: 232)
         }
     }
 }
