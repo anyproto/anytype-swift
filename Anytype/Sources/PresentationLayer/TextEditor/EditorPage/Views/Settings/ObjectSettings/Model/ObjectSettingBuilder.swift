9bd9aca3ca256152837ea604d0e4ca08fb30b89a
Vova Ignatov <abbadoh@gmail.com>
2024-11-14 14:13:58 +0000

    IOS-3652 Remove layout action from object

--- Anytype/Sources/PresentationLayer/TextEditor/EditorPage/Views/Settings/ObjectSettings/Model/ObjectSettingBuilder.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/EditorPage/Views/Settings/ObjectSettings/Model/ObjectSettingBuilder.swift
@@ -4,6 +4,8 @@ import AnytypeCore
 final class ObjectSettingBuilder {
     func build(details: ObjectDetails, permissions: ObjectPermissions) -> [ObjectSetting] {
         .builder {
+            ObjectSetting.relations
+            
             if permissions.canChangeIcon {
                 ObjectSetting.icon
             }
@@ -12,12 +14,9 @@ final class ObjectSettingBuilder {
                 ObjectSetting.cover
             }
             
-            if permissions.canChangeLayout {
+            if permissions.canChangeLayout && !FeatureFlags.primitives {
                 ObjectSetting.layout
             }
-            
-            ObjectSetting.relations
-            
             if FeatureFlags.versionHistory, permissions.canShowVersionHistory {
                 ObjectSetting.history
             }
