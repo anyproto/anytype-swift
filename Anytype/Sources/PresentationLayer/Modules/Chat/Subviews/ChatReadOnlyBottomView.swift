0787c81b2fa85a2d097733df3ed47899e9a0fd0e
Mikhail Golovko <m@anytype.io>
2024-12-02 13:25:23 +0300

    IOS-3529 Fix read-only design

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/ChatReadOnlyBottomView.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/ChatReadOnlyBottomView.swift
@@ -4,13 +4,17 @@ struct ChatReadOnlyBottomView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 8) {
             IconView(asset: .X18.lock)
-                .foregroundStyle(Color.Control.active)
+                .foregroundStyle(Color.Control.button)
             Text(Loc.Chat.readOnly)
                 .anytypeStyle(.caption1Regular)
-                .foregroundStyle(Color.Text.secondary)
+                .foregroundStyle(Color.Text.primary)
+            Spacer()
         }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 12)
+        .background(Color.Background.navigationPanel)
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
         .padding(.horizontal, 20)
-        .padding(.vertical, 10)
-        .background(Color.Background.primary)
     }
 }
