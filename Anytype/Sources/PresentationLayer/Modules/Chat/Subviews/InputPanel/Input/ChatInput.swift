48a5c72b8f998f565741a43e492a5f203893a8fc
Mikhail Golovko <m@anytype.io>
2024-12-25 14:12:39 +0300

    IOS-3925 Remove back from chat input

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/InputPanel/Input/ChatInput.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/InputPanel/Input/ChatInput.swift
@@ -15,69 +15,70 @@ struct ChatInput: View {
     let onTapSend: () -> Void
     let onTapLinkTo: (_ range: NSRange) -> Void
     
-    @Environment(\.pageNavigation) private var pageNavigation
-    
     var body: some View {
         HStack(alignment: .bottom, spacing: 8) {
-            Image(asset: .X32.Island.back)
-                .foregroundColor(.Control.navPanelIcon)
-                .frame(height: 56)
-                .onTapGesture {
-                    pageNavigation.pop()
-                }
-            
-            ZStack(alignment: .topLeading) {
-                if text.string.isEmpty {
-                    Text(Loc.Message.Input.emptyPlaceholder)
-                        .anytypeStyle(.bodyRegular)
-                        .foregroundColor(.Text.tertiary)
-                        .padding(.top, 15)
-                        .allowsHitTesting(false)
-                        .lineLimit(1)
-                }
-                ChatTextView(text: $text, editing: $editing, mention: $mention, minHeight: 56, maxHeight: 156, linkTo: onTapLinkTo)
-            }
-            
-            Menu {
-                Button {
-                    onTapAddObject()
-                } label: {
-                    Text(Loc.Chat.Actions.Menu.objects)
-                }
-                Button {
-                    onTapAddMedia()
-                } label: {
-                    Text(Loc.Chat.Actions.Menu.media)
-                }
-                Button {
-                    onTapAddFiles()
-                } label: {
-                    Text(Loc.Chat.Actions.Menu.files)
-                }
-                Button {
-                    onTapCamera()
-                } label: {
-                    Text(Loc.Chat.Actions.Menu.camera)
-                }
-            } label: {
-                Image(asset: .X32.plus)
-                    .foregroundColor(.Control.navPanelIcon)
-            }
-            .frame(height: 56)
-            .menuOrder(.fixed)
-            .disabled(disableAddButton)
-            
-            if hasAdditionalData || !text.string.isEmpty {
-                Button {
-                    onTapSend()
-                } label: {
-                    EnableStateImage(enable: .Chat.SendMessage.active, disable: .Chat.SendMessage.inactive)
-                }
-                .disabled(disableSendButton)
-                .frame(width: 32, height: 56)
-            }
-                
+            plusButton
+            input
+            sendButton
         }
         .padding(.horizontal, 8)
+    }
+    
+    private var plusButton: some View {
+        Menu {
+            Button {
+                onTapAddObject()
+            } label: {
+                Text(Loc.Chat.Actions.Menu.objects)
+            }
+            Button {
+                onTapAddMedia()
+            } label: {
+                Text(Loc.Chat.Actions.Menu.media)
+            }
+            Button {
+                onTapAddFiles()
+            } label: {
+                Text(Loc.Chat.Actions.Menu.files)
+            }
+            Button {
+                onTapCamera()
+            } label: {
+                Text(Loc.Chat.Actions.Menu.camera)
+            }
+        } label: {
+            Image(asset: .X32.plus)
+                .foregroundColor(.Control.navPanelIcon)
+        }
+        .frame(height: 56)
+        .menuOrder(.fixed)
+        .disabled(disableAddButton)
+    }
+    
+    private var input: some View {
+        ZStack(alignment: .topLeading) {
+            if text.string.isEmpty {
+                Text(Loc.Message.Input.emptyPlaceholder)
+                    .anytypeStyle(.bodyRegular)
+                    .foregroundColor(.Text.tertiary)
+                    .padding(.top, 15)
+                    .allowsHitTesting(false)
+                    .lineLimit(1)
+            }
+            ChatTextView(text: $text, editing: $editing, mention: $mention, minHeight: 56, maxHeight: 156, linkTo: onTapLinkTo)
+        }
+    }
+    
+    @ViewBuilder
+    private var sendButton: some View {
+        if hasAdditionalData || !text.string.isEmpty {
+            Button {
+                onTapSend()
+            } label: {
+                EnableStateImage(enable: .Chat.SendMessage.active, disable: .Chat.SendMessage.inactive)
+            }
+            .disabled(disableSendButton)
+            .frame(width: 32, height: 56)
+        }
     }
 }
