9ebc20ac3fd17765f9bc0f800bdd3bd4e136bcd0
Vova Ignatov <abbadoh@gmail.com>
2025-01-22 11:09:56 +0000

    IOS-3897 Add bigger font for bigger profile icons

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/IconView/Common/ImageCharIconView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/IconView/Common/ImageCharIconView.swift
@@ -16,7 +16,8 @@ struct ImageCharIconView: View {
         Config(side: 40, fontSize: 24),
         Config(side: 48, fontSize: 30),
         Config(side: 64, fontSize: 36),
-        Config(side: 80, fontSize: 48)
+        Config(side: 80, fontSize: 48),
+        Config(side: 112, fontSize: 88)
     ].sorted(by: { $0.side > $1.side }) // Order by DESC side for simple search
     
     let text: String
