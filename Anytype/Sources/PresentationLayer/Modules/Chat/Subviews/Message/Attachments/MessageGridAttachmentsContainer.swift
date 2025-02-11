04d0eba57f64ac8ea574d357406cd1201b898145
Mikhail Golovko <m@anytype.io>
2025-02-11 18:26:14 +0300

    IOS-4103 Fix

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/Message/Attachments/MessageGridAttachmentsContainer.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/Message/Attachments/MessageGridAttachmentsContainer.swift
@@ -4,62 +4,36 @@ import Services
 
 struct MessageGridAttachmentsContainer: View {
 
-    let objects: [[MessageAttachmentDetails]]
-    let oneSide: CGFloat
+    let objects: [MessageAttachmentDetails]
     let spacing: CGFloat
     let onTapObject: (MessageAttachmentDetails) -> Void
     
-    private let twoSide: CGFloat
-    private let treeSide: CGFloat
-    
     init(
-        objects: [[MessageAttachmentDetails]],
-        oneSide: CGFloat,
+        objects: [MessageAttachmentDetails],
         spacing: CGFloat,
         onTapObject: @escaping (MessageAttachmentDetails) -> Void
     ) {
         self.objects = objects
-        self.oneSide = oneSide
         self.spacing = spacing
         self.onTapObject = onTapObject
-        self.twoSide = (oneSide - spacing) * 0.5
-        self.treeSide = (oneSide - spacing * 2) * 0.33
     }
     
     var body: some View {
-        VStack(spacing: spacing) {
-            ForEach(objects, id:\.self) { rowObjects in
-                HStack(spacing: spacing) {
-                    ForEach(rowObjects, id:\.id) { object in
-                        Button {
-                            onTapObject(object)
-                        } label: {
-                            switch object.layoutValue {
-                            case .video:
-                                MessageVideoView(details: object)
-                            default: // image and other types (for bugs)
-                                ImageIdIconView(imageId: object.id)
-                            }
-                        }
-                        .frame(width: rowItemSize(rowItems: rowObjects.count), height: rowItemSize(rowItems: rowObjects.count))
-                        .cornerRadius(4)
+        MessageGridAttachmentsLayout(spacing: spacing) {
+            ForEach(objects, id:\.id) { object in
+                Button {
+                    onTapObject(object)
+                } label: {
+                    switch object.layoutValue {
+                    case .video:
+                        MessageVideoView(details: object)
+                    default: // image and other types (for bugs)
+                        ImageIdIconView(imageId: object.id)
                     }
                 }
+                .cornerRadius(4)
             }
-        }
-        .cornerRadius(12)
-    }
-    
-    private func rowItemSize(rowItems: Int) -> CGFloat? {
-        switch rowItems {
-        case 3:
-            return treeSide
-        case 2:
-            return twoSide
-        case 1:
-            return oneSide
-        default:
-            return nil
+            .cornerRadius(12)
         }
     }
 }
