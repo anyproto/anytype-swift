28dad9e9f4e38b1b919fe9884e017d4f7a5ef7f6
Mikhail Golovko <m@anytype.io>
2025-02-13 21:21:43 +0300

    IOS-0 Test

--- Anytype/Sources/ApplicationLayer/Exit1Report/StdOutDuplicator.swift
+++ Anytype/Sources/ApplicationLayer/Exit1Report/StdOutDuplicator.swift
@@ -13,53 +13,55 @@ final class StdOutDuplicator {
     
         guard !started else { return }
         
-//            guard let logFile = fopen(logFilePath, "a") else {
-//                return
-//            }
+            guard let logFile = fopen(logFilePath, "a") else {
+                return
+            }
 //            
-//            let stdoutDescriptor = fileno(logFile)
+            let stdoutDescriptor = fileno(logFile)
 ////            let originalStdout = dup(STDOUT_FILENO)
 ////            let originalStderr = dup(STDERR_FILENO)
 //
-//            dup2(stdoutDescriptor, STDOUT_FILENO)
-//            dup2(stdoutDescriptor, STDERR_FILENO)
+            dup2(stdoutDescriptor, STDOUT_FILENO)
+            dup2(stdoutDescriptor, STDERR_FILENO)
         
-        FileManager.default.createFile(atPath: logFilePath, contents: nil)
-        guard let fileHandle = FileHandle(forWritingAtPath: logFilePath) ?? FileHandle(forUpdatingAtPath: logFilePath) else {
-            return
-        }
-        fileHandle.seekToEndOfFile()
-        logFileHandle = fileHandle
-
-        let originalStdout = dup(STDOUT_FILENO)
-        let originalStderr = dup(STDERR_FILENO)
-        
-        
-        let outPipe = Pipe()
-        stdoutPipe = outPipe
-        dup2(outPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
-
-        
-        let errPipe = Pipe()
-        stderrPipe = errPipe
-        dup2(errPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
-
-        
-        DispatchQueue.global(qos: .background).async {
-            while let data = try? outPipe.fileHandleForReading.read(upToCount: 4096),
-                    let output = String(data: data, encoding: .utf8) {
-                write(originalStdout, output, output.utf8.count)
-                fileHandle.write(data)
-            }
-        }
-        
-        DispatchQueue.global(qos: .background).async {
-            while let data = try? errPipe.fileHandleForReading.read(upToCount: 4096),
-                    let output = String(data: data, encoding: .utf8) {
-                write(originalStderr, output, output.utf8.count)
-                fileHandle.write(data)
-            }
-        }
+//        FileManager.default.createFile(atPath: logFilePath, contents: nil)
+//        guard let fileHandle = FileHandle(forWritingAtPath: logFilePath) ?? FileHandle(forUpdatingAtPath: logFilePath) else {
+//            return
+//        }
+//        fileHandle.seekToEndOfFile()
+//        logFileHandle = fileHandle
+//
+//        let originalStdout = dup(STDOUT_FILENO)
+//        let originalStderr = dup(STDERR_FILENO)
+//        
+//        
+//        let outPipe = Pipe()
+//        stdoutPipe = outPipe
+//        dup2(outPipe.fileHandleForWriting.fileDescriptor, STDOUT_FILENO)
+//
+//        
+//        let errPipe = Pipe()
+//        stderrPipe = errPipe
+//        dup2(errPipe.fileHandleForWriting.fileDescriptor, STDERR_FILENO)
+//
+//        
+//        DispatchQueue.global(qos: .background).async {
+//            while let data = try? outPipe.fileHandleForReading.read(upToCount: 4096),
+//                    let output = String(data: data, encoding: .utf8) {
+////                write(originalStdout, output, output.utf8.count)
+//                fileHandle.write(data)
+//                fileHandle.synchronizeFile()
+//            }
+//        }
+//        
+//        DispatchQueue.global(qos: .background).async {
+//            while let data = try? errPipe.fileHandleForReading.read(upToCount: 4096),
+//                    let output = String(data: data, encoding: .utf8) {
+////                write(originalStderr, output, output.utf8.count)
+//                fileHandle.write(data)
+//                fileHandle.synchronizeFile()
+//            }
+//        }
         
         started = true
     }
