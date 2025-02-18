40388251e0e04f86ae6de312f3cea53995a61dbd
Mikhail Golovko <m@anytype.io>
2025-02-18 16:38:52 +0300

    IOS-4173 paste2

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/InputPanel/Input/ChatInputLinkParser.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/InputPanel/Input/ChatInputLinkParser.swift
@@ -8,6 +8,7 @@ enum ChatInputLinkParserChange: Equatable {
 
 protocol ChatInputLinkParserProtocol: AnyObject {
     func handleInput(sourceText: NSAttributedString, range: NSRange, replacementText: String) -> ChatInputLinkParserChange?
+    func handlePaste(sourceText: NSAttributedString, range: NSRange, replacementText: String) -> [ChatInputLinkParserChange]
 }
 
 final class ChatInputLinkParser: ChatInputLinkParserProtocol {
@@ -24,6 +25,38 @@ final class ChatInputLinkParser: ChatInputLinkParserProtocol {
         
         guard replacementText == " " || replacementText == "\n" else { return nil }
         
+        let replacedAttributedText = sourceText.mutable
+        replacedAttributedText.replaceCharacters(in: range, with: replacementText)
+        
+        let replacedText = replacedAttributedText.string
+
+        let matches = regex.matches(
+            in: replacedText,
+            options: [],
+            range: NSRange(location: 0, length: replacedText.utf16.count)
+        )
+        
+        let textLenDiff = replacedText.count - sourceText.string.count
+        guard let match = matches.last, match.range.contains(range.location - textLenDiff) else { return nil }
+        
+        let link = (replacedText as NSString).substring(with: match.range).lowercased()
+    
+        guard let linkUrl = URL(string: link) else { return nil }
+        
+        return .addLinkStyle(range: match.range, link: linkUrl, text: replacedAttributedText)
+    }
+    
+    func handlePaste(sourceText: NSAttributedString, range: NSRange, replacementText: String) -> [ChatInputLinkParserChange] {
+        
+    }
+    
+    // MARK: - Private
+    
+    func handle(
+        sourceText: NSAttributedString,
+        range: NSRange,
+        replacementText: String
+    ) -> [ChatInputLinkParserChange] {
         let replacedAttributedText = sourceText.mutable
         replacedAttributedText.replaceCharacters(in: range, with: replacementText)
         
