178cabc44685bd23167d7ac3b389f7aab3a853a7
Vova Ignatov <abbadoh@gmail.com>
2025-02-20 14:45:20 +0000

    IOS-4062 Updated config

--- Anytype/Sources/PresentationLayer/Common/SwiftUI/IconView/Specific/Object/CustomIconView.swift
+++ Anytype/Sources/PresentationLayer/Common/SwiftUI/IconView/Specific/Object/CustomIconView.swift
@@ -14,9 +14,13 @@ struct CustomIconView: View {
     private static let configs = [
         Config(side: 16, padding: 0),
         Config(side: 18, padding: 0),
-        Config(side: 40, padding: 6),
-        Config(side: 48, padding: 6),
-        Config(side: 64, padding: 8)
+        Config(side: 32, padding: 6),
+        Config(side: 40, padding: 8),
+        Config(side: 48, padding: 9),
+        Config(side: 64, padding: 14),
+        Config(side: 80, padding: 16),
+        Config(side: 96, padding: 22),
+        Config(side: 112, padding: 24)
     ].sorted(by: { $0.side > $1.side }) // Order by DESC side for simple search
     
     
