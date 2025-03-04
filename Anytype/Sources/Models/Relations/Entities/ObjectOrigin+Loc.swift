78835a257d9e80a76e33442c1e786bf6ad63d26d
Mikhail Golovko <m@anytype.io>
2025-03-04 17:33:59 +0300

    IOS-4273 Fixes

--- Anytype/Sources/Models/Relations/Entities/ObjectOrigin+Loc.swift
+++ Anytype/Sources/Models/Relations/Entities/ObjectOrigin+Loc.swift
@@ -4,7 +4,7 @@ import Services
 extension ObjectOrigin {
     var title: String? {
         switch self {
-        case .none, .UNRECOGNIZED, .api:
+        case .none, .UNRECOGNIZED:
             return nil
         case .clipboard:
             return Loc.Relation.Origin.clipboard
