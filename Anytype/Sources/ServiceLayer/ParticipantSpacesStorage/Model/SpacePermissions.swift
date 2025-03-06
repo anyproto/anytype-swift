1519ab485e3216fa8ddcbf6595c0b2d1f687986d
Vova Ignatov <abbadoh@gmail.com>
2025-03-06 11:00:22 +0000

    IOS-4283 Add approve requests permissions

--- Anytype/Sources/ServiceLayer/ParticipantSpacesStorage/Model/SpacePermissions.swift
+++ Anytype/Sources/ServiceLayer/ParticipantSpacesStorage/Model/SpacePermissions.swift
@@ -11,6 +11,7 @@ struct SpacePermissions: Equatable {
     let canCancelJoinRequest: Bool
     let canDeleteLink: Bool
     let canEditPermissions: Bool
+    let canApproveRequests: Bool
 }
 
 extension SpacePermissions {
@@ -46,5 +47,6 @@ extension SpacePermissions {
         canCancelJoinRequest = spaceView.accountStatus == .spaceJoining
         canDeleteLink = isOwner && !isLocalMode
         canEditPermissions = isOwner && !isLocalMode
+        canApproveRequests = isOwner
     }
 }
