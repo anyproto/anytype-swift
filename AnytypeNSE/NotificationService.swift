babd3a9614c64199624e0ea6c1909322dbdd0992
Anna Zakharova <joe_pusya@anytype.io>
2025-03-10 12:39:12 +0000

    IOS-4235 Add raw NSE

---
+++ AnytypeNSE/NotificationService.swift
@@ -0,0 +1,32 @@
+import UserNotifications
+//import Services
+
+class NotificationService: UNNotificationServiceExtension {
+    
+//    private let authMiddleService: any AuthMiddleServiceProtocol = Container.shared.authMiddleService()
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            let text = bestAttemptContent.userInfo["text"] ?? "[kuku]"
+            bestAttemptContent.title = "bestAttemptContent.title \(text)"
+            
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
