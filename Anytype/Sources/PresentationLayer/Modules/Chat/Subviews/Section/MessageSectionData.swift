3b206058e9a56df457069b645e9ddca56f020f47
Mikhail Golovko <m@anytype.io>
2025-03-13 11:43:07 +0300

    IOS-4300 Split file

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/Section/MessageSectionData.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/Section/MessageSectionData.swift
@@ -5,17 +5,3 @@ struct MessageSectionData: Identifiable, Equatable, Hashable, ChatCollectionSect
     var id: Int
     var items: [MessageSectionItem]
 }
-
-enum MessageSectionItem: Equatable, Hashable, Identifiable {
-    case message(_ data: MessageViewData)
-    case unread(_ id: String)
-    
-    var id: String {
-        switch self {
-        case .message(let data):
-            data.id
-        case .unread(let id):
-            id
-        }
-    }
-}
