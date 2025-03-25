3a8cf2892d4fc92db71ba9b3f0110b9369f3be03
Vova Ignatov <abbadoh@gmail.com>
2025-03-25 13:51:14 +0000

    IOS-4332 Supported plurals in editor

--- Anytype/Sources/ServiceLayer/Object/TypeProvider/ObjectTypeProvider.swift
+++ Anytype/Sources/ServiceLayer/Object/TypeProvider/ObjectTypeProvider.swift
@@ -101,6 +101,7 @@ final class ObjectTypeProvider: ObjectTypeProviderProtocol, Sendable {
         return ObjectType(
             id: id,
             name: Loc.ObjectType.deletedName,
+            pluralName: Loc.ObjectType.deletedName,
             icon: .deleted,
             description: "",
             hidden: false,
