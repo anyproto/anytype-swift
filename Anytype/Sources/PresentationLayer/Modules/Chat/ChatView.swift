9e98cf518e37835fad81160852ee4ee215d08e29
Mikhail Golovko <m@anytype.io>
2025-03-26 18:55:39 +0300

    IOS-4115 Update

--- Anytype/Sources/PresentationLayer/Modules/Chat/ChatView.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/ChatView.swift
@@ -189,6 +189,8 @@ struct ChatView: View {
             await model.scrollToBottom()
         } handleVisibleRange: { from, to in
             model.visibleRangeChanged(from: from, to: to)
+        } onTapCollectionBackground: {
+            model.onTapDismissKeyboard()
         }
         .messageYourBackgroundColor(model.messageYourBackgroundColor)
     }
