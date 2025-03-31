f15914ebe2aef51166c57d176799a3db32876292
Mikhail Golovko <m@anytype.io>
2025-03-31 17:23:34 +0300

    IOS-4379 Add generator

--- Modules/Loc/Scripts/generate.sh
+++ Modules/Loc/Scripts/generate.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-./Tools/SwiftGen/swiftgen config run --config ${SCRIPT_DIR}/../Templates/swiftgen.yml
+./Tools/SwiftGen/swiftgen config run --config ${SCRIPT_DIR}/Configs/swiftgen.yml
