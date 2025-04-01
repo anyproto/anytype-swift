b51e92b1e466e1f42de0dd075e9176002ab95849
Vova Ignatov <abbadoh@gmail.com>
2025-04-01 17:01:15 +0100

    IOS-4378 Fix plural title

--- Anytype/Sources/PresentationLayer/Search screen/WidgetSourceSearch/WidgetSourceSearchViewModel/WidgetSourceSearchViewModel.swift
+++ Anytype/Sources/PresentationLayer/Search screen/WidgetSourceSearch/WidgetSourceSearchViewModel/WidgetSourceSearchViewModel.swift
@@ -155,7 +155,7 @@ private extension SearchObjectRowView.Model {
 private extension SearchObjectRowView.Model {
     
     init(details: ObjectDetails) {
-        let title = FeatureFlags.pluralNames ? details.pluralName : details.title
+        let title = FeatureFlags.pluralNames ? details.pluralTitle : details.title
         self.id = details.id
         self.icon = details.objectIconImage
         self.title = title
