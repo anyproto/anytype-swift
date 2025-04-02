7637e37d360a64d31d601ea9e642d57267579627
Anna Zakharova <joe_pusya@anytype.io>
2025-04-02 15:56:46 +0100

    IOS-4175 Enable objectTypeWidgets and openMediaFileInPreview toggles

--- Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
+++ Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
@@ -59,9 +59,8 @@ public extension FeatureDescription {
     
     static let objectTypeWidgets = FeatureDescription(
         title: "Object Type widgets",
-        type: .feature(author: "m@anytype.io,joe_pusya@anytype.io", releaseVersion: "11"),
-        releaseAnytypeValue: false,
-        releaseAnyAppValue: true
+        type: .feature(author: "m@anytype.io,joe_pusya@anytype.io", releaseVersion: "10"),
+        defaultValue: true
     )
 
     static let newTypeIcons = FeatureDescription(
@@ -72,9 +71,8 @@ public extension FeatureDescription {
     
     static let openMediaFileInPreview = FeatureDescription(
         title: "Open all media files in preview",
-        type: .feature(author: "joe_pusya@anytype.io", releaseVersion: "11"),
-        releaseAnytypeValue: false,
-        releaseAnyAppValue: true
+        type: .feature(author: "joe_pusya@anytype.io", releaseVersion: "10"),
+        defaultValue: true
     )
     
     static let openBookmarkAsLink = FeatureDescription(
