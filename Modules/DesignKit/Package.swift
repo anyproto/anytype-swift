81636d0b79a56ff7aeb2e8bb73c522764448340e
Mikhail Golovko <m@anytype.io>
2025-04-23 13:09:12 +0300

    IOS-4506 Fix build

--- Modules/DesignKit/Package.swift
+++ Modules/DesignKit/Package.swift
@@ -9,16 +9,19 @@ let package = Package(
         .iOS(.v16),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DesignKit",
             targets: ["DesignKit"]),
     ],
+    dependencies: [
+        .package(path: "../AnytypeCore")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "DesignKit",
+            dependencies: [
+                "AnytypeCore"
+            ],
             resources: [
                 .process("Resources")
             ],
