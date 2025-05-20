cbdcc6962fdee9882954f108bfa191a8205fc988
Vova Ignatov <abbadoh@gmail.com>
2025-05-20 12:07:23 +0100

    IOS-4593 Turn off toggle

--- Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
+++ Modules/AnytypeCore/AnytypeCore/Utils/FeatureFlags/FeatureDescription+Flags.swift
@@ -27,7 +27,7 @@ public extension FeatureDescription {
     static let hideWebPayments = FeatureDescription(
         title: "Hide web based payments for memebership tier",
         type: .feature(author: "vova@anytype.io", releaseVersion: "On demand"),
-        defaultValue: true
+        defaultValue: false
     )
     
     static let homeSpaceLevelChat = FeatureDescription(
