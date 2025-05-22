6461f8993cf07f4c8e51b4328490c137441681a4
Mikhail Golovko <m@anytype.io>
2025-05-22 11:51:48 +0300

    IOS-4658 Reorder

--- Modules/Services/Sources/Models/Details/Entities/AnytypeWidgetId.swift
+++ Modules/Services/Sources/Models/Details/Entities/AnytypeWidgetId.swift
@@ -2,9 +2,9 @@ import Foundation
 
 public enum AnytypeWidgetId: String, CaseIterable, Sendable {
     case favorite = "favorite"
+    case chat = "chat"
     case allObjects = "allObjects"
     case recent = "recent"
     case recentOpen = "recentOpen"
     case bin = "bin"
-    case chat = "chat"
 }
