053f1fb2374bd6cf77fbfd7760538ae37e973962
Anna Zakharova <joe_pusya@anytype.io>
2025-06-04 15:52:15 +0100

    IOS-4509 Update to 1.0.1

--- Modules/NotificationsCore/Package.swift
+++ Modules/NotificationsCore/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["NotificationsCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/anyproto/any-crypto-swift", from: "1.0.0"),
+        .package(url: "https://github.com/anyproto/any-crypto-swift", from: "1.0.1"),
         .package(path: "../AnytypeCore"),
         .package(path: "../SecureService"),
     ],
