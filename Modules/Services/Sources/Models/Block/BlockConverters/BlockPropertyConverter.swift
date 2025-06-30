55e27f0615fcbb14a0a352b109a4dbec8e8683ba
Vova Ignatov <abbadoh@gmail.com>
2025-06-30 13:19:12 +0100

    IOS-4660 Rename relation to property in block models and services
    - Rename RelationListWithValueService to PropertyListWithValueService
    - Rename BlockRelation to BlockProperty
    - Rename BlockRelationConverter to BlockPropertyConverter
    - Update all references throughout codebase

--- Modules/Services/Sources/Models/Block/BlockConverters/BlockPropertyConverter.swift
+++ Modules/Services/Sources/Models/Block/BlockConverters/BlockPropertyConverter.swift
@@ -3,12 +3,12 @@ import ProtobufMessages
 public extension Anytype_Model_Block.Content.Relation {
     var blockContent: BlockContent {
         .relation(
-            BlockRelation(key: key)
+            BlockProperty(key: key)
         )
     }
 }
 
-public extension BlockRelation {
+public extension BlockProperty {
     var asMiddleware: Anytype_Model_Block.OneOf_Content {
         .relation(.with {
             $0.key = key
