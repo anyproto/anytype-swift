1ece59ca7b9f189fe3082b3fe4cadad65ceb1156
Mikhail Golovko <m@anytype.io>
2025-07-07 16:16:14 +0300

    IOS-4837 Rename downloading state

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/LinkObject/Common/MessageImageView.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/LinkObject/Common/MessageImageView.swift
@@ -1,17 +1,14 @@
 import SwiftUI
+import Services
 
 struct MessageImageView: View {
     
-    let imageId: String
-    
-    init(imageId: String) {
-        self.imageId = imageId
-    }
+    let details: MessageAttachmentDetails
     
     var body: some View {
         GeometryReader { reader in
             ToggleCachedAsyncImage(
-                url: ImageMetadata(id: imageId, side: .width(min(reader.size.width, reader.size.height))).contentUrl,
+                url: ImageMetadata(id: details.id, side: .width(min(reader.size.width, reader.size.height))).contentUrl,
                 urlCache: .anytypeImages
             ) { content in
                 switch content {
