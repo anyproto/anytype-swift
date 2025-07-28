d910e9e5c05f332af302e061119a2cb27becf57f
Anna Zakharova <joe_pusya@anytype.io>
2025-07-28 14:05:00 +0100

    IOS-4788 Update actions icon

--- Modules/Assets/Sources/Assets/Generated/ImageAssets.swift
+++ Modules/Assets/Sources/Assets/Generated/ImageAssets.swift
@@ -799,7 +799,6 @@ extension ImageAsset {
   }
   public enum X32 {
     public static let actions = ImageAsset.bundle(name: "x32/Actions")
-    public static let actions2 = ImageAsset.bundle(name: "x32/Actions2")
     public static let addBelow = ImageAsset.bundle(name: "x32/Add Below")
     public enum AddColumn {
       public static let above = ImageAsset.bundle(name: "x32/Add Column/Above")
