2666068c60c20e9b7561d8f29f8636910079f13c
Vova Ignatov <abbadoh@gmail.com>
2025-08-18 12:34:52 +0100

    IOS-4996 Supported site actions

--- Anytype/Sources/PresentationLayer/Settings/PublishedSites/PublishedSitesView.swift
+++ Anytype/Sources/PresentationLayer/Settings/PublishedSites/PublishedSitesView.swift
@@ -4,6 +4,7 @@ import Services
 
 struct PublishedSitesView: View {
     @StateObject private var model: PublishedSitesViewModel
+    @Environment(\.pageNavigation) private var pageNavigation
     
     init() {
         _model = StateObject(wrappedValue: PublishedSitesViewModel())
@@ -15,7 +16,13 @@ struct PublishedSitesView: View {
             TitleView(title: Loc.mySites)
             content
         }
+        .snackbar(toastBarData: $model.toastBarData)
+        .safariSheet(url: $model.safariUrl)
+        
         .task { await model.loadData() }
+        .onAppear {
+            model.pageNavigation = pageNavigation
+        }
     }
     
     var content: some View {
@@ -49,14 +56,83 @@ struct PublishedSitesView: View {
     
     private func siteRow(_ site: PublishState) -> some View {
         HStack(spacing: 12) {
-            IconView(icon: site.details.objectIconImage)
-                .frame(width: 48, height: 48)
-            Text(site.details.name)
-                .anytypeStyle(.uxTitle2Medium)
-            Spacer()
+            Button {
+                model.onOpenObjectTap(site)
+            } label: {
+                IconView(icon: site.details.objectIconImage)
+                    .frame(width: 48, height: 48)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(site.details.name)
+                        .anytypeStyle(.uxTitle2Medium)
+                        .lineLimit(1)
+                    HStack(alignment: .center, spacing: 6) {
+                        Text("\(model.formattedDate(site.date))")
+                            .anytypeStyle(.caption1Regular)
+                            .foregroundStyle(Color.Text.secondary)
+                            .lineLimit(1)
+                        
+                        Image(asset: .CustomIcons.ellipse)
+                            .resizable()
+                            .frame(width: 2, height: 2)
+                            .foregroundStyle(Color.Text.secondary)
+                        
+                        Text("\(model.formattedSize(site.size))")
+                            .anytypeStyle(.caption1Regular)
+                            .foregroundStyle(Color.Text.secondary)
+                            .lineLimit(1)
+                    }
+                }
+                Spacer()
+            }
+            
+            
+            Menu {
+                menuButtons(site)
+            } label: {
+                MoreIndicator()
+            }
+
         }
         .padding(.horizontal, 16)
         .padding(.vertical, 12)
+    }
+    
+    private func menuButtons(_ site: PublishState) -> some View {
+        Group {
+            Button {
+                model.onOpenObjectTap(site)
+            } label: {
+                Text(Loc.openObject)
+                Spacer()
+                Image(systemName: "arrow.down.left.and.arrow.up.right")
+            }
+            
+            Button {
+                model.onViewInBrowserTap(site)
+            } label: {
+                Text(Loc.viewInBrowser)
+                Spacer()
+                Image(systemName: "globe")
+            }
+            
+            Button {
+                model.onCopyLinkTap(site)
+            } label: {
+                Text(Loc.copyLink)
+                Spacer()
+                Image(systemName: "document.on.document")
+            }
+            
+            Divider()
+            
+            AsyncButton(role: .destructive) {
+                try await model.onUnpublishTap(site)
+            } label: {
+                Text(Loc.unpublish)
+                Spacer()
+                Image(systemName: "xmark")
+            }
+        }
     }
     
     private var emptyView: some View {
--- Anytype/Sources/PresentationLayer/Settings/PublishedSites/PublishedSitesViewModel.swift
+++ Anytype/Sources/PresentationLayer/Settings/PublishedSites/PublishedSitesViewModel.swift
@@ -7,10 +7,21 @@ import AnytypeCore
 final class PublishedSitesViewModel: ObservableObject {
     @Injected(\.publishingService)
     private var publishingService: any PublishingServiceProtocol
+    @Injected(\.accountParticipantsStorage)
+    private var participantStorage: any AccountParticipantsStorageProtocol
+    @Injected(\.publishedUrlBuilder)
+    private var urlBuilder: any PublishedUrlBuilderProtocol
     
+    private let dateFormatter = HistoryDateFormatter()
+    private let byteCountFormatter = ByteCountFormatter.fileFormatter
     
     @Published var sites: [PublishState]?
     @Published var error: String?
+    @Published var safariUrl: URL?
+    @Published var toastBarData: ToastBarData?
+    
+    var pageNavigation: PageNavigation?
+    lazy var domain: DomainType? = { participantStorage.participants.first?.publishingDomain }()
     
     init() { }
     
@@ -21,5 +32,49 @@ final class PublishedSitesViewModel: ObservableObject {
         } catch {
             self.error = error.localizedDescription
         }
+    }
+    
+    func formattedDate(_ date: Date) -> String {
+        dateFormatter.localizedDateString(for: date)
+    }
+    
+    func formattedSize(_ size: Int64) -> String {
+        byteCountFormatter.string(fromByteCount: size)
+    }
+    
+    func onOpenObjectTap(_ site: PublishState) {
+        pageNavigation?.open(.editor(.page(
+            EditorPageObject(objectId: site.objectId, spaceId: site.spaceId)
+        )))
+    }
+    
+    func onViewInBrowserTap(_ site: PublishState) {
+        AnytypeAnalytics.instance().logClickShareObjectOpenPage(objectType: site.details.analyticsType, route: .mySites)
+        guard let domain else { return }
+        guard let publishedUrl = urlBuilder.buildPublishedUrl(domain: domain, customPath: site.uri) else { return }
+        
+        
+        safariUrl = publishedUrl
+    }
+    
+    func onCopyLinkTap(_ site: PublishState) {
+        AnytypeAnalytics.instance().logClickShareObjectCopyUrl(objectType: site.details.analyticsType)
+        guard let domain else { return }
+        guard let publishedUrl = urlBuilder.buildPublishedUrl(domain: domain, customPath: site.uri) else { return }
+        
+        UIPasteboard.general.string = publishedUrl.absoluteString
+        toastBarData = ToastBarData(Loc.copiedToClipboard(Loc.link))
+    }
+    
+    func onUnpublishTap(_ site: PublishState) async throws {
+        AnytypeAnalytics.instance().logClickShareObjectUnpublish(objectType: site.details.analyticsType)
+        
+        try await publishingService.remove(spaceId: site.spaceId, objectId: site.objectId)
+        
+        AnytypeAnalytics.instance().logShareObjectUnpublish(objectType: site.details.analyticsType)
+        toastBarData = ToastBarData(Loc.PublishingToWeb.unpublished)
+        
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+        await loadData()
     }
 }
