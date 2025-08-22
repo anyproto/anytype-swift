07b9d26add9d95db2515f2183bcdd2436af46fc4
Vova Ignatov <abbadoh@gmail.com>
2025-08-22 11:53:15 +0100

    IOS-5033 Updated pin icon

--- Anytype/Sources/PresentationLayer/Modules/SpaceHub/Subviews/NewSpaceCardLabel.swift
+++ Anytype/Sources/PresentationLayer/Modules/SpaceHub/Subviews/NewSpaceCardLabel.swift
@@ -172,9 +172,9 @@ struct NewSpaceCardLabel: View {
     @ViewBuilder
     private var pin: some View {
         if !spaceData.preview.hasCounters && FeatureFlags.pinnedSpaces && spaceData.spaceView.isPinned {
-            Image(asset: .X24.pin)
+            Image(asset: .X18.pin)
                 .foregroundStyle(Color.Control.transparentSecondary)
-                .frame(width: 22, height: 22)
+                .frame(width: 18, height: 18)
         }
     }
     
