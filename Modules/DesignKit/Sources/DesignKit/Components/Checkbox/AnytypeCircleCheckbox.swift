ffa399d11af4f68a7a9686b8058537caf464f171
Mikhail Golovko <m@anytype.io>
2025-08-26 13:26:10 +0300

    IOS-4290 Fix

--- Modules/DesignKit/Sources/DesignKit/Components/Checkbox/AnytypeCircleCheckbox.swift
+++ Modules/DesignKit/Sources/DesignKit/Components/Checkbox/AnytypeCircleCheckbox.swift
@@ -7,17 +7,20 @@ public struct AnytypeCircleCheckbox: View {
     private(set) var backgroundCheckedColor: Color
     private(set) var foregroundCheckedColor: Color
     private(set) var foregroundUncheckedColor: Color
+    private var interactive: Bool
     
     public init(
         checked: Binding<Bool>,
         backgroundCheckedColor: Color = .Control.primary,
         foregroundCheckedColor: Color = .Control.white,
-        foregroundUncheckedColor: Color = .Control.tertiary
+        foregroundUncheckedColor: Color = .Control.tertiary,
+        interactive: Bool = true
     ) {
         self._checked = checked
         self.backgroundCheckedColor = backgroundCheckedColor
         self.foregroundCheckedColor = foregroundCheckedColor
         self.foregroundUncheckedColor = foregroundUncheckedColor
+        self.interactive = interactive
     }
     
     public var body: some View {
@@ -34,19 +37,21 @@ public struct AnytypeCircleCheckbox: View {
                 Circle()
                     .stroke(foregroundUncheckedColor, lineWidth: 1)
             }
-        }.onTapGesture {
+        }
+        .onTapGesture {
             UISelectionFeedbackGenerator().selectionChanged()
             withAnimation {
                 checked.toggle()
             }
         }
+        .allowsHitTesting(interactive)
         .frame(width: 24, height: 24)
     }
 }
 
 public extension AnytypeCircleCheckbox {
     init(checked: Bool) {
-        self.init(checked: .constant(checked))
+        self.init(checked: .constant(checked), interactive: false)
     }
 }
 
