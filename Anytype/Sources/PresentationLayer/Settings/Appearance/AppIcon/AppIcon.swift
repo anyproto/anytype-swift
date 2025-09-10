164bc92c2aef0466f0f631c6a8d1924aec8ca2e9
Vova Ignatov <abbadoh@gmail.com>
2025-09-10 11:20:59 +0100

    IOS-5134 Added eggo to debug and nightly builds

--- Anytype/Sources/PresentationLayer/Settings/Appearance/AppIcon/AppIcon.swift
+++ Anytype/Sources/PresentationLayer/Settings/Appearance/AppIcon/AppIcon.swift
@@ -4,18 +4,29 @@ import SwiftUI
 enum AppIcon: CaseIterable {
     case smile
     case standart
+    case eggo
+    
+    var avaliableIcons: [AppIcon] {
+        #if DEBUG || RELEASE_NIGHTLY
+        [.smile, .standart, .eggo]
+        #else
+        [ .smile, .standart]
+        #endif
+    }
     
     var previewAsset: ImageAsset {
         switch self {
-        case .standart: return .AppIconsPreview.appIcon
-        case .smile: return .AppIconsPreview.appIconSmile
+        case .standart: .AppIconsPreview.appIcon
+        case .smile: .AppIconsPreview.appIconSmile
+        case .eggo: .AppIconsPreview.appIconEgg
         }
     }
     
     var iconName: String? {
         switch self {
-        case .standart: return nil
-        case .smile: return "AppIconSmile"
+        case .standart: nil
+        case .smile: "AppIconSmile"
+        case .eggo: "AppIconEgg"
         }
     }
 }
