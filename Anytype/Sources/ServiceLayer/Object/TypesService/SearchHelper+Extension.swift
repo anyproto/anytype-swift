d22da53a7fb368acc7444853afa321335a68a01f
Mikhail Golovko <m@anytype.io>
2025-09-23 15:40:14 +0300

    IOS-5205 Update order

--- Anytype/Sources/ServiceLayer/Object/TypesService/SearchHelper+Extension.swift
+++ Anytype/Sources/ServiceLayer/Object/TypesService/SearchHelper+Extension.swift
@@ -5,14 +5,33 @@ extension SearchHelper {
     static func defaultObjectTypeSort(isChat: Bool) -> [DataviewSort] {
         let nameSort = SearchHelper.sort(relation: .name, type: .asc)
         
-        let customSort = SearchHelper.customSort(relationKey: BundledPropertyKey.uniqueKey.rawValue, values: [
-            ObjectTypeUniqueKey.page.value,
-            ObjectTypeUniqueKey.task.value,
-            ObjectTypeUniqueKey.collection.value,
-            ObjectTypeUniqueKey.set.value,
-            ObjectTypeUniqueKey.bookmark.value,
-            ObjectTypeUniqueKey.note.value
-        ])
+        let customSort = isChat
+            ? SearchHelper.customSort(relationKey: BundledPropertyKey.uniqueKey.rawValue, values: [
+                ObjectTypeUniqueKey.image.value,
+                ObjectTypeUniqueKey.bookmark.value,
+                ObjectTypeUniqueKey.file.value,
+                ObjectTypeUniqueKey.page.value,
+                ObjectTypeUniqueKey.note.value,
+                ObjectTypeUniqueKey.task.value,
+                ObjectTypeUniqueKey.collection.value,
+                ObjectTypeUniqueKey.set.value,
+                ObjectTypeUniqueKey.project.value,
+                ObjectTypeUniqueKey.video.value,
+                ObjectTypeUniqueKey.audio.value
+            ])
+            : SearchHelper.customSort(relationKey: BundledPropertyKey.uniqueKey.rawValue, values: [
+                ObjectTypeUniqueKey.page.value,
+                ObjectTypeUniqueKey.note.value,
+                ObjectTypeUniqueKey.task.value,
+                ObjectTypeUniqueKey.collection.value,
+                ObjectTypeUniqueKey.set.value,
+                ObjectTypeUniqueKey.bookmark.value,
+                ObjectTypeUniqueKey.project.value,
+                ObjectTypeUniqueKey.image.value,
+                ObjectTypeUniqueKey.file.value,
+                ObjectTypeUniqueKey.video.value,
+                ObjectTypeUniqueKey.audio.value
+            ])
         
         let orderIdSort = SearchHelper.sort(relation: .orderId, type: .asc)
         
