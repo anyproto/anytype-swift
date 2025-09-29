c2a1957926e14a03dcf6ad7eee24b12a9f94021a
Mikhail Golovko <m@anytype.io>
2025-09-29 18:10:19 +0300

    IOS-5233 fix

--- Modules/ProtobufMessages/Scripts/generate.sh
+++ Modules/ProtobufMessages/Scripts/generate.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="${SCRIPT_DIR}/../../../"
+
+# Generate models from protoc
+
+protoc --plugin=protoc-gen-swift="${ROOT_DIR}/build/swift-protobuf/build/release" --swift_out="${ROOT_DIR}/Dependencies/Test" --proto_path="${ROOT_DIR}/Dependencies/Middleware/protobuf/protos" ${ROOT_DIR}/Dependencies/Middleware/protobuf/protos/*.proto
 
 # ./Tools/SwiftGen/swiftgen config run --config ${SCRIPT_DIR}/Configs/swiftgen.yml
-./build/sourcery --config ${SCRIPT_DIR}/Loc/sourcery.yml
+#./build/sourcery --config ${SCRIPT_DIR}/Loc/sourcery.yml
