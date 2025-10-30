f542091d5784be2966acc86d403f6425e03424c2
Mikhail Golovko <m@anytype.io>
2025-10-30 12:30:12 +0300

    IOS-5387 Test

--- Anytype/Sources/PresentationLayer/Flows/SpaceHub/SpaceHubCoordinatorView.swift
+++ Anytype/Sources/PresentationLayer/Flows/SpaceHub/SpaceHubCoordinatorView.swift
@@ -27,92 +27,92 @@ struct SpaceHubCoordinatorView: View {
             .updateShortcuts(spaceId: model.fallbackSpaceId)
             .snackbar(toastBarData: $model.toastBarData)
             
-            .sheet(item: $model.showGalleryImport) { data in
-                GalleryInstallationCoordinatorView(data: data)
-            }
-            .sheet(isPresented: $model.showSpaceManager) {
-                SpacesManagerView()
-            }
-            .sheet(item: $model.membershipTierId) { tierId in
-                MembershipCoordinator(initialTierId: tierId.value)
-            }
-            .sheet(item: $model.membershipNameFinalizationData) {
-                MembershipNameFinalizationView(tier: $0)
-            }
-            .sheet(item: $model.showGlobalSearchData) {
-                GlobalSearchView(data: $0)
-            }
-            .sheet(item: $model.typeSearchForObjectCreationSpaceId) {
-                model.typeSearchForObjectCreationModule(spaceId: $0.value)
-            }
-            .anytypeSheet(item: $model.spaceJoinData) {
-                SpaceJoinView(data: $0, onManageSpaces: {
-                    model.onManageSpacesSelected()
-                })
-            }
-            .anytypeSheet(item: $model.userWarningAlert, dismissOnBackgroundView: false) {
-                UserWarningAlertCoordinatorView(alert: $0)
-            }
-            .anytypeSheet(isPresented: $model.showObjectIsNotAvailableAlert) {
-                ObjectIsNotAvailableAlert()
-            }
-            .sheet(item: $model.showSpaceShareData) {
-                SpaceShareCoordinatorView(data: $0)
-                    .pageNavigation(model.pageNavigation)
-            }
-            .sheet(item: $model.showSpaceMembersData) {
-                SpaceMembersView(data: $0)
-                    .pageNavigation(model.pageNavigation)
-            }
-            .anytypeSheet(item: $model.profileData) {
-                ProfileView(info: $0)
-                    .pageNavigation(model.pageNavigation)
-            }
-            .anytypeSheet(item: $model.spaceProfileData) {
-                SpaceProfileView(info: $0)
-            }
-            .safariBookmarkObject($model.bookmarkScreenData) {
-                model.onOpenBookmarkAsObject($0)
-            }
-            .sheet(item: $model.spaceCreateData) {
-                SpaceCreateCoordinatorView(data: $0)
-            }
-            .sheet(isPresented: $model.showSpaceTypeForCreate) {
-                SpaceCreateTypePickerView(onSelectSpaceType: { type in
-                    model.onSpaceTypeSelected(type)
-                }, onSelectQrCodeScan: {
-                    model.onSelectQrCodeScan()
-                })
-                .navigationZoomTransition(sourceID: "SpaceCreateTypePickerView", in: namespace)
-            }
-            .qrCodeScanner(shouldScan: $model.shouldScanQrCode)
-            .sheet(isPresented: $model.showSharingExtension) {
-                SharingExtensionCoordinatorView()
-            }
-            .sheet(isPresented: $model.showAppSettings) {
-                SettingsCoordinatorView()
-                    .pageNavigation(model.pageNavigation)
-            }
-        
-            // load photos
-            .photosPicker(isPresented: $model.showPhotosPicker, selection: $model.photosItems)
-            .onChange(of: model.photosItems) {
-                model.photosPickerFinished()
-            }
-        
-            // load from camera
-            .cameraAccessFullScreenCover(item: $model.cameraData) {
-                SimpleCameraView(data: $0)
-            }
-            
-            // load files
-            .fileImporter(
-                isPresented: $model.showFilesPicker,
-                allowedContentTypes: [.data],
-                allowsMultipleSelection: true
-            ) { result in
-                model.fileImporterFinished(result: result)
-            }
+//            .sheet(item: $model.showGalleryImport) { data in
+//                GalleryInstallationCoordinatorView(data: data)
+//            }
+//            .sheet(isPresented: $model.showSpaceManager) {
+//                SpacesManagerView()
+//            }
+//            .sheet(item: $model.membershipTierId) { tierId in
+//                MembershipCoordinator(initialTierId: tierId.value)
+//            }
+//            .sheet(item: $model.membershipNameFinalizationData) {
+//                MembershipNameFinalizationView(tier: $0)
+//            }
+//            .sheet(item: $model.showGlobalSearchData) {
+//                GlobalSearchView(data: $0)
+//            }
+//            .sheet(item: $model.typeSearchForObjectCreationSpaceId) {
+//                model.typeSearchForObjectCreationModule(spaceId: $0.value)
+//            }
+//            .anytypeSheet(item: $model.spaceJoinData) {
+//                SpaceJoinView(data: $0, onManageSpaces: {
+//                    model.onManageSpacesSelected()
+//                })
+//            }
+//            .anytypeSheet(item: $model.userWarningAlert, dismissOnBackgroundView: false) {
+//                UserWarningAlertCoordinatorView(alert: $0)
+//            }
+//            .anytypeSheet(isPresented: $model.showObjectIsNotAvailableAlert) {
+//                ObjectIsNotAvailableAlert()
+//            }
+//            .sheet(item: $model.showSpaceShareData) {
+//                SpaceShareCoordinatorView(data: $0)
+//                    .pageNavigation(model.pageNavigation)
+//            }
+//            .sheet(item: $model.showSpaceMembersData) {
+//                SpaceMembersView(data: $0)
+//                    .pageNavigation(model.pageNavigation)
+//            }
+//            .anytypeSheet(item: $model.profileData) {
+//                ProfileView(info: $0)
+//                    .pageNavigation(model.pageNavigation)
+//            }
+//            .anytypeSheet(item: $model.spaceProfileData) {
+//                SpaceProfileView(info: $0)
+//            }
+//            .safariBookmarkObject($model.bookmarkScreenData) {
+//                model.onOpenBookmarkAsObject($0)
+//            }
+//            .sheet(item: $model.spaceCreateData) {
+//                SpaceCreateCoordinatorView(data: $0)
+//            }
+//            .sheet(isPresented: $model.showSpaceTypeForCreate) {
+//                SpaceCreateTypePickerView(onSelectSpaceType: { type in
+//                    model.onSpaceTypeSelected(type)
+//                }, onSelectQrCodeScan: {
+//                    model.onSelectQrCodeScan()
+//                })
+//                .navigationZoomTransition(sourceID: "SpaceCreateTypePickerView", in: namespace)
+//            }
+//            .qrCodeScanner(shouldScan: $model.shouldScanQrCode)
+//            .sheet(isPresented: $model.showSharingExtension) {
+//                SharingExtensionCoordinatorView()
+//            }
+//            .sheet(isPresented: $model.showAppSettings) {
+//                SettingsCoordinatorView()
+//                    .pageNavigation(model.pageNavigation)
+//            }
+//        
+//            // load photos
+//            .photosPicker(isPresented: $model.showPhotosPicker, selection: $model.photosItems)
+//            .onChange(of: model.photosItems) {
+//                model.photosPickerFinished()
+//            }
+//        
+//            // load from camera
+//            .cameraAccessFullScreenCover(item: $model.cameraData) {
+//                SimpleCameraView(data: $0)
+//            }
+//            
+//            // load files
+//            .fileImporter(
+//                isPresented: $model.showFilesPicker,
+//                allowedContentTypes: [.data],
+//                allowsMultipleSelection: true
+//            ) { result in
+//                model.fileImporterFinished(result: result)
+//            }
     }
     
     private var content: some View {  
