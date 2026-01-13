8b9e76682ea8372a015e94af6a693f28d08cb8ec
Vova Ignatov <abbadoh@gmail.com>
2026-01-13 11:44:35 +0000

    IOS-5658 Remove unused EditorBarItemState and simplify UIEditorBarButtonItem

--- Anytype/Sources/PresentationLayer/TextEditor/EditorPage/Views/NavigationBar/Settings/UIEditorBarButtonItem.swift
+++ Anytype/Sources/PresentationLayer/TextEditor/EditorPage/Views/NavigationBar/Settings/UIEditorBarButtonItem.swift
@@ -3,28 +3,18 @@ import UIKit
 final class UIEditorBarButtonItem: UIView, CustomizableHitTestAreaView {
     // CustomizableHitTestAreaView
     var minHitTestArea: CGSize = Constants.minimumHitArea
-    
-    private let backgroundView = UIView()
+
     private let button = UIButton(type: .custom)
-    
-    private var state = EditorBarItemState.initial
-    
+
     init(imageAsset: ImageAsset, action: @escaping () -> Void) {
         super.init(frame: .zero)
         setup(imageAsset: imageAsset, action: action)
     }
-    
+
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         return containsCustomHitTestArea(point) ? button : nil
     }
-    
-    func changeState(_ state: EditorBarItemState) {
-        if self.state != state {
-            self.state = state
-            updateState()
-        }
-    }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -32,22 +22,15 @@ final class UIEditorBarButtonItem: UIView, CustomizableHitTestAreaView {
 }
 
 private extension UIEditorBarButtonItem {
-    
+
     func setup(imageAsset: ImageAsset, action: @escaping () -> Void) {
-        setupBackgroundView()
         setupButton(imageAsset: imageAsset, action: action)
         setupLayout()
-        
-        updateState()
     }
-    
-    func setupBackgroundView() {
-        backgroundView.backgroundColor = .black.withAlphaComponent(0.35)
-        backgroundView.layer.cornerRadius = 7
-    }
-    
+
     func setupButton(imageAsset: ImageAsset, action: @escaping () -> Void) {
         button.setImage(UIImage(asset: imageAsset), for: .normal)
+        button.tintColor = .Control.secondary
         button.addAction(
             UIAction(handler: { _ in action() } ),
             for: .touchUpInside
@@ -57,33 +40,20 @@ private extension UIEditorBarButtonItem {
             .undoScale(scale: Constants.pressedScale, duration: Constants.animationDuration)
         )
     }
-    
+
     func setupLayout() {
         layoutUsing.anchors {
             $0.size(CGSize(width: 28, height: 28))
         }
-        
-        addSubview(backgroundView) {
-            $0.pinToSuperview()
-        }
-        
+
         addSubview(button) {
             $0.pinToSuperview()
         }
     }
-    
-    func updateState() {
-        backgroundView.alpha = state.backgroundAlpha
-        button.tintColor = state.buttonTintColor
-    }
-    
+
     func enableAnimation(_ inAnimator: ViewAnimator<UIView>, _ outAnimator: ViewAnimator<UIView>) {
         let recognizer = TouchGestureRecognizer { [weak self] recognizer in
-            guard
-                let self = self
-            else {
-                return
-            }
+            guard let self else { return }
             switch recognizer.state {
             case .began:
                 inAnimator.animate(self.button)
@@ -96,16 +66,14 @@ private extension UIEditorBarButtonItem {
         recognizer.cancelsTouchesInView = false
         button.addGestureRecognizer(recognizer)
     }
-    
 }
 
 private extension UIEditorBarButtonItem {
-    
+
     enum Constants {
         static let animationDuration: TimeInterval = 0.1
         static let pressedScale: CGFloat = 0.8
-        
+
         static let minimumHitArea = CGSize(width: 44, height: 44)
     }
-    
 }
