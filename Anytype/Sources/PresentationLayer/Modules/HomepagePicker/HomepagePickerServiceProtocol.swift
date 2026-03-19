3c0508d6fd8752da7dcd8d3997d0f0d93201be5a
Konstantin Mordan <k@anytype.io>
2026-03-19 14:21:39 +0300

    IOS-5903: Add Sendable conformance to HomepagePickerServiceProtocol

--- Anytype/Sources/PresentationLayer/Modules/HomepagePicker/HomepagePickerServiceProtocol.swift
+++ Anytype/Sources/PresentationLayer/Modules/HomepagePicker/HomepagePickerServiceProtocol.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol HomepagePickerServiceProtocol {
+protocol HomepagePickerServiceProtocol: Sendable {
     /// Creates the homepage object (if needed) and sets it as homepage.
     /// Returns the homepageId (objectId for Chat/Page/Collection, "widgets" for Widgets).
     func createHomepage(spaceId: String, option: HomepagePickerOption) async throws -> String
