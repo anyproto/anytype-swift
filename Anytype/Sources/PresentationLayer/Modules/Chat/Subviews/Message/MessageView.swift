149b803b8429344bd31f7e072f220950ab6fbbdb
Konstantin Mordan <k@anytype.io>
2026-04-03 11:33:10 +0300

    IOS-5947 Fix reaction toggle and long-press disabled when limits reached

--- Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/Message/MessageView.swift
+++ Anytype/Sources/PresentationLayer/Modules/Chat/Subviews/Message/MessageView.swift
@@ -187,7 +187,7 @@ struct MessageView: View {
             MessageReactionList(
                 rows: data.reactions,
                 canAddReaction: data.canAddReaction,
-                canToggleReaction: data.canAddReaction,
+                canToggleReaction: data.canReply,
                 position: data.position,
                 onTapRow: { reaction in
                     try await output?.didTapOnReaction(data: data, emoji: reaction.emoji)
